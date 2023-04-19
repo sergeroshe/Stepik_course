57a3b5cc09b5be2284665cf2780d67d02c866a8e
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-04-19 09:33:39 +0300

    Update Valid_number.py
    Improved MATCH_LIST

--- Valid_number.py
+++ Valid_number.py
@@ -1,11 +1,11 @@
 POSITIVE = 'YES'
 NEGATIVE = 'NO'
-SEP_LIST = '. , : ; -'
+MATCH_LIST = '375.,:;-'
 DIGIT_CHAR = 'd'
 SEP_CHAR = 'p'
 PATTERN_LIST = ['7-ddd-ddd-dddd', '7-dddpddpdd', 'ddd-ddd-dddd', '+375-dd-ddd-dddd']
 
-phone_num = input() #  7-111-111-1111, 7-111.11;11, 111-111-1111, +375-25-640-8668
+phone_num = input() #  7-111-111-1111, 7-111.11;11, 111-111-1111, +375-25-640-8668, 7,111.11;11
 phone_num_len = len(phone_num)
 
 output = POSITIVE
@@ -16,7 +16,7 @@ for pattern in PATTERN_LIST:
         continue
     else:
         for i in range(phone_num_len):
-            if pattern[i].isdigit() or pattern[i] in SEP_LIST:
+            if pattern[i] in MATCH_LIST:
                 if phone_num[i] != pattern[i]:
                     output = NEGATIVE
                     break
@@ -25,7 +25,7 @@ for pattern in PATTERN_LIST:
                     output = NEGATIVE
                     break
             elif pattern[i] == SEP_CHAR:
-                if phone_num[i] not in SEP_LIST:
+                if phone_num[i] not in MATCH_LIST:
                     output = NEGATIVE
                     break
             output = POSITIVE
