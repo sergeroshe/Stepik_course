87e063270d84fbed3437b1c7ca919e9e8e3423fd
sergero <roshchektaev@gmail.com>
2023-04-19 23:12:11 +0300

    Any valid symbol option added

--- Valid_number.py
+++ Valid_number.py
@@ -1,12 +1,12 @@
 POSITIVE_ANSWER = 'YES'
 NEGATIVE_ANSWER = 'NO'
 INITIAL_NUM_CHAR = '+'
-SEP_LIST = '.;:-'
+SEP_LIST = '.;:-,'
 DIGIT_CHAR = 'd'
 SEP_CHAR = 'p'
-PATTERN_LIST = ['7-ddd-ddd-dddd', '7-dddpddpdd', 'ddd-ddd-dddd', '+375-dd-ddd-dddd']
+PATTERN_LIST = ['7-ddd-ddd-dddd', '7-dddpddpdd', 'ddd-ddd-dddd', '+375-dd-ddd-dddd', 'X_1_dps']
 
-phone_num = input() #  7-111-111-1111, 7-111.11;11, 111-111-1111, +375-25-640-8668, 7,111.11;11
+phone_num = 'X_1_3:R' #  7-111-111-1111, 7-111.11;11, 111-111-1111, +375-25-640-8668, 7,111.11;11
 phone_num_len = len(phone_num)
 
 pattern_found = False # is pattern mathing number found in PATTERN_LIST
@@ -15,11 +15,7 @@ for pattern in PATTERN_LIST:
     pattern_matches = True
     if phone_num_len == len(pattern):
         for i in range(phone_num_len):
-            if pattern[i].isdigit() or pattern[i] == INITIAL_NUM_CHAR:
-                if phone_num[i] != pattern[i]:
-                    pattern_matches = False
-                    break
-            elif pattern[i] == DIGIT_CHAR:
+            if pattern[i] == DIGIT_CHAR:
                 if not phone_num[i].isdigit():
                     pattern_matches = False
                     break
@@ -27,6 +23,9 @@ for pattern in PATTERN_LIST:
                 if phone_num[i] not in SEP_LIST:
                     pattern_matches = False
                     break
+            elif phone_num[i] != pattern[i]:
+                pattern_matches = False
+                break
 
     else:
         pattern_matches = False
