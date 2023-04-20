6845b6dae693a0c0435817dda339e23a3a52b09d
sergero <roshchektaev@gmail.com>
2023-04-20 12:16:57 +0300

    Replaced slices with loop

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,8 +1,13 @@
-num_list = [78, -32, 5]
+num_list = [-48, -41, -32, 5, 78, 84, 90, 96, -71]
 list_len = len(num_list)
 
-for i in range(list_len - 1):
-    max_current_idx = num_list.index(max(num_list[:list_len - i]))
-    num_list[max_current_idx], num_list[-i - 1] = num_list[-i - 1], num_list[max_current_idx]
+for i in range(list_len - 1, -1, -1):
+    max_num = num_list[i]
+    max_num_idx = i
+    for j in range(i - 1, -1, -1):
+        if num_list[j] > max_num:
+            max_num = num_list[j]
+            max_num_idx = j
+    num_list[i], num_list[max_num_idx] = num_list[max_num_idx], num_list[i]
 
 print(num_list)
