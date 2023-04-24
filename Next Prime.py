2c78eeb0225e6190568cdbb3c613be51f513d63d
sergero <roshchektaev@gmail.com>
2023-04-24 22:15:47 +0300

    Is number prime? Variables and range improving

--- Next Prime.py
+++ Next Prime.py
@@ -1,13 +1,13 @@
 def is_prime(num):
     next_prime_num = 0
     num += 1
-    half_num_len = num // 2 + 1
-    for i in range(num, num + half_num_len):
+    half_num = num // 2 + 1
+    for i in range(num, num + half_num):
         count = 0
-        for j in range(1, half_num_len):
+        for j in range(1, half_num):
             if i % j == 0:
                 count += 1
-            if count > 2:
+            if count == 2:
                 break
         count += 1
         if count == 2:
