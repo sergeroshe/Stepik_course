a1373381f4e7f12e6acfcf5ed2544a376741151f
sergero <roshchektaev@gmail.com>
2023-04-24 18:54:49 +0300

    Is valid triangle: brain training version

--- Is Valid Triangle_vers_2.py
+++ Is Valid Triangle_vers_2.py
@@ -4,8 +4,10 @@ def is_valid_triangle(side_1, side_2, side_3):
     for i in range(len(sides_angles_list)):
         side_angle = sides_angles_list[i]
         other_sides_sum = sides_angles_list[i - 1] + sides_angles_list[i - 2]
+
         sides_angles_list[i], sides_angles_list[i - 2], sides_angles_list[i - 1]\
             = sides_angles_list[i - 2], sides_angles_list[i - 1], sides_angles_list[i]
+        # shift element left
         if side_angle >= other_sides_sum:
             result = False
             break
