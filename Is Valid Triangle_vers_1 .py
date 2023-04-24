b26384edc643642b2cd40e3e74487819d277cc4c
sergero <roshchektaev@gmail.com>
2023-04-24 18:53:06 +0300

    Simple version

---
+++ Is Valid Triangle_vers_1 .py
@@ -0,0 +1,14 @@
+def is_valid_triangle(side_1, side_2, side_3):
+    while side_1 < (side_2 + side_3) and side_2 < (side_1 + side_3)\
+            and side_3 < (side_1 + side_2):
+        return True
+    else:
+        return False
+
+
+def main():
+    a, b, c = [int(input()), int(input()), int(input())]
+    print(is_valid_triangle(a, b, c))
+
+
+main()
