bbf2555ab9bc0b3899aa01cbe2d8a56ae725b2a8
sergero <roshchektaev@gmail.com>
2023-04-24 14:05:15 +0300

    Task Dividers_1.py with old function reusing. Need a discussion about var. factor using

--- Dividers_1.py
+++ Dividers_1.py
@@ -1,23 +1,20 @@
-# объявление функции
 def get_factors(num):
-
     dividers_list = []
     for i in range(1, num // 2 + 1):
         if num % i == 0:
             dividers_list.append(i)
 
-    return dividers_list + [num]
+    dividers_list.append(num)
+    return dividers_list
 
 
 def main():
+    count = 0
     # считываем данные
     given_num = int(input())
-    print(get_factors(given_num))
+    for factor in get_factors(given_num):  # why is factor not used? want to get it
+        count += 1
+    print(count)
 
 
-# вызываем функцию
 main()
-
-#  Напишите функцию get_factors(num),
-#  принимающую в качестве аргумента натуральное число
-#  и возвращающую список всех делителей данного числа.
