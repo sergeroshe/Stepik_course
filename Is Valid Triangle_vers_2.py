d4afefd0862b412c38f0212c3fa08084475d30e0
sergero <roshchektaev@gmail.com>
2023-04-25 11:09:50 +0300

    Is valid triangle, var. improved

--- Is Valid Triangle_vers_2.py
+++ Is Valid Triangle_vers_2.py
@@ -1,19 +1,19 @@
 def is_valid_triangle(side_1, side_2, side_3):
-    sides_angles_list = [side_1, side_2, side_3]
+    side_angles_list = [side_1, side_2, side_3]
     result = True
-    for i in range(len(sides_angles_list)):
-        side_angle = sides_angles_list[i]
-        other_sides_sum = sides_angles_list[i - 1] + sides_angles_list[i - 2]
+    for i in range(len(side_angles_list)):
+        side_angle = side_angles_list[i]
+        other_sides_sum = side_angles_list[i - 1] + side_angles_list[i - 2]
 
-        sides_angles_list[i], sides_angles_list[i - 2], sides_angles_list[i - 1]\
-            = sides_angles_list[i - 2], sides_angles_list[i - 1], sides_angles_list[i]
+        side_angles_list[i], side_angles_list[i - 2], side_angles_list[i - 1]\
+            = side_angles_list[i - 2], side_angles_list[i - 1], side_angles_list[i]
         # shift element left
         if side_angle >= other_sides_sum:
             result = False
             break
     return result
 
-#
+
 def main():
     a, b, c = [int(input()), int(input()), int(input())]
     print(is_valid_triangle(a, b, c))
