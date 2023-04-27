43bb0c57d20ca48a5b1df6450b6e9ec5d466ae56
sergero <roshchektaev@gmail.com>
2023-04-27 14:07:17 +0300

    Refactoring best practices

--- Is Valid Triangle_vers_1 .py
+++ Is Valid Triangle_vers_1 .py
@@ -1,14 +1,18 @@
 def is_valid_triangle(side_1, side_2, side_3):
-    while side_1 < (side_2 + side_3) and side_2 < (side_1 + side_3)\
-            and side_3 < (side_1 + side_2):
-        return True
-    else:
-        return False
-#
+    value = False
+    if side_1 < (side_2 + side_3) and\
+       side_2 < (side_1 + side_3) and\
+       side_3 < (side_1 + side_2):
+        value = True
+    return value
+
 
 def main():
-    a, b, c = [int(input()), int(input()), int(input())]
-    print(is_valid_triangle(a, b, c))
+    side_a, side_b, side_c = [int(input()), int(input()), int(input())]
+
+    is_valid = is_valid_triangle(side_a, side_b, side_c)
+
+    print(is_valid)
 
 
 main()
