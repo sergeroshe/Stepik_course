b7c9358816208ca199510ea3182d5f082283d6e8
sergero <roshchektaev@gmail.com>
2023-04-27 19:52:21 +0300

    Find method calls number reduced

--- Find_all.py
+++ Find_all.py
@@ -2,20 +2,23 @@ def find_all(source, symbol):
 
     symbol_idx_list = []
     count = 0
+    len_source = len(source)
 
-    while source.find(symbol) != -1:
-        symbol_idx = source.find(symbol)
-        symbol_idx_list.append(symbol_idx + count)
-        source = source.replace(symbol, '', 1)
-        count += 1
+    while count < len_source:
+        symbol_idx = source.find(symbol, count)
+        if symbol_idx == -1:
+            break
+        else:
+            symbol_idx_list.append(symbol_idx)
+            count = symbol_idx + 1
 
     return symbol_idx_list
 
 
 def main():
-    source_string = input()    # maybe source-string?
+    source_string = input()
     char = input()
-    symbol_occur_list = find_all(source_string, char)   # 'a'
+    symbol_occur_list = find_all(source_string, char)
     print(symbol_occur_list)
 
 
