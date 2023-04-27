5a43cb7be5555426c8c3472c1727406e9c28379b
sergero <roshchektaev@gmail.com>
2023-04-27 16:57:44 +0300

    Iteration number reduced

--- Find_all.py
+++ Find_all.py
@@ -1,32 +1,22 @@
-def find_all(target, symbol):
+def find_all(source, symbol):
+
     symbol_idx_list = []
-    for i in range(len(target)):
-        symbol_idx = target.find(symbol, i)
-        if target[i] == symbol:
-            symbol_idx_list.append(symbol_idx)
+    count = 0
+
+    while source.find(symbol) != -1:
+        symbol_idx = source.find(symbol)
+        symbol_idx_list.append(symbol_idx + count)
+        source = source.replace(symbol, '', 1)
+        count += 1
 
     return symbol_idx_list
-# use find, var.:target_string
 
 
 def main():
-    target_string = input()  # maybe source-string?
+    source_string = input()    # maybe source-string?
     char = input()
-    print(find_all(target_string, char))
+    symbol_occur_list = find_all(source_string, char)   # 'a'
+    print(symbol_occur_list)
 
 
 main()
-
-
-
-
-
-
-
-
-# Найти всех
-# Напомним, что строковый метод find('a') возвращает местоположение первого вхождения символа a в строке.
-# Проблема заключается в том, что данный метод не находит местоположение всех символов а.
-# Напишите функцию с именем find_all(target, symbol), которая принимает два аргумента:
-# строку target и символ symbol и возвращает список, содержащий все местоположения этого символа в строке.
-# Примечание 1. Если указанный символ не встречается в строке, то следует вернуть пустой список.
