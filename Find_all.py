74db377d0d0ecbda82800f50b1382a4c9dc74ca1
sergero <roshchektaev@gmail.com>
2023-04-30 16:41:37 +0300

    break op. removed. Value -1 in loop header used.

--- Find_all.py
+++ Find_all.py
@@ -1,25 +1,41 @@
 def find_all(source, symbol):
 
     symbol_idx_list = []
-    count = 0
-    len_source = len(source)
+    symbol_idx = 0
+    start_idx = 0
 
-    while count <= len_source - 1:
-        symbol_idx = source.find(symbol, count)
-        if symbol_idx == -1:
-            break
-        else:
+    while symbol_idx != -1:                 # remove break -1 in loop header
+        symbol_idx = source.find(symbol, start_idx)
+        if symbol_idx != -1:
             symbol_idx_list.append(symbol_idx)
-            count = symbol_idx + 1
+            start_idx = symbol_idx + 1
 
     return symbol_idx_list
 
 
 def main():
-    source_string = 'abc'
-    char = 'a'
+    source_string = input()
+    char = input()
     symbol_occur_list = find_all(source_string, char)
     print(symbol_occur_list)
 
 
 main()
+
+
+
+# def find_all(source, symbol):
+#
+#     symbol_idx_list = []
+#     start_idx = 0
+#     len_source = len(source)
+#
+#     while start_idx <= len_source - 1:
+#         symbol_idx = source.find(symbol, start_idx)
+#         if symbol_idx == -1:
+#             break
+#         else:
+#             symbol_idx_list.append(symbol_idx)
+#             start_idx = symbol_idx + 1
+#
+#     return symbol_idx_list
