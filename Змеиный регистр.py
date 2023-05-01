37c4dcc37a2731e4c4564d4080cab029d8c1f9b9
sergero <roshchektaev@gmail.com>
2023-05-01 19:59:54 +0300

    Join method proper usage applied, 'if' conditions number reduced

--- Змеиный регистр.py
+++ Змеиный регистр.py
@@ -3,16 +3,16 @@ SEP_CHAR = '_'
 
 def convert_to_python_case(text):
     word_list = []
-    start_word = 0
+    start_word_idx = 0
 
     for i in range(1, len(text)):
         string_letter = text[i]
         if string_letter.isupper():
-            end_word = i
-            word_list.append(text[start_word:end_word].lower())
-            start_word = end_word
+            end_word_idx = i
+            word_list.append(text[start_word_idx:end_word_idx].lower())
+            start_word_idx = end_word_idx
 
-    word_list.append(text[start_word:].lower())
+    word_list.append(text[start_word_idx:].lower())
     converted_string = SEP_CHAR.join(word_list)
 
     return converted_string
