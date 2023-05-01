8caee4e7faff18fbf430513f2544c69c150044b4
sergero <roshchektaev@gmail.com>
2023-05-01 19:58:31 +0300

    Join method proper usage applied, 'if' conditions number reduced

--- Змеиный регистр.py
+++ Змеиный регистр.py
@@ -5,22 +5,21 @@ def convert_to_python_case(text):
     word_list = []
     start_word = 0
 
-    for i in range(len(text)):
+    for i in range(1, len(text)):
         string_letter = text[i]
-        if i > 0:
-            if string_letter.isupper():
-                end_word = i
-                word_list.append(text[start_word:end_word].lower() + SEP_CHAR)
-                start_word = end_word
+        if string_letter.isupper():
+            end_word = i
+            word_list.append(text[start_word:end_word].lower())
+            start_word = end_word
 
     word_list.append(text[start_word:].lower())
-    converted_string = ''.join(word_list)
+    converted_string = SEP_CHAR.join(word_list)
 
     return converted_string
 
 
 def main():
-    txt = input()
+    txt = 'ThisIsCamelCased'
 
     converted_to_python_string = convert_to_python_case(txt)
 
