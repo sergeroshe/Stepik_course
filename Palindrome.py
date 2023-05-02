ef88274a5dede2243411a2e55d78f45ced495722
sergero <roshchektaev@gmail.com>
2023-05-02 11:54:48 +0300

    Useless i count deleted

--- Palindrome.py
+++ Palindrome.py
@@ -2,33 +2,30 @@ IGNORE_CHAR_LIST = ' ,/.!?-'
 
 
 def is_palindrome(sentence):
-    value = True                                   # move to constant
+    value = True
     len_s = len(sentence) - 1
     half_len = len(sentence) // 2
-    left_side_count = 0
-    right_side_count = 0
-    i = 0
+    left_side_idx = 0
+    right_side_idx = 0
 
-    while left_side_count <= half_len and right_side_count <= half_len:
-        left_side = sentence[left_side_count]
-        right_side = sentence[len_s - right_side_count]
-        if left_side not in IGNORE_CHAR_LIST and right_side not in IGNORE_CHAR_LIST:
-            left_side_count += 1
-            right_side_count += 1
-            if right_side != left_side:
+    while left_side_idx <= half_len and right_side_idx <= half_len:
+        left_side_element = sentence[left_side_idx]
+        right_side_element = sentence[len_s - right_side_idx]
+        if left_side_element not in IGNORE_CHAR_LIST and right_side_element not in IGNORE_CHAR_LIST:
+            left_side_idx += 1
+            right_side_idx += 1
+            if right_side_element != left_side_element:
                 value = False
                 break
-        elif left_side in IGNORE_CHAR_LIST:
-            left_side_count += 1
-        elif right_side in IGNORE_CHAR_LIST:
-            right_side_count += 1
+        elif left_side_element in IGNORE_CHAR_LIST:
+            left_side_idx += 1
+        elif right_side_element in IGNORE_CHAR_LIST:
+            right_side_idx += 1
 
-        i += 1
     return value
 
 
 def main():
-
     line = input().lower()
 
     is_line_palindrome = is_palindrome(line)
