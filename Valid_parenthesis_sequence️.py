ddf2ce91b70b64421a76115653018dc457758ff1
sergero <roshchektaev@gmail.com>
2023-05-02 12:36:23 +0300

    Removed useless variable

--- Valid_parenthesis_sequence️.py
+++ Valid_parenthesis_sequence️.py
@@ -1,29 +1,23 @@
 def is_correct_bracket(text):
-    opening_parenthesis = '('
-    closing_parenthesis = ')'
+    opening_parenthesis = '('      # move to constant
+    closing_parenthesis = ')'           # is_parenthesis_open
     count = 0
 
-    is_parenthesis_open = False
     value = False
     for char in text:
         if char == opening_parenthesis:
             count += 1
-            is_parenthesis_open = True
         elif char == closing_parenthesis:
             count -= 1
-            if count == 0:
-                is_parenthesis_open = False
         if count < 0:
-            is_parenthesis_open = True
             break
-    if not is_parenthesis_open:
+    if count == 0:
         value = True
 
     return value
 
 
 def main():
-
     txt = input()
 
     is_bracket_correct = is_correct_bracket(txt)
