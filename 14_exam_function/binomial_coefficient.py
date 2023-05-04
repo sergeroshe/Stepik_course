3d890c138a413d38980969a54a1f1df292c984a5
sergero <roshchektaev@gmail.com>
2023-05-04 18:47:33 +0300

    Initial commit: binomial_coefficient.py

---
+++ 14_exam_function/binomial_coefficient.py
@@ -0,0 +1,18 @@
+from math import factorial
+
+
+def compute_binom(n, k):
+    bin_coefficient = int(factorial(n) / (factorial(k) * factorial(n - k)))
+    return bin_coefficient
+
+
+def main():
+    n = int(input())
+    k = int(input())
+
+    bin_coefficient_value = compute_binom(n, k)
+
+    print(bin_coefficient_value)
+
+
+main()
