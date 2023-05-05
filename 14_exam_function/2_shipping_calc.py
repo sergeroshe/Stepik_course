613cb0a3f1e509317bcdaebc36408bb889901ada
sergero <roshchektaev@gmail.com>
2023-05-05 16:01:54 +0300

    Added constants to parameters

--- 14_exam_function/2_shipping_calc.py
+++ 14_exam_function/2_shipping_calc.py
@@ -2,15 +2,15 @@ FIRST_ITEM_PRICE = 1000
 SUBSEQUENT_ITEM_PRICE = 120
 
 
-def get_shipping_cost(quantity):
-    total_price = FIRST_ITEM_PRICE + (quantity - 1) * SUBSEQUENT_ITEM_PRICE
+def get_shipping_cost(quantity, first_item_price, subsequent_item_price):
+    total_price = first_item_price + (quantity - 1) * subsequent_item_price
     return total_price
 
 
 def main():
     item_amount = int(input())
 
-    shipping_cost = get_shipping_cost(item_amount)
+    shipping_cost = get_shipping_cost(item_amount, FIRST_ITEM_PRICE, SUBSEQUENT_ITEM_PRICE)
 
     print(shipping_cost)
 
