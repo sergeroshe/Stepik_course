dea40dbffb29ce487e28ff1b79320e64972991e5
sergero <roshchektaev@gmail.com>
2023-05-10 17:56:10 +0300

    imported random_module, added GREETING constant, added random_int variable

--- 15_mini_project_work/1_num_guessing_game.py
+++ 15_mini_project_work/1_num_guessing_game.py
@@ -1,9 +1,13 @@
+import random
+
+GREETING = 'Добро пожаловать в числовую угадайку'
 START_RANGE = 1
 
 
 def min_guaranteed_guess_count(start_range, end_range):
     middle = (start_range + end_range) // 2
     division_count = 0
+    rand_num = random.randint(1, 100)
 
     while middle != start_range:
         middle = (start_range + end_range) // 2
