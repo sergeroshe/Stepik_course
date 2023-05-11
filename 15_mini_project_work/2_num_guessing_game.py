70a612b9fcfef8fd2da3ef304123697f3056c000
sergero <roshchektaev@gmail.com>
2023-05-11 22:41:39 +0300

    Realised task of implying is_valid func in main

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -1,36 +1,39 @@
 import random
-GREETING = 'Добро пожаловать в числовую угадайку! \n'
+GREETING = 'Добро пожаловать в числовую угадайку!'
 GUESS_LEFT_BORDER = 1
-GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхний диапазон чисел: \n'
+GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите число от '
 VALID_LEFT_BORDER = 1
 VALID_RIGHT_BORDER = 100
-ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до 100?'
+ERROR_MESSAGE = "Ошибка ввода! Необходима ввести число от "
 
 
-def num_guessing_game(start_range, end_range):
-    middle = (start_range + end_range) // 2
+def num_guessing_game(left_border, right_border):
+    middle = (left_border + right_border) // 2
     division_count = 0
 
-    while middle != start_range:
-        middle = (start_range + end_range) // 2
+    while middle != left_border:
+        middle = (left_border + right_border) // 2
         division_count += 1
-        end_range = middle
+        right_border = middle
 
     return division_count
 
 
 def is_valid(input_string):
-    result = input_string.isdigit() and VALID_LEFT_BORDER < int(input_string) < VALID_RIGHT_BORDER
+    result = input_string.isdigit() and VALID_LEFT_BORDER < int(input_string) <= VALID_RIGHT_BORDER
     return result
 
 
 def main():
     print(GREETING)
-    right_border = int(input('Введите верхний числовой диапазон для игры \n'))
+    right_border = int(input(GUESS_RIGHT_BORDER_PROMPT_MESSAGE + str(VALID_LEFT_BORDER)
+                             + ' до ' + str(VALID_RIGHT_BORDER) + ': \n'))
+    if not is_valid(str(right_border)):
+        print(ERROR_MESSAGE + str(VALID_LEFT_BORDER) + ' до ' + str(VALID_RIGHT_BORDER))
+    else:
+        try_count = num_guessing_game(GUESS_LEFT_BORDER, right_border)
 
-    try_count = num_guessing_game(GUESS_LEFT_BORDER, right_border)
-
-    print(try_count)
+        print(try_count)
 
 
 main()
