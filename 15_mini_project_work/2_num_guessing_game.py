b2195143db909f6bd329bf32da8600803e712fd3
sergero <roshchektaev@gmail.com>
2023-05-11 17:09:25 +0300

    Added infinite while loop

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -1,9 +1,10 @@
 import random
 
 GUESS_LEFT_BORDER = 1
-GREETING = 'Добро пожаловать в числовую угадайку'
+GREETING = 'Добро пожаловать в числовую угадайку!'
 RANDOM_LEFT_BORDER = 1
 RANDOM_RIGHT_BORDER = 100
+ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до 100?'
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -26,6 +27,21 @@ def main():
     guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
     print(guaranteed_min_tries)
+
+
+def is_valid(input_string):
+    result = input_string.isdigit() and 1 < int(input_string) < 100
+    # return result
+
+    while result:
+        input_num = 0
+        input_value = input()
+        if not is_valid(input_value):
+            print(ERROR_MESSAGE)
+        else:
+            input_num = int(input_value)
+
+        print(input_num)
 
 
 main()
