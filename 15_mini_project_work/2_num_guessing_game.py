7c74d2b328be90601a11b609dd4df0a829e72af1
sergero <roshchektaev@gmail.com>
2023-05-11 21:41:24 +0300

    Renamed var. end_range to right_border

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -4,6 +4,7 @@ GUESS_LEFT_BORDER = 1
 GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхний диапазон чисел: \n'
 VALID_LEFT_BORDER = 1
 VALID_RIGHT_BORDER = 100
+ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до 100?'
 
 
 def num_guessing_game(start_range, end_range):
@@ -19,16 +20,15 @@ def num_guessing_game(start_range, end_range):
 
 
 def is_valid(input_string):
-    guess_right_border = int(input(GUESS_RIGHT_BORDER_PROMPT_MESSAGE))
     result = input_string.isdigit() and VALID_LEFT_BORDER < int(input_string) < VALID_RIGHT_BORDER
     return result
 
 
 def main():
     print(GREETING)
-    end_range = int(input())
+    right_border = int(input('Введите верхний числовой диапазон для игры \n'))
 
-    try_count = num_guessing_game(GUESS_LEFT_BORDER, end_range)
+    try_count = num_guessing_game(GUESS_LEFT_BORDER, right_border)
 
     print(try_count)
 
