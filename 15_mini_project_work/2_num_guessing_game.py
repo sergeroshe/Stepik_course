8b5d10998120e1cc8eb5fe516ab25a518440547e
sergero <roshchektaev@gmail.com>
2023-05-11 17:36:07 +0300

    Added is_valid func call

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -5,7 +5,7 @@ GREETING = 'Добро пожаловать в числовую угадайку
 RANDOM_LEFT_BORDER = 1
 RANDOM_RIGHT_BORDER = 100
 ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до 100?'
-GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхний диапазон чисел:'
+GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхний диапазон чисел: \n'
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -24,26 +24,23 @@ def main():
     print(GREETING)
     guess_right_border = int(input(GUESS_RIGHT_BORDER_PROMPT_MESSAGE))
 
+    def is_valid(input_string):
+        result = input_string.isdigit() and 1 < int(input_string) < 100
+        while result:
+            input_value = input()
+            if not is_valid(input_value):
+                print(ERROR_MESSAGE)
+            else:
+                input_num = int(input_value)
+
     rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
 
     guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
-    print(guaranteed_min_tries)
+    # print(guaranteed_min_tries)
 
-
-def is_valid(input_string):
-    result = input_string.isdigit() and 1 < int(input_string) < 100
     # return result
-
-    while result:
-        input_num = 0
-        input_value = input()
-        if not is_valid(input_value):
-            print(ERROR_MESSAGE)
-        else:
-            input_num = int(input_value)
-
-        print(input_num)
+    is_valid(input())
 
 
 main()
