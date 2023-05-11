160966de289324b40b402590c1574ae15e1d8af9
sergero <roshchektaev@gmail.com>
2023-05-11 13:42:12 +0300

    Extracted random params to consts

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -2,6 +2,8 @@ import random
 
 START_RANGE = 1
 GREETING = 'Добро пожаловать в числовую угадайку'
+RANDOM_LEFT_BORDER = 1
+RANDOM_RIGHT_BORDER = 100
 
 
 def min_guaranteed_guess_count(start_range, end_range):
@@ -19,7 +21,7 @@ def min_guaranteed_guess_count(start_range, end_range):
 def main():
     print(GREETING)
     end_range = int(input())
-    rand_num = random.randint(1, 100)
+    rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
 
     guaranteed_min_tries = min_guaranteed_guess_count(START_RANGE, end_range)
 
