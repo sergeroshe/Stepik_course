90b16aeee1e057434999ba62fe10689fb180dee0
sergero <roshchektaev@gmail.com>
2023-05-11 13:48:20 +0300

    Replaced range-like names with border-like names for consistency

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -1,29 +1,29 @@
 import random
 
-START_RANGE = 1
+GUESS_LEFT_BORDER = 1
 GREETING = 'Добро пожаловать в числовую угадайку'
 RANDOM_LEFT_BORDER = 1
 RANDOM_RIGHT_BORDER = 100
 
 
-def min_guaranteed_guess_count(start_range, end_range):
-    middle = (start_range + end_range) // 2
+def min_guaranteed_guess_count(left_border, right_border):
+    middle = (left_border + right_border) // 2
     division_count = 0
 
-    while middle != start_range:
-        middle = (start_range + end_range) // 2
+    while middle != left_border:
+        middle = (left_border + right_border) // 2
         division_count += 1
-        end_range = middle
+        right_border = middle
 
     return division_count
 
 
 def main():
     print(GREETING)
-    end_range = int(input())
+    guess_right_border = int(input())
     rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
 
-    guaranteed_min_tries = min_guaranteed_guess_count(START_RANGE, end_range)
+    guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
     print(guaranteed_min_tries)
 
