4bb39e74a4f381ad91aaca37c83dbb15a2f935d0
sergero <roshchektaev@gmail.com>
2023-05-12 12:36:29 +0300

    Added infinite loop

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -1,11 +1,13 @@
 import random
 
 GUESS_LEFT_BORDER = 1
-GREETING = 'Добро пожаловать в числовую угадайку! \n'
+GREETING = 'Добро пожаловать в числовую угадайку'
 RANDOM_LEFT_BORDER = 1
 RANDOM_RIGHT_BORDER = 100
-ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до'
-GUESS_RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхний диапазон чисел: \n'
+VALID_LEFT_BORDER = 1
+VALID_RIGHT_BORDER = 100
+ERROR_MESSAGE = 'А может быть все-таки введем целое число от 1 до 100?'
+PROMPT_MESSAGE = 'Введите число от 1 до 100'
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -21,29 +23,25 @@ def min_guaranteed_guess_count(left_border, right_border):
 
 
 def is_valid(input_string):
-    guess_right_border = int(input(GUESS_RIGHT_BORDER_PROMPT_MESSAGE))
-    result = input_string.isdigit() and 1 < int(input_string) < RANDOM_RIGHT_BORDER
+    result = input_string.isdigit() and VALID_LEFT_BORDER <= int(input_string) <= VALID_RIGHT_BORDER
     return result
+
+
+is_guess_wrong = True
+while is_guess_wrong:
+    guess = input()
+    if not is_valid(guess):
+        print(ERROR_MESSAGE)
 
 
 def main():
     print(GREETING)
-    result = is_valid(input_value)
-    while result:
-        input_value = input()
-        if not is_valid(input_value):
-            print(ERROR_MESSAGE + str(guess_right_border))
-        else:
-            input_num = int(input_value)
-    is_valid(input())
-
+    guess_right_border = int(input())
     rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
 
     guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
-    # print(guaranteed_min_tries)
-
-    # return result
+    print(guaranteed_min_tries)
 
 
 main()
