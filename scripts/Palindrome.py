a6e63678e571d0ac761f06f28e7f80c53efc4e67
sergero <roshchektaev@gmail.com>
2023-05-13 12:36:43 +0300

    Replaced IGNORE_CHAR_LIST with VALID_CHAR_LIST, added both sides not in VALID_CHAR_LIST checking

--- scripts/Palindrome.py
+++ scripts/Palindrome.py
@@ -7,25 +7,29 @@ def is_palindrome(sentence):
     while left_side_idx != right_side_idx:
         left_side_element = sentence[left_side_idx]
         right_side_element = sentence[right_side_idx]
-        if left_side_element not in IGNORE_CHAR_LIST and right_side_element not in IGNORE_CHAR_LIST:
+        if left_side_element in VALID_CHAR_LIST and right_side_element in VALID_CHAR_LIST:
             left_side_idx += 1
             right_side_idx -= 1
             if right_side_element != left_side_element:
                 value = False
                 break
-        elif left_side_element in IGNORE_CHAR_LIST:
+        elif left_side_element not in VALID_CHAR_LIST:
             left_side_idx += 1
-        elif right_side_element in IGNORE_CHAR_LIST:
+        elif right_side_element not in VALID_CHAR_LIST:
+            right_side_idx -= 1
+        else:
+            left_side_idx += 1
             right_side_idx -= 1
 
     return value
 
 
-IGNORE_CHAR_LIST = ' ,/.!?-'
+VALID_CHAR_LIST = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
 
 def main():
     line = input().lower()
+    # line = 'Карман, жена, но Какашкин - вор! О, Ковалева... Вела во коровник. Ша! Как она нежна! рак...'.lower()
 
     is_line_palindrome = is_palindrome(line)
 
