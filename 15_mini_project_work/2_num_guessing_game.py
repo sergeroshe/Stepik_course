024b8013688944916de9f7ffb8565fe40c1e1960
sergero <roshchektaev@gmail.com>
2023-05-15 20:35:02 +0300

    Added is_guessed() func

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -10,6 +10,10 @@ ERROR_MESSAGE = f'А может быть все-таки введем целое
                 f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}? \n'
 PROMPT_MESSAGE = f'Введите число от ' \
                  f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}: \n'
+TOO_SMALL_MESSAGE = 'Ваше число меньше загаданного, попробуйте еще разок'
+TOO_BIG_MESSAGE = 'Ваше число больше загаданного, попробуйте еще разок'
+WIN_MESSAGE = 'Вы угадали, поздравляем!'
+FAREWALL_MESSAGE = 'Спасибо, что играли в числовую угадайку. Еще увидимся...'
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -29,17 +33,28 @@ def is_valid(input_string):
     return result
 
 
-def main():
-    print(GREETING)
-
+def is_guessed():
+    answer = ''
     is_guess_wrong = True
+    rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
     while is_guess_wrong:
         guess = input(PROMPT_MESSAGE)
         if not is_valid(guess):
             print(ERROR_MESSAGE)
         else:
             guess_num = int(guess)
-            is_guess_wrong = False
+            if guess_num == rand_num:
+                is_guess_wrong = False
+                answer = WIN_MESSAGE
+            elif guess_num < rand_num:
+                answer = TOO_SMALL_MESSAGE
+            else:
+                answer = TOO_BIG_MESSAGE
+    return answer
+
+
+def main():
+    print(GREETING)
 
     # while True:
     #     guess = input(PROMPT_MESSAGE)
@@ -50,7 +65,6 @@ def main():
     #         break
 
     guess_right_border = int(input())
-    rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
 
     guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
