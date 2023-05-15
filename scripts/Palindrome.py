0e5e342e0fe6cd7a62c8ea94b553d0e05b6e95ae
sergero <roshchektaev@gmail.com>
2023-05-15 12:51:07 +0300

    Renamed sentence to text

--- scripts/Palindrome.py
+++ scripts/Palindrome.py
@@ -1,14 +1,14 @@
 IGNORE_CHAR_LIST = ' .,-_:!@#$%^&?'
 
 
-def is_palindrome(sentence):
+def is_palindrome(text):
     result = True
     left_side_idx = 0
-    right_side_idx = len(sentence) - 1
+    right_side_idx = len(text) - 1
 
     while left_side_idx != right_side_idx:
-        left_side_element = sentence[left_side_idx]
-        right_side_element = sentence[right_side_idx]
+        left_side_element = text[left_side_idx]
+        right_side_element = text[right_side_idx]
         is_left_side_letter = left_side_element not in IGNORE_CHAR_LIST
         is_right_side_letter = right_side_element not in IGNORE_CHAR_LIST
 
