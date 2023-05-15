bacd6cbf01319c65503e32f224e97ec287c90d7b
sergero <roshchektaev@gmail.com>
2023-05-15 21:13:30 +0300

    Renamed result var to is_same_letter

--- scripts/Palindrome.py
+++ scripts/Palindrome.py
@@ -2,11 +2,11 @@ IGNORE_CHAR_LIST = ' .,-_:!@#$%^&?'
 
 
 def is_palindrome(text):
-    result = True
+    is_same_letter = True
     left_side_idx = 0
     right_side_idx = len(text) - 1
 
-    while left_side_idx != right_side_idx and result:
+    while left_side_idx != right_side_idx and is_same_letter:
         left_side_char = text[left_side_idx]
         right_side_char = text[right_side_idx]
         is_left_side_letter = left_side_char not in IGNORE_CHAR_LIST
@@ -14,7 +14,7 @@ def is_palindrome(text):
 
         if is_left_side_letter and is_right_side_letter:
             if left_side_char != right_side_char:
-                result = False
+                is_same_letter = False
             else:
                 left_side_idx += 1
                 right_side_idx -= 1
@@ -25,7 +25,7 @@ def is_palindrome(text):
             if not is_right_side_letter:
                 right_side_idx -= 1
 
-    return result
+    return is_same_letter
 
 
 def main():
