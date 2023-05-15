98d597e0410cf5e9ae1174b8ff6db26ed3b5cc0e
sergero <roshchektaev@gmail.com>
2023-05-15 11:08:12 +0300

    Renamed left_side_letter to is_left_side_letter var., removed unnecessary calculation in 19 - 27 strings.

--- scripts/Palindrome.py
+++ scripts/Palindrome.py
@@ -9,28 +9,30 @@ def is_palindrome(sentence):
     while left_side_idx != right_side_idx:
         left_side_element = sentence[left_side_idx]
         right_side_element = sentence[right_side_idx]
-        left_side_letter = left_side_element not in IGNORE_CHAR_LIST
-        right_side_letter = right_side_element not in IGNORE_CHAR_LIST
-        if left_side_letter and right_side_letter:
-            left_side_idx += 1
-            right_side_idx -= 1
+        is_left_side_letter = left_side_element not in IGNORE_CHAR_LIST
+        is_right_side_letter = right_side_element not in IGNORE_CHAR_LIST
+
+        if is_left_side_letter and is_right_side_letter:
             if left_side_element != right_side_element:
                 result = False
                 break
-        elif not left_side_letter and not right_side_letter:
-            left_side_idx += 1
-            right_side_idx -= 1
-        elif not left_side_letter and right_side_letter:
-            left_side_idx += 1
-        elif not right_side_letter and left_side_letter:
-            right_side_idx -= 1
+            else:
+                left_side_idx += 1
+                right_side_idx -= 1
+        else:
+            if not is_left_side_letter:
+                left_side_idx += 1
+
+            if not is_right_side_letter:
+                right_side_idx -= 1
 
     return result
 
 
 def main():
     line = input().lower()
-    # line = ' Do me?.. Kill I victim? Must summit civil like mod.'.lower()
+    # line = 'Карман, жена, но Какашкин - вор! ' \
+    #        'О, Ковалева... Вела во коровник. Ша! Как она нежна! рак...'.lower()
 
     is_line_palindrome = is_palindrome(line)
 
