f249e0b86cb5bbfcfd05a3b94f29937aa3b81118
sergero <roshchektaev@gmail.com>
2023-05-16 19:29:48 +0300

    Initial commit of the feature of comparing an entered num with the hidden one. Added entered num evaluation based consts, amended main program while loop with conditions.

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -6,6 +6,9 @@ RANDOM_LEFT_BORDER = 1
 RANDOM_RIGHT_BORDER = 100
 VALID_LEFT_BORDER = 1
 VALID_RIGHT_BORDER = 100
+TOO_SMALL_MESSAGE = 'Ваше число меньше загаданного, попробуйте еще разок'
+TOO_BIG_MESSAGE = 'Ваше число больше загаданного, попробуйте еще разок'
+WIN_MESSAGE = 'Вы угадали, поздравляем!'
 ERROR_MESSAGE = f'А может быть все-таки введем целое число от ' \
                 f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}? \n'
 PROMPT_MESSAGE = f'Введите число от ' \
@@ -33,20 +36,26 @@ def main():
     print(GREETING)
 
     is_guess_wrong = True
+    rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
     while is_guess_wrong:
         guess = input(PROMPT_MESSAGE)
         if not is_valid(guess):
             print(ERROR_MESSAGE)
         else:
             guess_num = int(guess)
-            is_guess_wrong = False
+            if guess_num < rand_num:
+                print(TOO_SMALL_MESSAGE)
+            elif guess_num > rand_num:
+                print(TOO_BIG_MESSAGE)
+            else:
+                print(f'{WIN_MESSAGE} \n')
+                is_guess_wrong = False
 
-    guess_right_border = int(input())
-    rand_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
+    # guess_right_border = int(input())
 
-    guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
+    # guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
-    print(guaranteed_min_tries)
+    # print(guaranteed_min_tries)
 
 
 main()
