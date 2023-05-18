4f7b4af2908221293d389bf4aa8392ed1ce4ec34
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-05-18 13:35:23 +0300

    15_2_6_1 Added number of user attempts (#49)
    * Added number of user attempts.
    
    * Added sep param to guess_count output.
    
    * Farewell message output moved to final output.

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -14,6 +14,7 @@ ERROR_MESSAGE = f'А может быть все-таки введем целое
 PROMPT_MESSAGE = f'Введите число от ' \
                  f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}: \n'
 FAREWELL_MESSAGE = 'Спасибо, что играли в числовую угадайку. Еще увидимся...'
+GUESS_NUMBER_MESSAGE = 'Число сделанных вами попыток:'
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -38,6 +39,8 @@ def main():
 
     is_guess_wrong = True
     hidden_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
+    guess_count = 1
+
     while is_guess_wrong:
         guess = input(PROMPT_MESSAGE)
         if not is_valid(guess):
@@ -50,12 +53,15 @@ def main():
                 print(TOO_BIG_MESSAGE)
             else:
                 print(WIN_MESSAGE)
+                print(GUESS_NUMBER_MESSAGE, guess_count, sep='\n')
                 print(FAREWELL_MESSAGE)
+
                 is_guess_wrong = False
+        guess_count += 1
+
 
     # guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
 
     # print(guaranteed_min_tries)
-
 
 main()
