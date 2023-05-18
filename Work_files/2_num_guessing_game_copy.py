ce61fd38083b7475d9a5007c842dc09a2e44abc3
sergero <roshchektaev@gmail.com>
2023-05-18 18:53:06 +0300

    Added guess left border option

--- Work_files/2_num_guessing_game_copy.py
+++ Work_files/2_num_guessing_game_copy.py
@@ -3,6 +3,7 @@ import random
 YES_RESPONSE = 'д'
 GREETING = 'Добро пожаловать в числовую угадайку'
 GUESS_LEFT_BORDER = 1
+LEFT_BORDER_PROMPT_MESSAGE = 'Введите нижнюю границу числового диапазона: \n'
 RIGHT_BORDER_PROMPT_MESSAGE = 'Введите верхнюю границу числового диапазона: \n'
 TOO_SMALL_MESSAGE = 'Ваше число меньше загаданного, попробуйте еще разок'
 TOO_BIG_MESSAGE = 'Ваше число больше загаданного, попробуйте еще разок'
@@ -24,8 +25,8 @@ def min_guaranteed_guess_count(left_border, right_border):
     return division_count
 
 
-def is_valid(input_string, guess_right_border):
-    result = input_string.isdigit() and GUESS_LEFT_BORDER <= int(input_string) <= guess_right_border
+def is_valid(input_string, guess_left_border, guess_right_border):
+    result = input_string.isdigit() and guess_left_border <= int(input_string) <= guess_right_border
     return result
 
 
@@ -34,19 +35,20 @@ def main():
 
     game_is_going_on = True
     while game_is_going_on:
+        guess_left_border = int(input(LEFT_BORDER_PROMPT_MESSAGE))
         guess_right_border = int(input(RIGHT_BORDER_PROMPT_MESSAGE))
-        hidden_num = random.randint(GUESS_LEFT_BORDER, guess_right_border)
+        hidden_num = random.randint(guess_left_border, guess_right_border)
         prompt_message = f'Введите число от ' \
-                         f'{GUESS_LEFT_BORDER} до {guess_right_border}: \n'
+                         f'{guess_left_border} до {guess_right_border}: \n'
         error_message = f'А может быть все-таки введем целое число от ' \
-                        f'{GUESS_LEFT_BORDER} до {guess_right_border}? \n'
+                        f'{guess_left_border} до {guess_right_border}? \n'
         guess_count = 1
 
         is_guess_wrong = True
         while is_guess_wrong:
             guess = input(prompt_message)
 
-            if not is_valid(guess, guess_right_border):
+            if not is_valid(guess, guess_left_border, guess_right_border):
                 print(error_message)
             else:
                 guess_num = int(guess)
@@ -60,7 +62,7 @@ def main():
             guess_count += 1
         new_game_wish = input(NEW_GAME_PROPOSAL_MESSAGE).lower()
         if new_game_wish != YES_RESPONSE:
-            print(GUESS_NUMBER_MESSAGE, guess_count, sep='\n')
+            print(GUESS_NUMBER_MESSAGE, guess_count - 1, sep='\n')
             print(FAREWELL_MESSAGE)
             game_is_going_on = False
 
