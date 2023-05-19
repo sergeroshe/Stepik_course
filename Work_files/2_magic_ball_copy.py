89a2a88811e29c3edb606020c9503d75ea961af5
sergero <roshchektaev@gmail.com>
2023-05-19 12:56:57 +0300

    Added Main program cycle with consts.

--- Work_files/2_magic_ball_copy.py
+++ Work_files/2_magic_ball_copy.py
@@ -1,7 +1,12 @@
-import random
+from random import choice
 
 GREETING = 'Привет Мир, я Магический Шар, и я знаю ответ на любой твой вопрос.'
 USER_NAME_PROMPT = 'Введите ваше имя:\n'
+ENTER_QUESTION_PROMPT = 'Введите ваш вопрос:\n'
+FAREWELL_MESSAGE = 'Возвращайся если возникнут вопросы!'
+CONTINUE_GAME_PROMPT = 'Хотите задать еще один вопрос?\nНажмите "1", затем ENTER,' \
+                       ' если ДА\nНажмите любую клавишу, затем ENTER, если НЕТ\n'
+YES_RESPONSE = '1'
 
 ANSWERS = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определённо да', 'Можешь быть уверен в этом',
            'Мне кажется - да', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят - да', 'Да',
@@ -14,6 +19,15 @@ def main():
     print(GREETING)
 
     user_name = input(USER_NAME_PROMPT)
+    game_is_on = True
+    while game_is_on:
+        question = input(ENTER_QUESTION_PROMPT)
+        answer = choice(ANSWERS)
+        print(answer)
+        continue_game = input(CONTINUE_GAME_PROMPT)
+        if continue_game != YES_RESPONSE:
+            game_is_on = False
+    print(FAREWELL_MESSAGE)
 
 
 main()
