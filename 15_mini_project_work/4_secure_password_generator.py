5cad9c0760160c0e321be57aca0d0bcc78c17c3d
sergero <roshchektaev@gmail.com>
2023-05-22 12:34:52 +0300

    Added: consts with password elements, chars var.

--- 15_mini_project_work/4_secure_password_generator.py
+++ 15_mini_project_work/4_secure_password_generator.py
@@ -1,7 +1,13 @@
 import random
 
-def secure_password_generator:
-    pass
+DIGITS = '0123456789'
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PUNCTUATION = '!#$%&*+-=?@^_.'
+
+
+def secure_password_generator():
+    chars = ''
 
 
 def main():
