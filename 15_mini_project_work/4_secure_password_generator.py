26d7000fbdbf6257018ffc0da1c6034fa9825d8a
sergero <roshchektaev@gmail.com>
2023-05-22 15:46:13 +0300

    Renamed vars to be more relevant to their content.

--- 15_mini_project_work/4_secure_password_generator.py
+++ 15_mini_project_work/4_secure_password_generator.py
@@ -13,11 +13,12 @@ def secure_password_generator():
 
 
 passwd_num = int(input())  # Количество паролей для генерации;
-passwd_len = int(input())  # Длину одного пароля;
-passwd_has_digits = True  # Включать ли цифры 0123456789?
-passwd_has_cap_letters = True  # Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
-passwd_has_low_letters = True  # Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
-passwd_has_spec_chars = True  # Включать ли символы !#$%&*+-=?@^_?
+passwd_len = int(input())
+# Длину одного пароля;
+has_passwd_digits = True  # Включать ли цифры 0123456789?
+has_passwd_cap_letters = True  # Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
+has_passwd_low_letters = True  # Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
+has_passwd_spec_chars = True  # Включать ли символы !#$%&*+-=?@^_?
 are_ambiguous_chars_exluded = True  # Исключать ли неоднозначные символы il1Lo0O?
 
 
