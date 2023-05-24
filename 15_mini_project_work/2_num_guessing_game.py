568174bd0a73dd595c9253a848b53d92f7fcea7d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-05-24 18:17:34 +0300

    15_2_6 Dynamic right border (#55)
    * Added RIGHT_BORDER_PROMPT_MESSAGE
    
    * Added guess_right_border param to main and is_valid funcs
    
    * Changed const RANDOM_RIGHT_BORDER to var. Changed related params in funcs. Added COLON_SEP const.
    
    * Moved error message text and related separators to consts.
    
    * Added empty line.
    
    * Fixed logical mistake in is_valid func params
    
    * Fixed mistakes in prompt outputs
    
    * Imported only needed functionality
    
    * Moved calculations out of loop. Renamed consts.

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -1,20 +1,17 @@
-import random
+from random import randint
 
 GUESS_LEFT_BORDER = 1
-GREETING = 'Добро пожаловать в числовую угадайку'
-RANDOM_LEFT_BORDER = 1
-RANDOM_RIGHT_BORDER = 100
-VALID_LEFT_BORDER = 1
-VALID_RIGHT_BORDER = 100
+GREETING = 'Добро пожаловать в числовую угадайку!'
 TOO_SMALL_MESSAGE = 'Ваше число меньше загаданного, попробуйте еще разок'
 TOO_BIG_MESSAGE = 'Ваше число больше загаданного, попробуйте еще разок'
 WIN_MESSAGE = 'Вы угадали, поздравляем!'
-ERROR_MESSAGE = f'А может быть все-таки введем целое число от ' \
-                f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}? \n'
-PROMPT_MESSAGE = f'Введите число от ' \
-                 f'{VALID_LEFT_BORDER} до {VALID_RIGHT_BORDER}: \n'
+ENTER_NUM_PROMPT = 'Введите число от '
+RIGHT_BORDER_PROMPT = 'Введите верхнюю границу числового диапазона: \n'
 FAREWELL_MESSAGE = 'Спасибо, что играли в числовую угадайку. Еще увидимся...'
 GUESS_NUMBER_MESSAGE = 'Число сделанных вами попыток:'
+ERROR_OUT_OF_GUESS_MESSAGE = 'А может быть все-таки введем целое число от '
+COLON_SEP = ':'
+UP_TO_SEP = ' до '
 
 
 def min_guaranteed_guess_count(left_border, right_border):
@@ -29,8 +26,8 @@ def min_guaranteed_guess_count(left_border, right_border):
     return division_count
 
 
-def is_valid(input_string):
-    result = input_string.isdigit() and VALID_LEFT_BORDER <= int(input_string) <= VALID_RIGHT_BORDER
+def is_valid(input_string, guess_right_border):
+    result = input_string.isdigit() and GUESS_LEFT_BORDER <= int(input_string) <= guess_right_border
     return result
 
 
@@ -38,13 +35,18 @@ def main():
     print(GREETING)
 
     is_guess_wrong = True
-    hidden_num = random.randint(RANDOM_LEFT_BORDER, RANDOM_RIGHT_BORDER)
     guess_count = 1
 
+    guess_right_border = int(input(RIGHT_BORDER_PROMPT))
+    hidden_num = randint(GUESS_LEFT_BORDER, guess_right_border)
+    error_message = f'{ERROR_OUT_OF_GUESS_MESSAGE}{GUESS_LEFT_BORDER}{UP_TO_SEP}{guess_right_border}? \n'
+    guess_prompt = f'{ENTER_NUM_PROMPT}{GUESS_LEFT_BORDER}{UP_TO_SEP}{guess_right_border}{COLON_SEP} \n'
+
     while is_guess_wrong:
-        guess = input(PROMPT_MESSAGE)
-        if not is_valid(guess):
-            print(ERROR_MESSAGE)
+        guess = input(guess_prompt)
+
+        if not is_valid(guess, guess_right_border):
+            print(error_message)
         else:
             guess_num = int(guess)
             if guess_num < hidden_num:
@@ -59,9 +61,8 @@ def main():
                 is_guess_wrong = False
         guess_count += 1
 
-
     # guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
-
     # print(guaranteed_min_tries)
+
 
 main()
