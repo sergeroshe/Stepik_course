a440f6951d9c1cff7a3f18c04c11826ad331fdea
sergero <roshchektaev@gmail.com>
2023-05-25 21:42:57 +0300

    Added str consts.

---
+++ 15_mini_project_work/secure_passwd_gen.py
@@ -0,0 +1,17 @@
+import random
+
+DIGITS = '0123456789'
+CAP_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+PUNCTUATION = '!#$%&*+-=?@^_.'
+
+
+chars = ''
+
+
+def main():
+    pass
+
+
+main()
+
