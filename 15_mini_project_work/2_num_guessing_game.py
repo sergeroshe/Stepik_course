56aeb26cdc54821d3aafa432e5e579fa23279706
sergero <roshchektaev@gmail.com>
2023-05-25 23:08:57 +0300

    Removed invalid TOTAL_GUESS_NUMBER_MESSAGE output.

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -10,7 +10,7 @@ ENTER_NUM_PROMPT = 'Введите число от '
 RIGHT_BORDER_PROMPT = 'Введите верхнюю границу числового диапазона: \n'
 FAREWELL_MESSAGE = 'Спасибо, что играли в числовую угадайку. Еще увидимся...'
 GUESS_NUMBER_MESSAGE = 'Число сделанных вами попыток:'
-TOTAL_GUESS_NUMBER_MESSAGE = 'Общее число сделанных вами попыток:'
+# GAME_NUMBER_MESSAGE = 'Общее число сделанных вами попыток:'
 ERROR_OUT_OF_GUESS_MESSAGE = 'А может быть все-таки введем целое число от '
 NEW_GAME_PROPOSAL_MESSAGE = 'Хотите сыграть еще? \nНажмите: "1", затем: ENTER, ' \
                             'если ДА\nНажмите любую клавишу, затем: ENTER, если НЕТ\n'
@@ -69,8 +69,6 @@ def main():
                     is_guess_wrong = False
         new_game_wish = input(NEW_GAME_PROPOSAL_MESSAGE).lower()
         if new_game_wish != YES_RESPONSE:
-            print(guess_count, sep='\n')
-            print(TOTAL_GUESS_NUMBER_MESSAGE, guess_count, sep='\n')
             game_is_going_on = False
     print(FAREWELL_MESSAGE)
     # guaranteed_min_tries = min_guaranteed_guess_count(GUESS_LEFT_BORDER, guess_right_border)
