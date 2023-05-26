131ccfbfe807e51d3f4e1958401924b7c8c00cf9
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-05-26 14:29:30 +0300

    15_4_1 String constants (#61)
    * Added str consts.
    
    * Renamed constant names better way.

---
+++ 15_mini_project_work/secure_passwd_gen.py
@@ -0,0 +1,17 @@
+import random
+
+DIGITS = '0123456789'
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+PUNCTUATION_CHARS = '!#$%&*+-=?@^_.'
+
+
+chars = ''
+
+
+def main():
+    pass
+
+
+main()
+
