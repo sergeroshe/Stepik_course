d22afd3dc8962c33b857ea1f66bfaceb9ba3e014
sergero <roshchektaev@gmail.com>
2023-06-01 17:58:30 +0300

    Added impossible question option processing.

--- 15_mini_project_work/3_magic_ball_game.py
+++ 15_mini_project_work/3_magic_ball_game.py
@@ -11,6 +11,10 @@ USER_NAME_GREETING = 'Добро пожаловать, '
 CONTINUE_GAME_PROMPT = 'Хочешь задать еще один вопрос?\nНажми ENTER,' \
                        ' если ДА\n"1", затем ENTER, если НЕТ\n'
 ENTER_QUESTION_PROMPT = 'Введи свой вопрос:\n'
+IGNORE_QUESTION_LIST = ['где', 'когда', 'почему', 'сколько', 'кто',
+                        'с кем', 'кого', 'зачем', 'почему', 'чего',
+                        'как', 'какой', 'каком', 'какую', 'какие', 'какого']
+IMPOSSIBLE_QUESTION_ERROR = 'Я могу ответить только на вопрос, требующий ответа ДА или НЕТ'
 NO_RESPONSE = '1'
 FAREWELL_MESSAGE = 'Возвращайся если возникнут вопросы!'
 
@@ -19,6 +23,19 @@ def game_run():
     game_is_on = True
     while game_is_on:
         question = input(ENTER_QUESTION_PROMPT).lower()
+        question_is_not_correct = True
+        while question_is_not_correct:
+            is_question_impossible = False
+            for word in IGNORE_QUESTION_LIST:
+                if word in question:
+                    is_question_impossible = True
+            if is_question_impossible:
+                print(IMPOSSIBLE_QUESTION_ERROR)
+            if not is_question_impossible:
+                question_is_not_correct = False
+            else:
+                question = input(ENTER_QUESTION_PROMPT).lower()
+
         answer = choice(ANSWERS)
         print(answer)
 
