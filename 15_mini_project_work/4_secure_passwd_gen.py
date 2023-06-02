80013bb81b0159c9b178f0bb79fd114a37b45469
sergero <roshchektaev@gmail.com>
2023-06-02 11:00:20 +0300

    Initial commit 4_secure_passwd_gen.py

---
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -0,0 +1,16 @@
+import random
+
+DIGITS = '0123456789'
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+PUNCTUATION_CHARS = '!#$%&*+-=?@^_.'
+
+
+chars = ''
+
+
+def main():
+    pass
+
+
+main()
