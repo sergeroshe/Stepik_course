7745fdd451b63ca833ae64ecaa45273ae3bedac3
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-07 15:00:56 +0300

    15_4_3 Customization of generated passwords (#106)
    * Added AMBIGUOUS_CHARS const.
    
    * Added 'chars' var. configuration.
    
    * Simplified getting config
    
    * Deleted get_password_config func
    
    * Renamed passwd_num to passwd_count
    
    * Code style fix
    
    * Code style fix

--- 15_mini_project_work/4_secure_passwd_gen.py
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -4,37 +4,42 @@ DIGITS = '0123456789'
 UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 PUNCTUATION_CHARS = '!#$%&*+-=?@^_.'
-PASSWD_NUM_PROMPT = 'Количество паролей для генерации: \n'
+AMBIGUOUS_CHARS = 'il1Lo0O'
+PASSWD_COUNT_PROMPT = 'Количество паролей для генерации: \n'
 CUR_PASSWD_LEN_PROMPT = 'Введите длину пароля '
 NO_RESPONSE = '1'
-INCLUDE_NUM_PROMPT = 'Включать ли цифры 0123456789?\n'
-INCLUDE_CAP_LETTERS_PROMPT = 'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n'
-INCLUDE_SMALL_LETTERS_PROMPT = 'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?\n'
+INCLUDE_NUMBERS_PROMPT = 'Включать ли цифры 0123456789?\n'
+INCLUDE_UPPER_LETTERS_PROMPT = 'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n'
+INCLUDE_LOWER_LETTERS_PROMPT = 'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?\n'
 INCLUDE_PUNCT_PROMPT = 'Включать ли символы !#$%&*+-=?@^_?\n'
-EXCLUDE_AMBIG_CHARS_PROMPT = 'Исключать ли неоднозначные символы il1Lo0O?\n'
-
-
-def get_password_config():
-    passwd_num = int(input(PASSWD_NUM_PROMPT))
-    cur_passwd_len = int(input(CUR_PASSWD_LEN_PROMPT))
-    answer = input(INCLUDE_NUM_PROMPT)
-    is_num_included = answer != NO_RESPONSE
-    answer = input(INCLUDE_CAP_LETTERS_PROMPT)
-    is_cap_letter_included = answer != NO_RESPONSE
-    answer = input(INCLUDE_SMALL_LETTERS_PROMPT)
-    is_small_letter_included = answer != NO_RESPONSE
-    answer = input(INCLUDE_PUNCT_PROMPT)
-    is_punctuation_included = answer != NO_RESPONSE
-    answer = input(EXCLUDE_AMBIG_CHARS_PROMPT)
-    is_ambig_chars_excluded = answer != NO_RESPONSE
-
-    return passwd_num, cur_passwd_len, is_num_included, is_cap_letter_included, is_small_letter_included, \
-        is_punctuation_included, is_ambig_chars_excluded
+EXCLUDE_AMBIGUOUS_CHARS_PROMPT = 'Исключать ли неоднозначные символы il1Lo0O?\n'
 
 
 def secure_password_generator():
     chars = ''
-    get_password_config()
+
+    passwd_count = int(input(PASSWD_COUNT_PROMPT))
+    cur_passwd_len = int(input(CUR_PASSWD_LEN_PROMPT))
+
+    numbers_included = input(INCLUDE_NUMBERS_PROMPT) != NO_RESPONSE
+    upper_letters_included = input(INCLUDE_UPPER_LETTERS_PROMPT) != NO_RESPONSE
+    lower_letters_included = input(INCLUDE_LOWER_LETTERS_PROMPT) != NO_RESPONSE
+    punctuation_included = input(INCLUDE_PUNCT_PROMPT) != NO_RESPONSE
+    ambiguous_chars_excluded = input(EXCLUDE_AMBIGUOUS_CHARS_PROMPT) != NO_RESPONSE
+
+    for _ in range(passwd_count):
+        if numbers_included:
+            chars += DIGITS
+        if upper_letters_included:
+            chars += UPPER_LETTERS
+        if lower_letters_included:
+            chars += LOWER_LETTERS
+        if punctuation_included:
+            chars += PUNCTUATION_CHARS
+        if ambiguous_chars_excluded:
+            for c in chars:
+                if c in AMBIGUOUS_CHARS:
+                    chars = chars.replace(c, '')
 
 
 def main():
