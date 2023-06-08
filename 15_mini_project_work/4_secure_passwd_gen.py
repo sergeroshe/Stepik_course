db6b189902742af72003c1ed7279cdfcaeae2604
sergero <roshchektaev@gmail.com>
2023-06-08 14:24:34 +0300

    Fixed failing app when entering empty line

--- 15_mini_project_work/4_secure_passwd_gen.py
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -22,8 +22,12 @@ def get_num_input(prompt, error_message):
     is_string_num = False
     input_string = input(prompt)
     while not is_string_num:
-        if input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
-            is_string_num = True
+        if len(input_string) != 0:
+            if input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+                is_string_num = True
+            else:
+                print(error_message)
+                input_string = input(prompt)
         else:
             print(error_message)
             input_string = input(prompt)
