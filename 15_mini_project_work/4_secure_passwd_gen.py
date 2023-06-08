0570090fcd59ec7bda78f505a579787ead54a251
sergero <roshchektaev@gmail.com>
2023-06-08 14:44:34 +0300

    Simplified if condition in get_num_input func

--- 15_mini_project_work/4_secure_passwd_gen.py
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -22,12 +22,9 @@ def get_num_input(prompt, error_message):
     is_string_num = False
     input_string = input(prompt)
     while not is_string_num:
-        if len(input_string) != 0:
-            if input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
-                is_string_num = True
-            else:
-                print(error_message)
-                input_string = input(prompt)
+        if len(input_string) != 0 and input_string[0] == '-' and\
+               input_string[1:].isdigit() or input_string.isdigit():
+            is_string_num = True
         else:
             print(error_message)
             input_string = input(prompt)
