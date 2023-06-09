32c86defc553ff5e281bdd03dbdc4a9d39ea0749
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-09 13:08:41 +0300

    15_4_6 Failing app when entering empty line bug fix (#115)
    * Fixed failing app when entering empty line
    
    * Simplified if condition in get_num_input func
    
    * Simplified if condition in get_num_input func with bool expr implementing
    
    * Simplified empty input string handling

--- 15_mini_project_work/4_secure_passwd_gen.py
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -22,7 +22,7 @@ def get_num_input(prompt, error_message):
     is_string_num = False
     input_string = input(prompt)
     while not is_string_num:
-        if input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+        if input_string and input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
             is_string_num = True
         else:
             print(error_message)
