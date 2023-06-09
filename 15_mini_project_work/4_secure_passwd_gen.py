e426e26092af3c369c25565a27a7f639ba085ceb
sergero <roshchektaev@gmail.com>
2023-06-09 19:13:38 +0300

    Removed unnecessary loop in secure_password_configurator func

--- 15_mini_project_work/4_secure_passwd_gen.py
+++ 15_mini_project_work/4_secure_passwd_gen.py
@@ -5,20 +5,24 @@ UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 PUNCTUATION_CHARS = '!#$%&*+-=?@^_.'
 AMBIGUOUS_CHARS = 'il1Lo0O'
+
 PASSWD_COUNT_PROMPT = 'Количество паролей для генерации: \n'
-CUR_PASSWD_LEN_PROMPT = 'Введите длину пароля:\n '
+CUR_PASSWD_LEN_PROMPT = 'Введите длину пароля:\n'
+INCLUDE_DIGITS_PROMPT = 'Включать ли цифры 0123456789?'
+INCLUDE_UPPER_LETTERS_PROMPT = 'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?'
+INCLUDE_LOWER_LETTERS_PROMPT = 'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?'
+INCLUDE_PUNCT_PROMPT = 'Включать ли символы !#$%&*+-=?@^_?'
+EXCLUDE_AMBIGUOUS_CHARS_PROMPT = 'Исключать ли неоднозначные символы il1Lo0O?'
+
 TYPE_ERROR_MESSAGE = 'Введенные данные должны быть числовыми!'
 NO_RESPONSE = '1'
-PASSWD_COUNT_MESSAGE = 'Количество паролей:\n'
+PASSWD_COUNT_MESSAGE = 'Количество паролей:'
+PASSWD_LEN_MESSAGE = 'Длина пароля:'
 POSITIVE_ACTION_CONFIRM = 'ДА'
 NEGATIVE_ACTION_CONFIRM = 'НЕТ'
-INCLUDE_NUMBERS_PROMPT = 'Включать ли цифры 0123456789?\n'
-INCLUDE_UPPER_LETTERS_PROMPT = 'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n'
-INCLUDE_LOWER_LETTERS_PROMPT = 'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?\n'
-INCLUDE_PUNCT_PROMPT = 'Включать ли символы !#$%&*+-=?@^_?\n'
-EXCLUDE_AMBIGUOUS_CHARS_PROMPT = 'Исключать ли неоднозначные символы il1Lo0O?\n'
+
 CURRENT_PASSWORD_OUTPUT = 'Password # '
-SUMMARY_CONFIG_MESSAGE = 'Вы выбрали следующую конфигурацию паролей:\n'
+SUMMARY_CONFIG_MESSAGE = 'Вы выбрали следующую конфигурацию паролей:'
 COLON_SEP = ':\n'
 
 
@@ -42,25 +46,25 @@ def secure_password_configurator():
     passwd_count = get_num_input(PASSWD_COUNT_PROMPT, TYPE_ERROR_MESSAGE)
     cur_passwd_len = get_num_input(CUR_PASSWD_LEN_PROMPT, TYPE_ERROR_MESSAGE)
 
-    digits_included = input(INCLUDE_NUMBERS_PROMPT) != NO_RESPONSE
+    digits_included = input(INCLUDE_DIGITS_PROMPT) != NO_RESPONSE
     upper_letters_included = input(INCLUDE_UPPER_LETTERS_PROMPT) != NO_RESPONSE
     lower_letters_included = input(INCLUDE_LOWER_LETTERS_PROMPT) != NO_RESPONSE
     punctuation_included = input(INCLUDE_PUNCT_PROMPT) != NO_RESPONSE
     ambiguous_chars_excluded = input(EXCLUDE_AMBIGUOUS_CHARS_PROMPT) != NO_RESPONSE
 
-    for _ in range(passwd_count):
-        if digits_included:
-            chars += DIGITS
-        if upper_letters_included:
-            chars += UPPER_LETTERS
-        if lower_letters_included:
-            chars += LOWER_LETTERS
-        if punctuation_included:
-            chars += PUNCTUATION_CHARS
-        if ambiguous_chars_excluded:
-            for c in chars:
-                if c in AMBIGUOUS_CHARS:
-                    chars = chars.replace(c, '')
+    # for _ in range(passwd_count):
+    if digits_included:
+        chars += DIGITS
+    if upper_letters_included:
+        chars += UPPER_LETTERS
+    if lower_letters_included:
+        chars += LOWER_LETTERS
+    if punctuation_included:
+        chars += PUNCTUATION_CHARS
+    if ambiguous_chars_excluded:
+        for c in chars:
+            if c in AMBIGUOUS_CHARS:
+                chars = chars.replace(c, '')
 
     return passwd_count, chars, cur_passwd_len
 
@@ -87,14 +91,17 @@ def main():
         current_passwd = CURRENT_PASSWORD_OUTPUT + str(i + 1) + COLON_SEP + password
         passwd_list.append(current_passwd)
 
-    # digits_included = DIGITS in password
-    # upper_letters_included = UPPER_LETTERS in password
-    # lower_letters_included = LOWER_LETTERS in password
-    # punctuation_included = PUNCTUATION_CHARS in password
-    # ambiguous_chars_excluded = AMBIGUOUS_CHARS not in password
+    # digits_included = False
+    # upper_letters_included = False
+    # lower_letters_included = False
+    # punctuation_included = False
+    # ambiguous_chars_excluded = False
+
     summary_config = SUMMARY_CONFIG_MESSAGE, PASSWD_COUNT_MESSAGE, passwd_count, \
-        digits_included, upper_letters_included, lower_letters_included,\
-        punctuation_included, ambiguous_chars_excluded
+        PASSWD_LEN_MESSAGE, length, INCLUDE_DIGITS_PROMPT, digits_included, INCLUDE_UPPER_LETTERS_PROMPT, \
+        upper_letters_included, INCLUDE_LOWER_LETTERS_PROMPT, lower_letters_included,\
+        INCLUDE_PUNCT_PROMPT, punctuation_included,\
+        EXCLUDE_AMBIGUOUS_CHARS_PROMPT, ambiguous_chars_excluded
     print(*summary_config, sep='\n')
     print(*passwd_list, sep='\n')
 
