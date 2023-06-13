85ece2ad0adf3a310f8e28674fbb4e3894b22d90
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-13 13:20:54 +0300

    15_5_3 Caesar's cypher in Cyrillic (#117)
    * Added caesar's cypher algorythm, get_num_input func for num input validation
    
    * Extracted caesar's cypher to separate func
    
    * Added caesar's cypher for Cyrillic - encrypt mode
    
    * Changed values of MIN_ALPHABET_CHAR_IDX and MAX_ALPHABET_CHAR_IDX consts to ord of needed letters based computation
    
    * Changed TYPE_ERROR_MESSAGE to English
    
    * Removed unused comment
    
    * Simplified non-empty input checking condition
    
    * Simplified for loop in str. 58, .lower method implemented for saving letter case functionality
    
    * Extracted input and print option out of caesar_cypher function
    
    * Refactoring
    
    * Refactoring
    
    * Reordered param list for caesar_cypher

---
+++ 15_mini_project_work/5_caesar's_cipher.py
@@ -0,0 +1,72 @@
+MIN_ALPHABET_CHAR_IDX = ord('А')
+MAX_ALPHABET_CHAR_IDX = ord('я')
+ALPHABET_RANGE = MAX_ALPHABET_CHAR_IDX - MIN_ALPHABET_CHAR_IDX + 1
+
+ENTER_SHIFT_PROMPT = 'Enter shift:\n'
+TYPE_ERROR_MESSAGE = 'The entered data must be numeric!'
+ENTER_MESSAGE_PROMPT = 'Enter message in Cyrillic to ciphre:\n'
+IGNORE_CHAR_LIST = '!#$%&*+-=?@^_., '
+
+
+def get_num_input(prompt, error_message):
+    is_string_num = False
+
+    input_string = input(prompt)
+
+    while not is_string_num:
+        if input_string and input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+            is_string_num = True
+        else:
+            print(error_message)
+            input_string = input(prompt)
+
+    is_string_num = int(input_string)
+    return is_string_num
+
+
+def caesar_cypher(source_msg, shift):
+
+    if shift < 0:
+        shift = ALPHABET_RANGE + shift
+    shift %= ALPHABET_RANGE
+
+    upper_letter_idx_list = []
+    result_msg_char_list = []
+    for i in range(len(source_msg)):
+        letter = source_msg[i]
+
+        if letter.isupper():
+            letter = letter.lower()
+            upper_letter_idx_list.append(i)
+        if letter not in IGNORE_CHAR_LIST:
+            result_msg_letter_idx = ord(letter) + shift
+
+            if result_msg_letter_idx < MIN_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = (MAX_ALPHABET_CHAR_IDX + 1) \
+                                        - (MIN_ALPHABET_CHAR_IDX - result_msg_letter_idx) % ALPHABET_RANGE
+            elif result_msg_letter_idx > MAX_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = MIN_ALPHABET_CHAR_IDX + \
+                                        (result_msg_letter_idx - (MAX_ALPHABET_CHAR_IDX + 1)) % ALPHABET_RANGE
+        else:
+            result_msg_letter_idx = ord(letter)
+
+        result_msg_char_list.append((chr(result_msg_letter_idx)).lower())
+
+    for idx in upper_letter_idx_list:
+        result_msg_char_list[idx] = result_msg_char_list[idx].upper()
+
+    result_msg = ''.join(result_msg_char_list)
+
+    return result_msg
+
+
+def main():
+    source_msg = input(ENTER_MESSAGE_PROMPT)
+    shift = get_num_input(ENTER_SHIFT_PROMPT, TYPE_ERROR_MESSAGE)
+
+    result_msg = caesar_cypher(source_msg, shift)
+
+    print(result_msg)
+
+
+main()
