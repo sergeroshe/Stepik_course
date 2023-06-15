4cfeb99e81bf5431dfa031c06fe913322fca642f
sergero <roshchektaev@gmail.com>
2023-06-15 18:19:01 +0300

    Added functions caesar_cypher_helper,  caesar_encrypt and necessary consts

--- 15_mini_project_work/5_8_ave_caesar.py
+++ 15_mini_project_work/5_8_ave_caesar.py
@@ -1,2 +1,55 @@
+MIN_ALPHABET_CHAR_IDX = ord('a')
+MAX_ALPHABET_CHAR_IDX = ord('z')
+ALPHABET_RANGE = MAX_ALPHABET_CHAR_IDX - MIN_ALPHABET_CHAR_IDX + 1
+ENTER_MESSAGE_PROMPT = 'Enter text in English to encrypt or decrypt:\n'
+IGNORE_CHAR_LIST = '!#$%&*+-=?@^_.«»,:; '
+
+
 def ave_caesar():
     pass
+
+
+def caesar_encrypt(source_msg, shift):
+    result_msg = caesar_cypher_helper(source_msg, shift)
+    return result_msg
+
+
+def caesar_cypher_helper(source_msg, shift):
+
+    # Shift within range [0, ALPHABET_RANGE)
+    # Works for negative shifts as well
+    normalized_shift = shift % ALPHABET_RANGE
+
+    upper_letter_idx_list = []
+    result_msg_char_list = []
+    for i in range(len(source_msg)):
+        letter = source_msg[i]
+
+        if letter.isupper():
+            letter = letter.lower()
+            upper_letter_idx_list.append(i)
+        if letter not in IGNORE_CHAR_LIST:
+            result_msg_letter_idx = ord(letter) + normalized_shift
+
+            if result_msg_letter_idx < MIN_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = (MAX_ALPHABET_CHAR_IDX + 1) \
+                                        - (MIN_ALPHABET_CHAR_IDX - result_msg_letter_idx) % ALPHABET_RANGE
+            elif result_msg_letter_idx > MAX_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = MIN_ALPHABET_CHAR_IDX + \
+                                        (result_msg_letter_idx - (MAX_ALPHABET_CHAR_IDX + 1)) % ALPHABET_RANGE
+        else:
+            result_msg_letter_idx = ord(letter)
+
+        result_msg_char_list.append(chr(result_msg_letter_idx))
+
+    for idx in upper_letter_idx_list:
+        result_msg_char_list[idx] = result_msg_char_list[idx].upper()
+
+    result_msg = ''.join(result_msg_char_list)
+
+    return result_msg
+
+
+
+
+
