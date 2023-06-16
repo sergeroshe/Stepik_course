605ae83689372241365945598a4980fdd0ba3862
sergero <roshchektaev@gmail.com>
2023-06-16 15:30:44 +0300

    Added number_sys_calc func

--- 15_mini_project_work/6_calculator_of_number_systems.py
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -1,27 +1,2 @@
-from math import sqrt
-
-total = 88
-APPLE_TREE = 32
-PEAR_TREE = 22
-PLUM_TREE = 16
-CHERRY_TREE = 17
-
-
 def number_sys_calc():
-    tree_list = [APPLE_TREE, PEAR_TREE, PLUM_TREE, CHERRY_TREE]
-    n_list = []
-
-    for tree in tree_list:
-        for dig in str(tree)[::-1]:
-            n = sqrt(int(dig)) / tree
-            n_list.append(n)
-
-    return n_list
-
-
-def main():
-    n = number_sys_calc()
-    print(n)
-
-
-main()
+    pass
