07b0bd90fe75f63fc545ed8d406fd68b8e5f913c
sergero <roshchektaev@gmail.com>
2023-06-16 18:11:27 +0300

    Changed result value type

--- 15_mini_project_work/6_calculator_of_number_systems.py
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -5,10 +5,10 @@ HEX_SYSTEM_CHARS = ['A', 'B', 'C', 'D', 'E', 'F']
 
 def number_sys_calc(num, base):
 
-    remainder_list = []
+    # remainder_list = []
     quotient = num // base
     remainder = num % base
-    remainder_list.append(remainder)
+    remainder_list_based_num_part = str(remainder)
 
     while quotient >= base:
         num = quotient
@@ -16,21 +16,17 @@ def number_sys_calc(num, base):
         remainder = num % base
         if base == 16 and remainder > 9:
             remainder = HEX_SYSTEM_CHARS[remainder - 10]
-        remainder_list.append(remainder)
-
-    remainder_list.reverse()
-    remainder_list_based_num_part = ''
-    for remainder in remainder_list:
         remainder_list_based_num_part += str(remainder)
 
-    num_str = str(quotient) + remainder_list_based_num_part
+    num_last_part = remainder_list_based_num_part[::-1]
+    num_str = str(quotient) + num_last_part
 
     return num_str
 
 
 def main():
-    num = int(input(ENTER_NUM_PROMPT))
-    base = int(input(ENTER_BASE_PROMPT))
+    num = 999999  # int(input(ENTER_NUM_PROMPT))
+    base = 16  # int(input(ENTER_BASE_PROMPT))
 
     new_number_system_num = number_sys_calc(num, base)
 
