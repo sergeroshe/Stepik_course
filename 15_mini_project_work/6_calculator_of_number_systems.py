d10852b752de3429808c2447599be71fb2321985
sergero <roshchektaev@gmail.com>
2023-06-19 16:02:18 +0300

    Removed obsolete comments

--- 15_mini_project_work/6_calculator_of_number_systems.py
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -1,27 +1,22 @@
 ENTER_NUM_PROMPT = 'Enter a number in decimal number system:\n'
 ENTER_BASE_PROMPT = 'Enter a base of new number system:\n'
 NUMBER_SYSTEM_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-# HEX_SYSTEM_CHARS = ['A', 'B', 'C', 'D', 'E', 'F']
-
 
 def number_sys_calc(num, base):
     quotient = num // base
     remainder = num % base
-    # if base == 16 and remainder > 9:
-    #     remainder = HEX_SYSTEM_CHARS[remainder - 10]
     last_num_part = str(NUMBER_SYSTEM_CHARS[remainder])
 
     while quotient >= base:
         num = quotient
         quotient = num // base
         remainder = num % base
-        # if base == 16 and remainder > 9:
-        #     remainder = NUMBER_SYSTEM_CHARS[remainder]
+
         last_num_part += str(remainder)
-    # if base == 16:
+
     if 9 < quotient < 16:
         quotient = NUMBER_SYSTEM_CHARS[quotient]
-    #     el
+
     if 0 == quotient:
         quotient = ''
 
