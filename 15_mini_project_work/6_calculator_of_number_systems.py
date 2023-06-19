2c0740d42d941b709581e70d1d7a29d63fae4d88
sergero <roshchektaev@gmail.com>
2023-06-19 15:43:55 +0300

    Func calculation simplified by 16-length NUMBER_SYSTEM_CHARS implementing

--- 15_mini_project_work/6_calculator_of_number_systems.py
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -1,29 +1,31 @@
 ENTER_NUM_PROMPT = 'Enter a number in decimal number system:\n'
 ENTER_BASE_PROMPT = 'Enter a base of new number system:\n'
-HEX_SYSTEM_CHARS = ['A', 'B', 'C', 'D', 'E', 'F']
+NUMBER_SYSTEM_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+# HEX_SYSTEM_CHARS = ['A', 'B', 'C', 'D', 'E', 'F']
 
 
 def number_sys_calc(num, base):
     quotient = num // base
     remainder = num % base
-    if base == 16 and remainder > 9:
-        remainder = HEX_SYSTEM_CHARS[remainder - 10]
-    remainder_list_based_num_part = str(remainder)
+    # if base == 16 and remainder > 9:
+    #     remainder = HEX_SYSTEM_CHARS[remainder - 10]
+    last_num_part = str(NUMBER_SYSTEM_CHARS[remainder])
 
     while quotient >= base:
         num = quotient
         quotient = num // base
         remainder = num % base
-        if base == 16 and remainder > 9:
-            remainder = HEX_SYSTEM_CHARS[remainder - 10]
-        remainder_list_based_num_part += str(remainder)
-    if base == 16:
-        if 9 < quotient < 16:
-            quotient = HEX_SYSTEM_CHARS[quotient - 10]
-        elif 0 == quotient:
-            quotient = ''
+        # if base == 16 and remainder > 9:
+        #     remainder = NUMBER_SYSTEM_CHARS[remainder]
+        last_num_part += str(remainder)
+    # if base == 16:
+    if 9 < quotient < 16:
+        quotient = NUMBER_SYSTEM_CHARS[quotient]
+    #     el
+    if 0 == quotient:
+        quotient = ''
 
-    num_last_part = remainder_list_based_num_part[::-1]
+    num_last_part = last_num_part[::-1]
     num_str = str(quotient) + num_last_part
 
     return num_str
