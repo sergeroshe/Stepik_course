95f2ff01eddd52e78b0804b3a5bc9ab0a4ed925d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-20 15:45:26 +0300

    15_5_8 Ave Caesar Encoding Program (#133)
    * Added normalized_shift var
    
    * Initial commit of 5_8_ave_caesar.py
    
    * Added functions caesar_cypher_helper,  caesar_encrypt and necessary consts
    
    * Added word_len_list_configurator func, implemented it's usage in main
    
    * Extracted function out of main
    
    * Removed unused function
    
    * Renamed raw_word_list to source_word_list
    
    * Removed duplicate code, refactored
    
    * Renamed function to word_len_shift_encryptor
    
    * Removed char duplicates in IGNORE_CHAR_LIST
    
    * Added ENTER_MESSAGE_PROMPT to input in main
    
    * Changed return type of encryption
    
    * Renamed func to caesar_encrypt_by_word_len
    
    * Renamed func to word_len_list_config
    
    * Changed return type in caesar_encrypt_by_word_len func
    
    * Renamed func to get_word_len_list
    
    * Renamed IGNORE_CHAR_LIST const to PUNCTUATION
    
    * Reduced split method usage in caesar_encrypt_by_word_len func
    
    * Replaced split method with strip method in get_word_len_list func
    
    * Expanded PUNCTUATION list
    
    * Expanded PUNCTUATION list
    
    * Renamed const to PUNCTUATION_CHARS
    
    * Renamed const to WORD_SEP
    
    * Renamed result_word to encrypted_word
    
    * Used len calc to be for single word only
    
    * Fixed 'Local variable 'result_word' might be referenced before assignment ' mistake
    
    * Renamed func to get_word_len
    
    * Renamed get_word_len func param to word
    
    * Changed for loop from range to list
    
    * Optimized the get_word_len function to return words containing punctuation.
    
    * Refactoring
    
    * Added unpunctuated prefix to vars
    
    * Added checking first and last word's char condition in get_unpunctuated_word_len func
    
    * Added break statement
    
    * Refactoring
    
    * Refactoring
    
    * Renamed word_len to shift in caesar_encrypt_by_word_len func

---
+++ 15_mini_project_work/5_8_ave_caesar.py
@@ -0,0 +1,83 @@
+MIN_ALPHABET_CHAR_IDX = ord('a')
+MAX_ALPHABET_CHAR_IDX = ord('z')
+ALPHABET_RANGE = MAX_ALPHABET_CHAR_IDX - MIN_ALPHABET_CHAR_IDX + 1
+ENTER_MESSAGE_PROMPT = 'Enter text in English to encrypt:\n'
+PUNCTUATION_CHARS = ',.!"!\'@#$%^&*()_+-'
+WORD_SEP = ' '
+
+
+def caesar_encrypt(source_msg, shift):
+    result_msg = caesar_cypher_helper(source_msg, shift)
+    return result_msg
+
+
+def caesar_cypher_helper(source_msg, shift):
+
+    # Shift within range [0, ALPHABET_RANGE)
+    # Works for negative shifts as well
+    normalized_shift = shift % ALPHABET_RANGE
+
+    upper_letter_idx_list = []
+    result_msg_char_list = []
+    for i in range(len(source_msg)):
+        letter = source_msg[i]
+
+        if letter.isupper():
+            letter = letter.lower()
+            upper_letter_idx_list.append(i)
+        if letter not in PUNCTUATION_CHARS:
+            result_msg_letter_idx = ord(letter) + normalized_shift
+
+            if result_msg_letter_idx < MIN_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = (MAX_ALPHABET_CHAR_IDX + 1) \
+                                        - (MIN_ALPHABET_CHAR_IDX - result_msg_letter_idx) % ALPHABET_RANGE
+            elif result_msg_letter_idx > MAX_ALPHABET_CHAR_IDX:
+                result_msg_letter_idx = MIN_ALPHABET_CHAR_IDX + \
+                                        (result_msg_letter_idx - (MAX_ALPHABET_CHAR_IDX + 1)) % ALPHABET_RANGE
+        else:
+            result_msg_letter_idx = ord(letter)
+
+        result_msg_char_list.append(chr(result_msg_letter_idx))
+
+    for idx in upper_letter_idx_list:
+        result_msg_char_list[idx] = result_msg_char_list[idx].upper()
+
+    result_msg = ''.join(result_msg_char_list)
+
+    return result_msg
+
+
+def get_unpunctuated_word_len(word):
+    unpunctuated_word = word
+    if not word[0].isalnum() or not word[-1].isalnum():
+        for char in PUNCTUATION_CHARS:
+            if char == word[0] or char == word[-1]:
+                unpunctuated_word = word.strip(char)
+                break
+
+    unpunctuated_word_len = len(unpunctuated_word)
+
+    return unpunctuated_word_len
+
+
+def caesar_encrypt_by_word_len(source_string):
+    source_word_list = source_string.split()
+
+    encrypted_msg = ''
+    for word in source_word_list:
+        shift = get_unpunctuated_word_len(word)
+        encrypted_word = caesar_encrypt(word, shift)
+        encrypted_msg += encrypted_word + WORD_SEP
+
+    return encrypted_msg
+
+
+def main():
+    source_string = input()
+
+    encrypted_string = caesar_encrypt_by_word_len(source_string)
+
+    print(encrypted_string)
+
+
+main()
