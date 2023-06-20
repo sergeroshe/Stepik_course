364a385b5f597fef3a89967bb9166380048fc94a
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-20 10:11:07 +0300

    15_6_3 Number systems calculator (#135)
    * Added number_sys_calc
    
    * Added number_sys_calc func
    
    * Added new base nuber system computation functionality to number_sys_calc func
    
    * Added new base number system computation functionality to number_sys_calc func
    
    * Added hexadecimal system char based conversion
    
    * Changed result value type
    
    * Fixed mistake in calculation when reminder or quotient is greater than 9
    
    * Fixed mistake in calculation
    
    * Fixed mistake in calculation
    
    * Supported number system for binary, octal and hexadecimal
    
    * Changed params in main to fixed values
    
    * Func calculation simplified by 16-length NUMBER_SYSTEM_CHARS implementing
    
    * Removed obsolete comments
    
    * Bug fix: last_num_part var value calculation should be based on NUMBER_SYSTEM_CHARS indexes
    
    * Renamed last_num_part to converted_digits
    
    * Added var converted_num_first_part to avoid different types combining
    
    * Refactoring
    
    * Renamed converted_num_first_part to converted_num_head
    
    * Used ternary operator instead of if else expr, renamed var
    
    * Renamed converted_num_head to converted_first_digits
    
    * Renamed converted_first_digits to leading_converted_digit
    
    * Renamed num_str to converted_num
    
    * Optimized leading_converted_digit calc code

---
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -0,0 +1,39 @@
+ENTER_NUM_PROMPT = 'Enter a number in decimal number system:\n'
+ENTER_BASE_PROMPT = 'Enter a base of new number system:\n'
+NUMBER_SYSTEM_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+def number_sys_calc(num, base):
+    quotient = num // base
+    remainder = num % base
+    converted_digits = NUMBER_SYSTEM_CHARS[remainder]
+
+    while quotient >= base:
+        num = quotient
+        quotient = num // base
+        remainder = num % base
+
+        converted_digits += NUMBER_SYSTEM_CHARS[remainder]
+
+    leading_converted_digit = NUMBER_SYSTEM_CHARS[quotient] if quotient else ''
+
+    reversed_converted_digits = converted_digits[::-1]
+    converted_num = leading_converted_digit + reversed_converted_digits
+
+    return converted_num
+
+
+def main():
+    num = int(input(ENTER_NUM_PROMPT))
+    # base = int(input(ENTER_BASE_PROMPT))
+
+    binary_number_system_num = number_sys_calc(num, base=2)
+    octal_number_system_num = number_sys_calc(num, base=8)
+    hexadecimal_number_system_num = number_sys_calc(num, base=16)
+
+    print(binary_number_system_num)
+    print(octal_number_system_num)
+    print(hexadecimal_number_system_num)
+
+
+main()
