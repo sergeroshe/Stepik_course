19d47ef74b5e1373e6a4e4dd7100782cbddcd45c
sergero <roshchektaev@gmail.com>
2023-06-21 16:59:48 +0300

    Extracted calc functionality into b_o_h_number_sys_calc func

--- 15_mini_project_work/6_5_number_system_calc_built_in_funcs.py
+++ 15_mini_project_work/6_5_number_system_calc_built_in_funcs.py
@@ -1,5 +1,6 @@
 ENTER_NUM_PROMPT = 'Enter a number in decimal number system:\n'
 TYPE_ERROR_MESSAGE = 'The entered data must be numeric!'
+NUMBER_SYSTEMS_BASE_LIST = [2, 8, 16]
 
 
 def get_num_input(prompt, error_message):
@@ -16,17 +17,25 @@ def get_num_input(prompt, error_message):
     return num
 
 
+def b_o_h_number_sys_calc(num):
+    converted_num_string = ''
+    for base in NUMBER_SYSTEMS_BASE_LIST:
+        if base == 2:
+            converted_num_string += str(bin(num))[2:] + '\n'
+        elif base == 8:
+            converted_num_string += str(oct(num))[2:] + '\n'
+        else:
+            converted_num_string += str(hex(num))[2:].upper() + '\n'
+
+    return converted_num_string
+
+
 def main():
 
     num = get_num_input(ENTER_NUM_PROMPT, TYPE_ERROR_MESSAGE)
+    converted_nums = b_o_h_number_sys_calc(num)
 
-    binary_number_system_num = str(bin(num))[2:]
-    octal_number_system_num = str(oct(num))[2:]
-    hexadecimal_number_system_num = str(hex(num))[2:].upper()
-
-    print(binary_number_system_num)
-    print(octal_number_system_num)
-    print(hexadecimal_number_system_num)
+    print(converted_nums)
 
 
 main()
