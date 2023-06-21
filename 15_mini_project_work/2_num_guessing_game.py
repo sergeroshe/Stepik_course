d1b2a8ae8f84ac5320a7d0aeecee291129f16146
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-21 13:10:21 +0300

    Fixed var name and removed an excessive cast in get_num_input (#141)

--- 15_mini_project_work/2_num_guessing_game.py
+++ 15_mini_project_work/2_num_guessing_game.py
@@ -50,14 +50,14 @@ def get_num_input(prompt, error_message):
     is_string_num = False
     input_string = input(prompt)
     while not is_string_num:
-        if input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+        if input_string and input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
             is_string_num = True
         else:
             print(error_message)
             input_string = input(prompt)
 
-    is_string_num = int(input_string)
-    return is_string_num
+    num = int(input_string)
+    return num
 
 
 def guessing_game_run():
