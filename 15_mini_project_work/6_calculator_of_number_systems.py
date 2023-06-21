02c5f8b6ef00a9bc3d4469af0f0fabb6fce2d58d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-06-21 13:51:16 +0300

    15_6_4 Dynamic baseline with validation (#138)
    * Added get_valid_base func
    
    * Bug fix: base input after get_num_input func validation
    
    * Bug fix: NUMBER_SYSTEM_CHARS length is 39
    
    * Renamed is_string_num to num
    
    * Refactoring
    
    * Renamed get_valid_base to get_constrained_num_input. Left and right border are included
    
    * Removed unnecessary cast
    
    * Translated error messages to Eng
    
    * Added dynamic baseline with validation
    
    * Extracted get_valid_base_num out of main
    
    * Fixed logic split violation in get_valid_base_num func
    
    * Removed unused func
    
    * Extracted 'Magic strings' out of func
    
    * Removed an excessive func get_valid_base_num

--- 15_mini_project_work/6_calculator_of_number_systems.py
+++ 15_mini_project_work/6_calculator_of_number_systems.py
@@ -1,6 +1,42 @@
 ENTER_NUM_PROMPT = 'Enter a number in decimal number system:\n'
-ENTER_BASE_PROMPT = 'Enter a base of new number system:\n'
-NUMBER_SYSTEM_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+NUMBER_SYSTEM_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                       'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                       'U', 'V', 'W', 'X', 'Y', 'Z', 'ﺍ', 'ﺏ', 'ﺕ', 'ﺙ']
+TYPE_ERROR_MESSAGE = 'The entered data must be numeric!'
+BASE_LEFT_BORDER = 1
+BASE_RIGHT_BORDER = len(NUMBER_SYSTEM_CHARS) + 1
+ENTER_BASE_PROMPT = f'Enter a number between {BASE_LEFT_BORDER} and {BASE_RIGHT_BORDER}' \
+                        f' inclusive for base of a new number system:\n'
+BASE_ERROR_MESSAGE = f'The number must be between {BASE_LEFT_BORDER} and {BASE_RIGHT_BORDER} inclusive!'
+
+
+def get_num_input(prompt, error_message):
+    is_string_num = False
+    input_string = input(prompt)
+    while not is_string_num:
+        if input_string and input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+            is_string_num = True
+        else:
+            print(error_message)
+            input_string = input(prompt)
+
+    num = int(input_string)
+    return num
+
+
+#  returns num input within range [left_border, right_border]
+def get_constrained_num_input(enter_base_prompt, type_error_message, base_error_message, left_border, right_border):
+    is_num_valid = False
+    num = get_num_input(enter_base_prompt, type_error_message)
+    while not is_num_valid:
+        if left_border <= num <= right_border:
+            is_num_valid = True
+        else:
+            print(base_error_message)
+            num = get_num_input(enter_base_prompt, type_error_message)
+
+    return num
 
 
 def number_sys_calc(num, base):
@@ -24,16 +60,14 @@ def number_sys_calc(num, base):
 
 
 def main():
-    num = int(input(ENTER_NUM_PROMPT))
-    # base = int(input(ENTER_BASE_PROMPT))
 
-    binary_number_system_num = number_sys_calc(num, base=2)
-    octal_number_system_num = number_sys_calc(num, base=8)
-    hexadecimal_number_system_num = number_sys_calc(num, base=16)
+    num = get_num_input(ENTER_NUM_PROMPT, TYPE_ERROR_MESSAGE)
+    base = get_constrained_num_input(ENTER_BASE_PROMPT, TYPE_ERROR_MESSAGE, BASE_ERROR_MESSAGE,
+                                     BASE_LEFT_BORDER, BASE_RIGHT_BORDER)
 
-    print(binary_number_system_num)
-    print(octal_number_system_num)
-    print(hexadecimal_number_system_num)
+    converted_number_system_num = number_sys_calc(num, base)
+
+    print(converted_number_system_num)
 
 
 main()
