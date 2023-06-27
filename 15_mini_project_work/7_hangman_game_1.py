b7521b298d8bac2c6161547587ac722391fd8f1c
sergero <roshchektaev@gmail.com>
2023-06-27 13:25:17 +0300

    Extracted successful_guess_processing out of game_run func

--- 15_mini_project_work/7_hangman_game_1.py
+++ 15_mini_project_work/7_hangman_game_1.py
@@ -173,8 +173,39 @@ def get_prompt(guessed_word):
     return enter_guess_prompt, len_error_message
 
 
-def game_run(tries_remained, guessed_word, guessed_letters, word_char_list, word_completion_list):
+def successful_guess_processing(input_string, guessed_word, guessed_letters, word_completion_list,
+                                word_char_list, enter_guess_prompt, len_error_message):
+    guess_processing = input_string in guessed_word and input_string != guessed_word
+    guessed = False
 
+    while guess_processing:
+        guessed_letter_idx_list = get_guessed_letter_indexes(guessed_word, input_string)
+
+        if input_string in guessed_letters:
+            print(REPEAT_ERROR)
+            break
+
+        guessed_letters.append(input_string)
+
+        word_completion_list = open_guessed_letters(guessed_letter_idx_list, word_completion_list, input_string)
+
+        if word_completion_list == word_char_list:
+            break
+        else:
+            print(*word_completion_list)
+            input_string = get_valid_string_input(guessed_word, enter_guess_prompt, len_error_message,
+                                                  TYPE_ERROR_MESSAGE)
+            if input_string == guessed_word:
+                guessed = True
+
+        word_completion_list = open_guessed_letters(guessed_letter_idx_list, word_completion_list, input_string)
+
+        guess_processing = input_string in guessed_word and input_string != guessed_word
+
+    return word_completion_list, guessed
+
+
+def game_run(tries_remained, guessed_word, guessed_letters, word_char_list, word_completion_list):
     guessed = guessed_word == word_char_list
     game_lost = False
     while not guessed and not game_lost:
@@ -188,29 +219,13 @@ def game_run(tries_remained, guessed_word, guessed_letters, word_char_list, word
 
         input_string = get_valid_string_input(guessed_word, enter_guess_prompt, len_error_message, TYPE_ERROR_MESSAGE)
 
-        successful_guess_processing = input_string in guessed_word and input_string != guessed_word
-        while successful_guess_processing:
-            input_letter = input_string
-            guessed_letter_idx_list = get_guessed_letter_indexes(guessed_word, input_letter)
-
-            if input_letter in guessed_letters:
-                print(REPEAT_ERROR)
-                break
-
-            guessed_letters.append(input_letter)
-
-            word_completion_list = open_guessed_letters(guessed_letter_idx_list, word_completion_list, input_string)
-
-            if word_completion_list == word_char_list:
-                break
-            else:
-                print(*word_completion_list)
-                input_string = get_valid_string_input(guessed_word, enter_guess_prompt, len_error_message,
-                                                      TYPE_ERROR_MESSAGE)
-
-            successful_guess_processing = input_string in guessed_word and input_string != guessed_word
-
-        guessed = input_string == guessed_word or word_completion_list == word_char_list
+        if input_string in guessed_word and input_string != guessed_word:
+            word_completion_list, guessed = successful_guess_processing(input_string, guessed_word, guessed_letters,
+                                                                        word_completion_list,
+                                                                        word_char_list, enter_guess_prompt,
+                                                                        len_error_message)
+        else:
+            guessed = input_string == guessed_word or word_completion_list == word_char_list
 
         tries_remained -= 1
 
@@ -224,7 +239,6 @@ def game_run(tries_remained, guessed_word, guessed_letters, word_char_list, word
 
 
 def hangman_game(guessed_word):
-
     print(GREETING)
     print(guessed_word)
 
