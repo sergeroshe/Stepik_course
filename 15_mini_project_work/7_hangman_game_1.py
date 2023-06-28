a4c7ba78a96b6b030744a49b518b4fc2ba7186e7
sergero <roshchektaev@gmail.com>
2023-06-28 19:00:35 +0300

    Changed constrain condition in get_constrained_alphabet_input func to any

--- 15_mini_project_work/7_hangman_game_1.py
+++ 15_mini_project_work/7_hangman_game_1.py
@@ -130,8 +130,12 @@ def get_constrained_alphabet_input(prompt, valid_len_list, len_error_message, ty
 
     while not valid_string_input:
         if input_string.isalpha():
-            if len(input_string) not in valid_len_list:
-                valid_string_input = True
+            if len(valid_len_list) == 0:
+                if len(input_string) not in valid_len_list:
+                    valid_string_input = True
+                else:
+                    if len(input_string) in valid_len_list:
+                        valid_string_input = True
             else:
                 print(len_error_message)
                 input_string = input(prompt)
