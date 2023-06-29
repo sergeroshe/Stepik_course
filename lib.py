56e0dd5d15876d101ec6a171ad1c94632a765a8e
sergero <roshchektaev@gmail.com>
2023-06-29 11:52:56 +0300

    Added get_num_input func

---
+++ lib.py
@@ -0,0 +1,12 @@
+def get_num_input(prompt, error_message):
+    is_string_num = False
+    input_string = input(prompt)
+    while not is_string_num:
+        if input_string and input_string[0] == '-' and input_string[1:].isdigit() or input_string.isdigit():
+            is_string_num = True
+        else:
+            print(error_message)
+            input_string = input(prompt)
+
+    num = int(input_string)
+    return num
