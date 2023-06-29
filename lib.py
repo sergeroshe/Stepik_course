bfae3f01971e6e25a1d498ea4cc0ee874ed1f7b0
sergero <roshchektaev@gmail.com>
2023-06-29 13:55:21 +0300

    Added get_constrained_num_input, find_all, get_constrained_alphabet_input funcs

--- lib.py
+++ lib.py
@@ -10,3 +10,51 @@ def get_num_input(prompt, error_message):
 
     num = int(input_string)
     return num
+
+
+def get_constrained_num_input(enter_base_prompt, type_error_message, base_error_message, left_border, right_border):
+    is_num_valid = False
+    num = get_num_input(enter_base_prompt, type_error_message)
+    while not is_num_valid:
+        if left_border <= num <= right_border:
+            is_num_valid = True
+        else:
+            print(base_error_message)
+            num = get_num_input(enter_base_prompt, type_error_message)
+
+    return num
+
+
+def find_all(source, symbol):
+    symbol_idx_list = []
+    start_idx = 0
+    symbol_idx = source.find(symbol, start_idx)
+
+    while symbol_idx != -1:
+        symbol_idx_list.append(symbol_idx)
+        start_idx = symbol_idx + 1
+        symbol_idx = source.find(symbol, start_idx)
+
+    return symbol_idx_list
+
+
+def get_constrained_alphabet_input(prompt, valid_len_list, len_error_message, type_error_message):
+    valid_string_input = False
+
+    input_string = input(prompt)
+
+    while not valid_string_input:
+        if input_string.isalpha():
+            if not valid_len_list:
+                valid_string_input = True
+            else:
+                if len(input_string) in valid_len_list:
+                    valid_string_input = True
+                else:
+                    print(len_error_message)
+                    input_string = input(prompt)
+        else:
+            print(type_error_message)
+            input_string = input(prompt)
+
+    return input_string
