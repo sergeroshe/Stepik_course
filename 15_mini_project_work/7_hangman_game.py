e0f747f7eb5b7d4b2cc09f5f2ddeb84184de4498
sergero <roshchektaev@gmail.com>
2023-07-10 13:42:58 +0300

    Renamed preguessed_letter_idx_list >- preguessed_char_idx_list

--- 15_mini_project_work/7_hangman_game.py
+++ 15_mini_project_work/7_hangman_game.py
@@ -296,11 +296,11 @@ def game_run(tries_remained, hidden_word, category_name):
     print_game_result(word_char_list, game_won)
 
 
-def get_word_char_completion_list(hidden_word, preguessed_letter_idx_list):
+def get_word_char_completion_list(hidden_word, preguessed_char_idx_list):
     word_completion = FILLING_CHAR * len(hidden_word)
     word_char_completion_list = list(word_completion)
-    for i in range(len(preguessed_letter_idx_list)):
-        word_char_completion_list[preguessed_letter_idx_list[i]] = hidden_word[preguessed_letter_idx_list[i]]
+    for i in range(len(preguessed_char_idx_list)):
+        word_char_completion_list[preguessed_char_idx_list[i]] = hidden_word[preguessed_char_idx_list[i]]
 
     return word_char_completion_list
 
