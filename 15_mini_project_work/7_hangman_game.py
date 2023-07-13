12306ea08b65b25db4e744d22b26bb4b4a0a206c
sergero <roshchektaev@gmail.com>
2023-07-13 19:35:54 +0300

    Added hidden_word_revealed option with const

--- 15_mini_project_work/7_hangman_game.py
+++ 15_mini_project_work/7_hangman_game.py
@@ -13,6 +13,7 @@ WIN_MESSAGE = 'Поздравляем, вы угадали слово! Вы по
 WRONG_GUESS_MESSAGE = 'Ответ неверный'
 GAME_LOST_MESSAGE = 'Вы проиграли.'
 TYPE_ERROR_MESSAGE = 'Введенные данные должны содержать только текст!\n'
+HIDDEN_WORD_RELEALED_MESSAGE = 'Вы открыли все буквы слова.'
 LEN_ERROR_MESSAGE = 'Введенное слово должно состоять из {word_len} букв!'
 REPEAT_LETTER_ERROR = 'Вы уже вводили эту букву, попробуйте другую'
 REPEAT_NUMBER_ERROR = 'Вы уже вводили эту цифру, попробуйте другую'
@@ -283,7 +284,7 @@ def get_constrained_num_input(enter_base_prompt, type_error_message, base_error_
     return num
 
 
-def get_pre_guessed_char_positions_list(hidden_word):
+def get_pre_guessed_char_positions_list(hidden_word, hidden_word_relealed):
     last_char_position = len(hidden_word)
     pre_guessed_char_positions_list = []
     pre_guessed_char_positions_list_full = False
@@ -301,8 +302,11 @@ def get_pre_guessed_char_positions_list(hidden_word):
 
         pre_guessed_char_positions_list_full = len(pre_guessed_char_positions_list) == last_char_position\
                                                or pre_guessed_char_position is None
+    if len(pre_guessed_char_positions_list) == last_char_position:
+        print(HIDDEN_WORD_RELEALED_MESSAGE)
+        hidden_word_relealed = True
 
-    return pre_guessed_char_positions_list
+    return pre_guessed_char_positions_list, hidden_word_relealed
 
 
 def game_run(tries_remained, hidden_word, category_name):
@@ -314,35 +318,38 @@ def game_run(tries_remained, hidden_word, category_name):
     valid_input_len_list = [1, hidden_word_len]
     enter_guess_prompt, len_error_message = get_dialog_messages(hidden_word, category_name)
     guessed_chars = []
-    pre_guessed_char_list = get_pre_guessed_char_positions_list(hidden_word)
-    word_char_completion_list = get_word_char_completion_list(hidden_word, pre_guessed_char_list)
-    while not game_won and tries_remained:
-        print_current_game_status(tries_remained, word_char_completion_list)
+    pre_guessed_char_list, hidden_word_relealed = get_pre_guessed_char_positions_list(hidden_word, game_won)
+    if not hidden_word_relealed:
+        word_char_completion_list = get_word_char_completion_list(hidden_word, pre_guessed_char_list)
+        while not game_won and tries_remained:
+            print_current_game_status(tries_remained, word_char_completion_list)
 
-        input_string = get_constrained_alphabet_input(enter_guess_prompt, valid_input_len_list,
-                                                      len_error_message, TYPE_ERROR_MESSAGE).upper()
-        if input_string == hidden_word:
-            game_won = True
-        elif len(input_string) == 1:
-            input_char = input_string
-            if input_char not in guessed_chars:
-                guessed_chars.append(input_char)
-                if input_char in hidden_word:
-                    guessed_char_idx_list = find_all(hidden_word, input_char)
-                    word_char_completion_list = open_guessed_letters(guessed_char_idx_list,
-                                                                     word_char_completion_list, input_char)
-                    game_won = word_char_completion_list == word_char_list
+            input_string = get_constrained_alphabet_input(enter_guess_prompt, valid_input_len_list,
+                                                          len_error_message, TYPE_ERROR_MESSAGE).upper()
+            if input_string == hidden_word:
+                game_won = True
+            elif len(input_string) == 1:
+                input_char = input_string
+                if input_char not in guessed_chars:
+                    guessed_chars.append(input_char)
+                    if input_char in hidden_word:
+                        guessed_char_idx_list = find_all(hidden_word, input_char)
+                        word_char_completion_list = open_guessed_letters(guessed_char_idx_list,
+                                                                         word_char_completion_list, input_char)
+                        game_won = word_char_completion_list == word_char_list
+                    else:
+                        print(WRONG_GUESS_MESSAGE)
+                        tries_remained -= 1
                 else:
-                    print(WRONG_GUESS_MESSAGE)
+                    print(REPEAT_LETTER_ERROR)
                     tries_remained -= 1
             else:
-                print(REPEAT_LETTER_ERROR)
+                print(WRONG_GUESS_MESSAGE)
                 tries_remained -= 1
-        else:
-            print(WRONG_GUESS_MESSAGE)
-            tries_remained -= 1
 
-    print_game_result(word_char_list, game_won)
+        print_game_result(word_char_list, game_won)
+    else:
+        print(*word_char_list)
 
 
 def get_word_char_completion_list(hidden_word, preguessed_char_idx_list):
