88459ce90dbb854abc62e493e5159fd2698ff6ba
sergero <roshchektaev@gmail.com>
2023-07-18 12:36:01 +0300

    Removed extra param tries_remained from hangman_game_run func

--- 15_mini_project_work/7_hangman_game.py
+++ 15_mini_project_work/7_hangman_game.py
@@ -307,7 +307,6 @@ def print_game_starting_status(hidden_word_len, category_name):
 
 
 def hangman_game(total_tries_remained, hidden_word, category_name):
-    tries_remained = total_tries_remained
     hidden_word_len = len(hidden_word)
     print_game_starting_status(hidden_word_len, category_name)
     word_char_list = list(hidden_word)
@@ -316,15 +315,16 @@ def hangman_game(total_tries_remained, hidden_word, category_name):
 
     if not is_hidden_word_revealed(pre_guessed_char_idx_list, hidden_word):
         word_char_completion_list = get_word_char_completion_list(hidden_word, pre_guessed_char_idx_list)
-        game_won = hangman_game_run(hidden_word, category_name, tries_remained, word_char_completion_list)
+        game_won = hangman_game_run(hidden_word, category_name, total_tries_remained, word_char_completion_list)
         print_game_result(word_char_list, game_won)
     else:
         print_hidden_word_revelation(word_char_list)
 
 
-def hangman_game_run(hidden_word, category_name, tries_remained, word_char_completion_list):
+def hangman_game_run(hidden_word, category_name, total_tries_remained, word_char_completion_list):
     word_char_list = list(hidden_word)
     hidden_word_len = len(hidden_word)
+    tries_remained = total_tries_remained
     guessed_chars = []
     valid_input_len_list = [1, hidden_word_len]
     enter_guess_prompt, len_error_message = get_dialog_messages(hidden_word, category_name)
