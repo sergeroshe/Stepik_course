c6e5a935541102b581c3ad0d5235d73d49d9fc47
sergero <roshchektaev@gmail.com>
2023-08-09 16:39:36 +0300

    Got rid of extra loop in string 35

--- ADVANCED_COURSE/PART_1/2_2_1_coordinate quarters.py
+++ ADVANCED_COURSE/PART_1/2_2_1_coordinate quarters.py
@@ -1,7 +1,7 @@
 def get_coordinate_quarter(x, y):
     coordinate_quarter_num = 0
-    x_coordinate = int(x)
-    y_coordinate = int(y)
+    x_coordinate = x
+    y_coordinate = y
     if x_coordinate and y_coordinate:
         if x_coordinate > 0 and y_coordinate > 0:
             coordinate_quarter_num = 1
@@ -15,29 +15,31 @@ def get_coordinate_quarter(x, y):
     return coordinate_quarter_num
 
 
-QUARTER_MESSAGE_LIST = ['Первая четверть: {first_quarter_points_count}',
-                        'Вторая четверть: {second_quarter_points_count}',
-                        'Третья четверть: {third_quarter_points_count}',
-                        'Четвертая четверть: {forth_quarter_points_count}']
-QUARTER_POINTS_COUNTER_LIST = [0, 0, 0, 0]
-point_amount = int(input())
+QUARTER_MESSAGE_LIST = ['Первая четверть: {current_quarter_points_count}',
+                        'Вторая четверть: {current_quarter_points_count}',
+                        'Третья четверть: {current_quarter_points_count}',
+                        'Четвертая четверть: {current_quarter_points_count}']
 
-coordinates_list = []
-result_string_list = []
-for _ in range(point_amount):
-    result_string_list = []
-    coordinate_line_list = input().split()
-    x = int(coordinate_line_list[0])
-    y = int(coordinate_line_list[1])
+point_amount = 2 # int(input())
+quarter_points_counter_list = [0, 0, 0, 0]
+
+for i in range(point_amount):
+    coordinates = [-9, 3] # input().split()
+    x = int(coordinates[0])
+    y = int(coordinates[1])
+
     coordinate_quarter = get_coordinate_quarter(x, y)
     if coordinate_quarter:
-        QUARTER_POINTS_COUNTER_LIST[coordinate_quarter - 1] += 1
-    for current_quarter in QUARTER_MESSAGE_LIST:
-        current_quarter = current_quarter.format(
-            first_quarter_points_count=QUARTER_POINTS_COUNTER_LIST[0],
-            second_quarter_points_count=QUARTER_POINTS_COUNTER_LIST[
-                1],
-            third_quarter_points_count=QUARTER_POINTS_COUNTER_LIST[2],
-            forth_quarter_points_count=QUARTER_POINTS_COUNTER_LIST[3])
-        result_string_list.append(current_quarter)
-print(*result_string_list, sep='\n')
+        quarter_points_counter_list[coordinate_quarter - 1] += 1
+
+    QUARTER_MESSAGE_LIST[0] = QUARTER_MESSAGE_LIST[0].format(current_quarter_points_count=
+                                                             quarter_points_counter_list[0])
+    QUARTER_MESSAGE_LIST[1] = QUARTER_MESSAGE_LIST[1].format(current_quarter_points_count=
+                                                             quarter_points_counter_list[1])
+    QUARTER_MESSAGE_LIST[2] = QUARTER_MESSAGE_LIST[2].format(current_quarter_points_count=
+                                                             quarter_points_counter_list[2])
+    QUARTER_MESSAGE_LIST[3] = QUARTER_MESSAGE_LIST[3].format(current_quarter_points_count=
+                                                             quarter_points_counter_list[3])
+
+
+print(*QUARTER_MESSAGE_LIST, sep='\n')
