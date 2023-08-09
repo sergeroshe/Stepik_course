e286385f86f16cfe0cefa81328b1989b04b1e3c2
sergero <roshchektaev@gmail.com>
2023-08-09 17:23:15 +0300

    Extracted logic to function display_quarters_for_coordinates

--- ADVANCED_COURSE/PART_1/2_2_1_coordinate quarters.py
+++ ADVANCED_COURSE/PART_1/2_2_1_coordinate quarters.py
@@ -1,45 +1,47 @@
+QUARTER_MESSAGE = 'Первая четверть: {first_quarter_points_count} \n' \
+                  ' Вторая четверть: {second_quarter_points_count} \n' \
+                  'Третья четверть: {third_quarter_points_count} \n' \
+                  'Четвертая четверть: {forth_quarter_points_count}'
+
+
 def get_coordinate_quarter(x, y):
     coordinate_quarter_num = 0
-    x_coordinate = x
-    y_coordinate = y
-    if x_coordinate and y_coordinate:
-        if x_coordinate > 0 and y_coordinate > 0:
+    if x and y:
+        if x > 0 and y > 0:
             coordinate_quarter_num = 1
-        elif x_coordinate < 0 < y_coordinate:
+        elif x < 0 < y:
             coordinate_quarter_num = 2
-        elif x_coordinate < 0 and y_coordinate < 0:
+        elif x < 0 and y < 0:
             coordinate_quarter_num = 3
-        elif x_coordinate > 0 > y_coordinate:
+        elif x > 0 > y:
             coordinate_quarter_num = 4
 
     return coordinate_quarter_num
 
 
-QUARTER_MESSAGE_LIST = ['Первая четверть: {current_quarter_points_count}',
-                        'Вторая четверть: {current_quarter_points_count}',
-                        'Третья четверть: {current_quarter_points_count}',
-                        'Четвертая четверть: {current_quarter_points_count}']
+def display_quarters_for_coordinates(quarter_message):
+    point_amount = int(input())
+    quarter_points_counter_list = [0, 0, 0, 0]
 
-point_amount = 2 # int(input())
-quarter_points_counter_list = [0, 0, 0, 0]
+    for i in range(point_amount):
+        coordinates = input().split()
+        x = int(coordinates[0])
+        y = int(coordinates[1])
 
-for i in range(point_amount):
-    coordinates = [-9, 3] # input().split()
-    x = int(coordinates[0])
-    y = int(coordinates[1])
+        coordinate_quarter = get_coordinate_quarter(x, y)
+        if coordinate_quarter:
+            quarter_points_counter_list[coordinate_quarter - 1] += 1
 
-    coordinate_quarter = get_coordinate_quarter(x, y)
-    if coordinate_quarter:
-        quarter_points_counter_list[coordinate_quarter - 1] += 1
+    quarter_message = quarter_message.format(first_quarter_points_count=quarter_points_counter_list[0],
+                                             second_quarter_points_count=quarter_points_counter_list[1],
+                                             third_quarter_points_count=quarter_points_counter_list[2],
+                                             forth_quarter_points_count=quarter_points_counter_list[3])
 
-    QUARTER_MESSAGE_LIST[0] = QUARTER_MESSAGE_LIST[0].format(current_quarter_points_count=
-                                                             quarter_points_counter_list[0])
-    QUARTER_MESSAGE_LIST[1] = QUARTER_MESSAGE_LIST[1].format(current_quarter_points_count=
-                                                             quarter_points_counter_list[1])
-    QUARTER_MESSAGE_LIST[2] = QUARTER_MESSAGE_LIST[2].format(current_quarter_points_count=
-                                                             quarter_points_counter_list[2])
-    QUARTER_MESSAGE_LIST[3] = QUARTER_MESSAGE_LIST[3].format(current_quarter_points_count=
-                                                             quarter_points_counter_list[3])
+    print(quarter_message)
 
 
-print(*QUARTER_MESSAGE_LIST, sep='\n')
+def main():
+    display_quarters_for_coordinates(QUARTER_MESSAGE)
+
+
+main()
