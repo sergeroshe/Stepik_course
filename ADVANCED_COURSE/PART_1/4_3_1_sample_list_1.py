27d8ae49d994b575bc67c135956f1bcdc2ddaeb5
sergero <roshchektaev@gmail.com>
2023-09-06 09:16:44 +0300

    Simplified list expression

--- ADVANCED_COURSE/PART_1/4_3_1_sample_list_1.py
+++ ADVANCED_COURSE/PART_1/4_3_1_sample_list_1.py
@@ -1,18 +1,4 @@
-#  Sample Input 1:
-# 3
-# Sample Output 1:
-#
-# [1, 2, 3]
-# [1, 2, 3]
-# [1, 2, 3]
-# Sample Input 2:
-total_list = []
-row_number = 3
-for i in range(row_number):
-    cur_list = []
-    for j in range(row_number):
-        cur_list.append(j + 1)
-
-    total_list.append(cur_list)
+row_number = int(input())
+total_list = [[j for j in range(1, row_number + 1)] for i in range(row_number)]
 
 print(*total_list, sep='\n')
