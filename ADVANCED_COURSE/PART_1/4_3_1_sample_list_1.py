18e4c6ea58a22eee610e1ff7eecef6dceb87fdd7
sergero <roshchektaev@gmail.com>
2023-09-07 13:08:56 +0300

    Renamed var to size

--- ADVANCED_COURSE/PART_1/4_3_1_sample_list_1.py
+++ ADVANCED_COURSE/PART_1/4_3_1_sample_list_1.py
@@ -1,4 +1,4 @@
-row_number = int(input())
-total_list = [[j for j in range(1, row_number + 1)] for i in range(row_number)]
+size = int(input())
+total_list = [[j for j in range(1, size + 1)] for i in range(size)]
 
 print(*total_list, sep='\n')
