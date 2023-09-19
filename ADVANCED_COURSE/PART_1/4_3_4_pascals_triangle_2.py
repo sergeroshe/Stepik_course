56b13c59a41941df42a97183ea1180dd490f0eb9
sergero <roshchektaev@gmail.com>
2023-09-19 16:35:20 +0300

    Implemented existing func usage

--- ADVANCED_COURSE/PART_1/4_3_4_pascals_triangle_2.py
+++ ADVANCED_COURSE/PART_1/4_3_4_pascals_triangle_2.py
@@ -1,19 +1,25 @@
-def get_pascals_triangle_row(size):
-    row = [1]
-    for i in range(size):
-        row.append(row[i] * (size - i) // (i + 1))
-    return row
+def get_pascals_triangle_row(size) -> list:
+    prev_row = []
+    cur_row = []
+
+    for i in range(size + 1):
+        cur_row = [1] * (i + 1)
+        for j in range(1, i):
+            cur_row[j] = prev_row[j - 1] + prev_row[j]
+        prev_row = cur_row
+
+    return cur_row
 
 
 def main():
-    pascals_triangle = []
+    # pascals_triangle = []
+    # row_number = 5
     row_number = int(input())
-    for i in range(row_number - 1, -1, -1):
-        pascals_triangle.append(get_pascals_triangle_row(i))
-    pascals_triangle.reverse()
-    for row in pascals_triangle:
-        print(*row)
+    for i in range(row_number):
+        row = get_pascals_triangle_row(i)
+        ones, *values = row
+        unpacked_row = ones, *values
+        print(*unpacked_row)
 
 
 main()
-
