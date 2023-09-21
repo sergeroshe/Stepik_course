0008a223d245f37c89cbf4da67c365b5147a03a5
sergero <roshchektaev@gmail.com>
2023-09-21 13:46:04 +0300

    Changed range in loop, removed break

--- ADVANCED_COURSE/PART_1/4_3_7_list_sublists.py
+++ ADVANCED_COURSE/PART_1/4_3_7_list_sublists.py
@@ -1,18 +1,14 @@
-# source_string_list = input().split()
-source_string_list = 'a b c'.split()
+source_string_list = input().split()
+# source_string_list = 'a b c d e'.split()
 len_source_string = len(source_string_list)
 
-sub_list = []
 # total_list = [source_string_list[i: i + 1] for i in range(0, len(source_string_list), 1)]
 total_list = [[]]
-for i in range(len_source_string):
-    sub_list_size = i + 1
-    for cur_char_idx in range(len_source_string):
+for sub_list_size in range(1, len_source_string + 1):
+    for cur_char_idx in range(len_source_string + 1 - sub_list_size):
         sub_list = source_string_list[cur_char_idx: cur_char_idx + sub_list_size]
-        if len(sub_list) == sub_list_size:
-            total_list.append(sub_list)
-        else:
-            break
+        total_list.append(sub_list)
+
 
 print(total_list)
 
