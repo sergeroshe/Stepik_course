8775a827e34640cdaead086bb962fbd1fc2aa1e3
sergero <roshchektaev@gmail.com>
2023-09-21 13:56:11 +0300

    Changed list creation place

--- ADVANCED_COURSE/PART_1/4_3_1_print_matrix_1.py
+++ ADVANCED_COURSE/PART_1/4_3_1_print_matrix_1.py
@@ -2,20 +2,17 @@ row_amount = int(input())
 col_amount = int(input())
 # row_amount = 2
 # col_amount = 3
-
-
 matrix = []
-row_list = []
 
 for i in range(row_amount):
+    row_list = []
     for j in range(col_amount):
         row_list.append(input())
 
     matrix.append(row_list)
-    row_list = []
 
-for row in matrix:
-    for col in row:
-        print(col, end=' ')
+for row in range(row_amount):
+    for col in range(col_amount):
+        print(matrix[row][col], end=' ')
     print()
 
