b5528668237fd1d9a35f560acc00c4d45a000be8
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-09-29 15:13:19 +0300

    Training files saving to repo (#244)
    * Changed matrix output approach
    
    * Fixed files wrong location

---
+++ AUTOMATION_TESTING_TRAINING/H_WORK_4/classes_learning.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, n, s):
+        self.name = n
+        self.surname = s
+
+
+p1 = Person("Sam", "Baker")
+# p1 = Person("Sam", "Baker")
+print(p1.name, p1.surname)
