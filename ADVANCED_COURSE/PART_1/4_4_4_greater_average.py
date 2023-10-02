1c819e5ed4589d8c7d3eccc5999501409a61e37b
sergero <roshchektaev@gmail.com>
2023-10-02 13:05:30 +0300

    Changed output data type to list

--- ADVANCED_COURSE/PART_1/4_4_4_greater_average.py
+++ ADVANCED_COURSE/PART_1/4_4_4_greater_average.py
@@ -1,19 +1,24 @@
 size = int(input())
-
+#
 matrix = []
+# size = 2
+# matrix = [['1 2 3 4'], ['5 6 3 15']]
 row = []
 for _ in range(size):
     row = input().split()
     int_row = [int(item) for item in row]
     matrix.append(int_row)
 
-# matrix = [['1 2 3 4'], ['5 6 3 15']]
+
 for row in matrix:
+    counter_list = []
     arithmetic_mean = sum(row) // len(row)
-    count = 0
+    counter = 0
     for col in row:
         if col > arithmetic_mean:
-            count += 1
-    print(count)
+            counter += 1
+    counter_list.append(counter)
+    print(*counter_list, sep='\n')
+
 
 # TODO: create list of data, and print val after
