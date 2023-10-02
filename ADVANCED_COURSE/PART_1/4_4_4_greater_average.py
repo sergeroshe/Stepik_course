a605a6e622e5a2074a9a85096559e79be23105c1
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-10-02 14:15:39 +0300

    4_4_4 Greater than average task (#255)
    * Added Greater than average task solution
    
    * Added TODO
    
    * Changed output data type to list
    
    * Fixed intend
    
    * Removed obsolete comment

---
+++ ADVANCED_COURSE/PART_1/4_4_4_greater_average.py
@@ -0,0 +1,22 @@
+size = int(input())
+#
+matrix = []
+# size = 2
+# matrix = [['1 2 3 4'], ['5 6 3 15']]
+row = []
+for _ in range(size):
+    row = input().split()
+    int_row = [int(item) for item in row]
+    matrix.append(int_row)
+
+counter_list = []
+for row in matrix:
+
+    arithmetic_mean = sum(row) // len(row)
+    counter = 0
+    for col in row:
+        if col > arithmetic_mean:
+            counter += 1
+    counter_list.append(counter)
+
+print(*counter_list, sep='\n')
