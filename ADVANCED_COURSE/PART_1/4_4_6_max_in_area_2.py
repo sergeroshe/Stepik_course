e9da20a85cc977bc541878f0a0003218cad68e0f
sergero <roshchektaev@gmail.com>
2023-10-04 16:52:08 +0300

    Removed append method

--- ADVANCED_COURSE/PART_1/4_4_6_max_in_area_2.py
+++ ADVANCED_COURSE/PART_1/4_4_6_max_in_area_2.py
@@ -7,27 +7,34 @@ for _ in range(size):
     int_row = [int(item) for item in row]
     matrix.append(int_row)
 
-# print(matrix)
-#
-# matrix = [[6, 0],
-#           [7, 9]]
-
 max_item = max([matrix[0][0]])
-
-area_list = []
 
 for i in range((len(matrix) + 1) // 2):
     for j in range(0, i + 1):
-        area_list.append(matrix[i][j])
-        area_list.append(matrix[-(i + 1)][j])
-        area_list.append(matrix[-(i + 1)][-(j + 1)])
-        area_list.append(matrix[i][-(j + 1)])
-        current_max = max(area_list)
+
+        current_max = max(matrix[i][j], matrix[-(i + 1)][j],
+                          matrix[-(i + 1)][-(j + 1)], matrix[i][-(j + 1)])
         if current_max > max_item:
             max_item = current_max
 
 print(max_item)
 
-# print(max(area_list))
+# matrix = [[6, 0],
+#           [7, 9]]
+
 # print(area_list)
 
+# Выше главной диагонали: i < j,
+# Главная диагональ: i = j
+# Ниже главной диагонали: i > j
+
+# Выше побочной диагонали: i + j + 1 < n
+# Побочная диагональ: i + j + 1 = n
+# Ниже побочной диагонали: i + j + 1 > n
+
+# i > j and i + j + 1 < n
+# i < j and i + j + 1 > n
+# matrix = [[6, 0, 4, 90],
+#           [7, 9, 34, 98],
+#           [6, 0, 74, 92],
+#           [6, 0, 72, 92]]
