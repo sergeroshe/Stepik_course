90571504cd303f9f49df950cce5d7f949759af1e
sergero <roshchektaev@gmail.com>
2023-10-09 13:56:28 +0300

    Added comment about perf

--- ADVANCED_COURSE/PART_1/4_4_6_max_in_area_2.py
+++ ADVANCED_COURSE/PART_1/4_4_6_max_in_area_2.py
@@ -9,17 +9,20 @@ for _ in range(size):
 
 max_item = max([matrix[0][0]])
 
-# for i in range((len(matrix) + 1) // 2):
-#     for j in range(0, i + 1):
-#         current_max = max(matrix[i][j], matrix[-(i + 1)][j],
-#                           matrix[-(i + 1)][-(j + 1)], matrix[i][-(j + 1)])
-#         if current_max > max_item:
-#             max_item = current_max
-for i in range(size):
-    for j in range(size):
-        if (i >= j and i + j + 1 < size) or (i <= j and i + j + 1 > size):
-            if matrix[i][j] > max_item:
-                max_item = matrix[i][j]
+for i in range((len(matrix) + 1) // 2):
+    for j in range(0, i + 1):
+        current_max = max(matrix[i][j], matrix[-(i + 1)][j],
+                          matrix[-(i + 1)][-(j + 1)], matrix[i][-(j + 1)])
+        if current_max > max_item:
+            max_item = current_max
+# naive solution suboptimal
+# because we touch all elements, but need just half
+
+# for i in range(size):
+#     for j in range(size):
+#         if (i >= j and i + j + 1 < size) or (i <= j and i + j + 1 > size):
+#             if matrix[i][j] > max_item:
+#                 max_item = matrix[i][j]
 
 print(max_item)
 
