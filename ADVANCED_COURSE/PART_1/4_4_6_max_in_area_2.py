5224debc97ac506528286be32bdc816ffb867341
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-10-09 13:58:09 +0300

    4_4_6 Max in area 2 (#259)
    * Added 4_4_6_max_in_area_2.py task raw solution
    
    * Reduced number of loops
    
    * Removed append method
    
    * Optional solution
    
    * Expanded description in comments
    
    * Added comment about perf

---
+++ ADVANCED_COURSE/PART_1/4_4_6_max_in_area_2.py
@@ -0,0 +1,42 @@
+size = int(input())
+
+matrix = []
+row = []
+for _ in range(size):
+    row = input().split()
+    int_row = [int(item) for item in row]
+    matrix.append(int_row)
+
+max_item = max([matrix[0][0]])
+
+for i in range((len(matrix) + 1) // 2):
+    for j in range(0, i + 1):
+        current_max = max(matrix[i][j], matrix[-(i + 1)][j],
+                          matrix[-(i + 1)][-(j + 1)], matrix[i][-(j + 1)])
+        if current_max > max_item:
+            max_item = current_max
+# naive solution suboptimal
+# because we touch all elements, but need just half
+
+# for i in range(size):
+#     for j in range(size):
+#         if (i >= j and i + j + 1 < size) or (i <= j and i + j + 1 > size):
+#             if matrix[i][j] > max_item:
+#                 max_item = matrix[i][j]
+
+print(max_item)
+
+
+# Выше главной диагонали: i < j,
+# Главная диагональ: i = j
+# Ниже главной диагонали: i > j
+
+# Выше побочной диагонали: i + j + 1 < n
+# Побочная диагональ: i + j + 1 = n
+# Ниже побочной диагонали: i + j + 1 > n
+
+# Выше главной диагонали и ниже побочной диагонали (правый треугольник),
+# включая границы: i <= j and i + j + 1 >= size
+# Ниже главной диагонали и выше побочной диагонали (левый треугольник),
+# включая границы: i >= j and i + j + 1 <= n
+
