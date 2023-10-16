fc8e1a2398961da5878703458d3ba5002742858c
sergero <roshchektaev@gmail.com>
2023-10-16 11:33:50 +0300

    Added task solution with ljust method. Optional solution in comments

---
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -0,0 +1,31 @@
+string_amount = int(input())
+column_amount = int(input())
+# string_amount = 4
+# column_amount = 6
+mult = []
+
+for i in range(string_amount):
+    row = []
+    for j in range(column_amount):
+        row.append(i * j)
+    mult.append(row)
+
+for i in range(string_amount):
+    for j in range(column_amount):
+        print(str(mult[i][j]).ljust(3), end=' ')
+    print()
+
+# string_amount = int(input())
+# column_amount = int(input())
+# # string_amount = 4
+# # column_amount = 6
+# mult = []
+#
+# for i in range(string_amount):
+#     row = []
+#     for j in range(column_amount):
+#         row.append(i * j)
+#     mult.append(row)
+#
+# for string in mult:
+#     print(*string)
