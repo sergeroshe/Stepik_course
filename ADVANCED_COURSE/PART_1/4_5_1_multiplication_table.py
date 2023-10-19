74b344fa98c57eb5315fad9b8136e3d8d6e1d346
sergero <roshchektaev@gmail.com>
2023-10-19 11:27:28 +0300

    Transformed "for" cycle to list expression

--- ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -1,14 +1,17 @@
-string_amount = int(input())
-column_amount = int(input())
-# string_amount = 4
-# column_amount = 6
+# string_amount = int(input())
+# column_amount = int(input())
+string_amount = 4
+column_amount = 6
 mult = []
+#
+# for i in range(string_amount):
+#     row = []
+#     for j in range(column_amount):
+#         row.append(i * j)
+#     mult.append(row)
 
 for i in range(string_amount):
-    row = []
-    for j in range(column_amount):
-        row.append(i * j)
-    mult.append(row)
+    mult.append([(i * j) for j in range(column_amount)])
 
 for i in range(string_amount):
     for j in range(column_amount):
