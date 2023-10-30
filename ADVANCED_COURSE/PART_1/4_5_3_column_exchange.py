7609c4e192f46eb1ef4fb71125d8ed7843f5af7e
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-10-30 19:41:04 +0300

    Added task initial solution

---
+++ ADVANCED_COURSE/PART_1/4_5_3_column_exchange.py
@@ -0,0 +1,9 @@
+row_amount = int(input())
+column_amount = int(input())
+
+matrix = [input().split() for i in range(row_amount)]
+column_x_idx, column_y_idx = map(int, input().split())
+
+for _, row in enumerate(matrix):
+    row[column_x_idx], row[column_y_idx] = row[column_y_idx], row[column_x_idx]
+    print(*row)
