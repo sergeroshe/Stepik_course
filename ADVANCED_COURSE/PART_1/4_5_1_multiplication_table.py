3ef78aa6afb408364fe2c9e2cbc73d3ef0da0771
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-10-31 14:03:55 +0300

    Renamed vars, added comment

--- ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -1,6 +1,6 @@
 # string_amount = int(input())
 # column_amount = int(input())
-string_amount = 4
+row_amount = 4
 column_amount = 6
 mult = []
 #
@@ -9,11 +9,12 @@ mult = []
 #     for j in range(column_amount):
 #         row.append(i * j)
 #     mult.append(row)
+# naive solution
+# for i in range(string_amount):
+#     mult.append([(i * j) for j in range(column_amount)])
 
-for i in range(string_amount):
-    mult.append([(i * j) for j in range(column_amount)])
 
-for i in range(string_amount):
+for i in range(row_amount):
     for j in range(column_amount):
         print(str(mult[i][j]).ljust(3), end=' ')
     print()
