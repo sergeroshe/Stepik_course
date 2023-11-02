8b7ee75e13e5c683e320aa0703f39acf2b2f9090
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-02 19:00:42 +0300

    Split logic and output.

--- ADVANCED_COURSE/PART_1/4_5_2_table_maximum.py
+++ ADVANCED_COURSE/PART_1/4_5_2_table_maximum.py
@@ -18,17 +18,11 @@ i = 0
 while not max_found and i <= row_amount - 1:
     j = 0
     while not max_found and j <= column_amount - 1:
-        cur_item = matrix[i][j]
-        if cur_item == max_item:
-            result = f'{i} {j}'
+        if matrix[i][j] == max_item:
+            result = [i, j]
             max_found = True
         j += 1
 
     i += 1
 
-print(result)
-# matrix = [[0, 3, 2, 4], [2, 3, 5, 5], [5, 1, 2, 3]]
-# string_amount = 3
-# column_amount = 4
-# print(*matrix, sep='\n')
-# print(max_item)
+print(*result)
