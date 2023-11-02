f507f0a2d977da2b2fcb73e72149f43b14cdf0f6
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-02 14:33:36 +0300

    Reduced multiplication operation count

--- ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -1,20 +1,35 @@
-# string_amount = int(input())
-# column_amount = int(input())
-row_amount = 4
-column_amount = 6
+row_amount = 5
+column_amount = 15
 mult = []
-#
-# for i in range(string_amount):
-#     row = []
-#     for j in range(column_amount):
-#         row.append(i * j)
-#     mult.append(row)
-# naive solution
-# for i in range(string_amount):
-#     mult.append([(i * j) for j in range(column_amount)])
 
+# for i in range(row_amount):
+#     mult.append([(i * j) for j in range(column_amount)])
+# print(*mult, sep='\n')
+# #
+# mult = []
 
 for i in range(row_amount):
-    for j in range(column_amount):
-        print(str(mult[i][j]).ljust(3), end=' ')
-    print()
+    row = []
+    for j in range(i, column_amount):
+        row.append(i * j)
+    for k in range(i):
+        row.insert(k, mult[k][i])
+    mult.append(row)
+print()
+
+for row in mult:
+    print(*row)
+# 0 0 0  0  0   0      #0
+# 0 1 2  3  4   5      #1
+# 0 2 4  6  8  10      #2
+# 0 3 6  9  12 15      #3
+# 0 4 8  12 16 20      #4
+# 0 5 10 15 20 25      #5
+
+# [0, 0, 0, 0, 0, 0, 0]
+# [0, 1, 2, 3, 4, 5, 6]
+# [0, 0, 4, 6, 8, 10, 12]   mult[2][1] = mult[1][2]
+# [0, 0, 0, 9, 12, 15, 18]  mult[3][1] = mult[1][3], mult[3][2] = mult[2][3]
+# [0, 0, 0, 0, 16, 20, 24]  mult[4][1] = mult[1][4], mult[4][2] = mult[2][4], mult[4][3] = mult[3][4]
+# [0, 0, 0, 0, 0, 25, 30]
+
