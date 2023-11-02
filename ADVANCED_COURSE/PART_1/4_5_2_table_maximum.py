f2787cd4b34e56b83b00794a9deddca873527e7b
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-02 18:05:59 +0300

    Changed for to while loop type. Removed breaks.

--- ADVANCED_COURSE/PART_1/4_5_2_table_maximum.py
+++ ADVANCED_COURSE/PART_1/4_5_2_table_maximum.py
@@ -1,32 +1,34 @@
 row_amount = int(input())
 column_amount = int(input())
+
 result = ''
 matrix = []
 max_item_row = 0
 max_item_column = 0
 max_item = 0
 
-for i in range(row_amount):
+for _ in range(row_amount):
     row = input()
     row = [int(x) for x in row.split()]
     matrix.append(row)
 
-result = ''
 max_item = max(max(matrix, key=max))
-flag = False
-for i in range(row_amount):
-    for j in range(column_amount):
+max_found = False
+i = 0
+while not max_found and i <= row_amount - 1:
+    j = 0
+    while not max_found and j <= column_amount - 1:
         cur_item = matrix[i][j]
         if cur_item == max_item:
             result = f'{i} {j}'
-            flag = True
-            break
-    if flag:
-        break
+            max_found = True
+        j += 1
 
-print(matrix)
-print(max_item)
+    i += 1
+
 print(result)
 # matrix = [[0, 3, 2, 4], [2, 3, 5, 5], [5, 1, 2, 3]]
 # string_amount = 3
 # column_amount = 4
+# print(*matrix, sep='\n')
+# print(max_item)
