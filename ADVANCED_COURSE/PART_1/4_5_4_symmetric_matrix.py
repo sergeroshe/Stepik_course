122c069b4c0052152a5fdafe6947c967a1a1a136
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-02 19:23:32 +0300

    Changed loop type to while, renamed const

--- ADVANCED_COURSE/PART_1/4_5_4_symmetric_matrix.py
+++ ADVANCED_COURSE/PART_1/4_5_4_symmetric_matrix.py
@@ -1,25 +1,26 @@
-Y_MESSAGE = 'YES'
-N_MESSAGE = 'NO'
+POSITIVE_ANSWER = 'YES'
+NEGATIVE_ANSWER = 'NO'
 
 matrix_size = int(input())
 matrix = [input().split() for _ in range(matrix_size)]
-#
+
 # matrix = ['0 1 2'.split(),
 #           '1 2 7'.split(),
 #           '2 3 4'.split()]
 # matrix_size = len(matrix)
 
-# 0 1 2
-# 1 2 7
-# 2 3 4
-answer = Y_MESSAGE
-symmetric = True
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        symmetric = matrix[i][j] == matrix[j][i]
-    if not symmetric:
-        answer = N_MESSAGE
-        symmetric = False
-        break
+answer = POSITIVE_ANSWER
+is_symmetric = True
+i = 0
+while is_symmetric and i < len(matrix):
+    j = 0
+    while is_symmetric and j < len(matrix[i]):
+        is_symmetric = matrix[i][j] == matrix[j][i]
+        j += 1
+    i += 1
+
+if not is_symmetric:
+    answer = NEGATIVE_ANSWER
+    symmetric = False
 
 print(answer)
