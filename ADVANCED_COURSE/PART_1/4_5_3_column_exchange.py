e8c99b7e9caa01c9337ab631aa7bc0c6a72d7211
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-02 18:35:16 +0300

    Split logic and output.

--- ADVANCED_COURSE/PART_1/4_5_3_column_exchange.py
+++ ADVANCED_COURSE/PART_1/4_5_3_column_exchange.py
@@ -6,4 +6,7 @@ column_x_idx, column_y_idx = map(int, input().split())
 
 for _, row in enumerate(matrix):
     row[column_x_idx], row[column_y_idx] = row[column_y_idx], row[column_x_idx]
+
+
+for row in matrix:
     print(*row)
