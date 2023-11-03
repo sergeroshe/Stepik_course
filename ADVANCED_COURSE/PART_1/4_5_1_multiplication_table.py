fd9606267900ee7e4fff4b06182dd7a90a0e1fc4
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-03 12:00:47 +0300

    Renamed loop control variables

--- ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -10,10 +10,10 @@ mult = []
 
 for i in range(row_amount):
     row = []
-    for k in range(i):
-        row.append(mult[k][i])
-    for j in range(i, column_amount):
-        row.append(i * j)
+    for j in range(i):
+        row.append(mult[j][i])
+    for k in range(i, column_amount):
+        row.append(i * k)
 
     mult.append(row)
 print()
@@ -27,9 +27,23 @@ for row in mult:
 # 0 4 8  12 16 20      #4
 # 0 5 10 15 20 25      #5
 
-# [0, 0, 0, 0, 0, 0, 0]
-# [0, 1, 2, 3, 4, 5, 6]
-# [0, 0, 4, 6, 8, 10, 12]   mult[2][1] = mult[1][2]
+# 0 0 0     #0
+# 0 1 2     #1
+# 0 2 4     #2
+# 0 3 6     #3
+# 0 4 8     #4
+# 0 5 10    #5
+# 0 6 12    #6
+# 0 7 14    #7
+
+
+# 0 0 0    #0
+# 0 1 2    #1
+# 0 0 4    #2
+
+# [0, 0, 0]
+# [0, 1, 2]
+# [0, 0, 4]   mult[2][1] = mult[1][2]
 # [0, 0, 0, 9, 12, 15, 18]  mult[3][1] = mult[1][3], mult[3][2] = mult[2][3]
 # [0, 0, 0, 0, 16, 20, 24]  mult[4][1] = mult[1][4], mult[4][2] = mult[2][4], mult[4][3] = mult[3][4]
 # [0, 0, 0, 0, 0, 25, 30]
