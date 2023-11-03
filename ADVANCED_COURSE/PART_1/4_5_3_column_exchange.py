7ceadfc44afdb5ce703af28c0cbaf067f6d2bf35
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-03 14:25:15 +0300

    4_5_3 Column exchange task (#269)
    * Added task initial solution
    
    * Split logic and output.
    
    * Changed enumerate to simple element call

---
+++ ADVANCED_COURSE/PART_1/4_5_3_column_exchange.py
@@ -0,0 +1,12 @@
+row_amount = int(input())
+column_amount = int(input())
+
+matrix = [input().split() for i in range(row_amount)]
+column_x_idx, column_y_idx = map(int, input().split())
+
+for row in matrix:
+    row[column_x_idx], row[column_y_idx] = row[column_y_idx], row[column_x_idx]
+
+
+for row in matrix:
+    print(*row)
