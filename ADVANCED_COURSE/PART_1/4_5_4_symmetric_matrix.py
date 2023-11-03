7742e53663f4b36c75faf78449f8f5965857bbfa
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-03 14:29:09 +0300

    4_5_4 Symmetric matrix (#271)
    * Initial task solution
    
    * Changed loop type to while, renamed const
    
    * Removed unused var

---
+++ ADVANCED_COURSE/PART_1/4_5_4_symmetric_matrix.py
@@ -0,0 +1,25 @@
+POSITIVE_ANSWER = 'YES'
+NEGATIVE_ANSWER = 'NO'
+
+matrix_size = int(input())
+matrix = [input().split() for _ in range(matrix_size)]
+
+# matrix = ['0 1 2'.split(),
+#           '1 2 7'.split(),
+#           '2 3 4'.split()]
+# matrix_size = len(matrix)
+
+answer = POSITIVE_ANSWER
+is_symmetric = True
+i = 0
+while is_symmetric and i < len(matrix):
+    j = 0
+    while is_symmetric and j < len(matrix[i]):
+        is_symmetric = matrix[i][j] == matrix[j][i]
+        j += 1
+    i += 1
+
+if not is_symmetric:
+    answer = NEGATIVE_ANSWER
+
+print(answer)
