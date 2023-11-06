f6b72f35f80835e4b2cde366eeb49698294d8607
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-06 17:50:34 +0300

    4_5_1 Multiplication table (#265)
    * Added task solution with ljust method. Optional solution in comments
    
    * Transformed "for" cycle to list expression
    
    * Transformed "for" cycle to list expression
    
    * Renamed vars, added comment
    
    * Reduced multiplication operation count
    
    * Perf: Changed insert to append method.
    
    * Renamed loop control variables
    
    * Changed first loop range to min of values
    
    * Combined optimised calc with possible one
    
    * Fixed calc mistake, added comments
    
    * Fixed calc mistake, added comments

---
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -0,0 +1,31 @@
+# row_amount = int(input())
+# column_amount = int(input())
+row_amount = 7
+column_amount = 7
+matrix = []
+
+# Simple but ineffective solution:
+# for i in range(row_amount):
+#     mult.append([(i * j) for j in range(column_amount)])
+# print(*mult, sep='\n')
+
+# mult = []
+
+for i in range(min(row_amount, column_amount)):
+    row = []
+    for j in range(i):
+        row.append(matrix[j][i])
+    for k in range(i, column_amount):
+        row.append(i * k)
+    matrix.append(row)
+
+# Bellow we start filling in the matrix from the point where we stopped in the previous part.
+# For example, if row_amount is 8 and column_amount is 3, we start with 3, counting that 3 is the 4th element index.
+
+if column_amount < row_amount:
+    for l in range(column_amount, row_amount):
+        matrix.append([(m * l) for m in range(column_amount)])
+
+for row in matrix:
+    print(*row)
+
