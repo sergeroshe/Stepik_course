ba6cc9f07ddabb5d69fe4d92ffc889db4c64e1ad
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-06 14:27:15 +0300

    4_5_2 Table Maximum (#267)
    * Added task initial solution
    
    * Renamed vars
    
    * Changed for to while loop type. Removed breaks.
    
    * Split logic and output.
    
    * Removed extra calc
    
    * Removed unused var
    
    * Removed unused conversion

---
+++ ADVANCED_COURSE/PART_1/4_5_2_table_maximum.py
@@ -0,0 +1,31 @@
+row_amount = int(input())
+column_amount = int(input())
+# column_amount = 3
+# row_amount = 3
+
+matrix = []
+
+for _ in range(row_amount):
+    row = input()
+    row = [int(x) for x in row.split()]
+    matrix.append(row)
+
+# matrix = ['5 3 4'.split(),
+#           '2 3 0'.split(),
+#           '4 1 5'.split()]
+
+max_item = matrix[0][0]
+result = [0, 0]
+
+i = 0
+while i <= row_amount - 1:
+    j = 0
+    while j <= column_amount - 1:
+        if matrix[i][j] > max_item:
+            max_item = matrix[i][j]
+            result = [i, j]
+        j += 1
+
+    i += 1
+
+print(*result)
