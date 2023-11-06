316cd842752d3dc341cd32ee60ea2388af14b271
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-06 17:34:48 +0300

    Fixed calc mistake, added comments

--- ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
+++ ADVANCED_COURSE/PART_1/4_5_1_multiplication_table.py
@@ -1,53 +1,32 @@
-# row_amount = int(input())
-# column_amount = int(input())
-row_amount = 8
-column_amount = 3
-mult = []
+row_amount = int(input())
+column_amount = int(input())
+# row_amount = 8
+# column_amount = 2
+matrix = []
 
+# Simple but ineffective solution:
 # for i in range(row_amount):
 #     mult.append([(i * j) for j in range(column_amount)])
 # print(*mult, sep='\n')
-# #
+
 # mult = []
 
 for i in range(min(row_amount, column_amount)):
     row = []
     for j in range(i):
-        row.append(mult[j][i])
+        row.append(matrix[j][i])
     for k in range(i, column_amount):
         row.append(i * k)
-    mult.append(row)
-if column_amount != row_amount:
-    for i in range((row_amount % column_amount) + 1, row_amount):
-        mult.append([(i * j) for j in range(column_amount)])
+    matrix.append(row)
 
-for row in mult:
+# Bellow we start filling in the matrix from the point where we stopped in the previous part.
+# For example, if row_amount is 8 and column_amount is 3, we start with 3
+# as 8 - (8 - 3) = 3, counting that 3 is the 4th element index.
+
+if column_amount < row_amount:
+    for l in range(row_amount - (row_amount - column_amount), row_amount):
+        matrix.append([(m * l) for m in range(column_amount)])
+
+for row in matrix:
     print(*row)
-# 0 0 0  0  0   0      #0
-# 0 1 2  3  4   5      #1
-# 0 2 4  6  8  10      #2
-# 0 3 6  9  12 15      #3
-# 0 4 8  12 16 20      #4
-# 0 5 10 15 20 25      #5
-
-# 0 0 0     #0
-# 0 1 2     #1
-# 0 2 4     #2
-# 0 3 6     #3
-# 0 4 8     #4
-# 0 5 10    #5
-# 0 6 12    #6
-# 0 7 14    #7
-
-
-# 0 0 0    #0
-# 0 1 2    #1
-# 0 0 4    #2
-
-# [0, 0, 0]
-# [0, 1, 2]
-# [0, 0, 4]   mult[2][1] = mult[1][2]
-# [0, 0, 0, 9, 12, 15, 18]  mult[3][1] = mult[1][3], mult[3][2] = mult[2][3]
-# [0, 0, 0, 0, 16, 20, 24]  mult[4][1] = mult[1][4], mult[4][2] = mult[2][4], mult[4][3] = mult[3][4]
-# [0, 0, 0, 0, 0, 25, 30]
 
