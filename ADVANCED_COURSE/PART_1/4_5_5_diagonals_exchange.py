44b764ad81e2c7525ea26cd73e8e335ba8157270
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-08 14:50:01 +0300

    4_5_5 Diagonals exchange (#275)
    * Added initial task solution
    
    * Removed obsolete comment
    
    * Reduced cycles number
    
    * Added optional solution in comments
    
    * Refactoring: extracted var

---
+++ ADVANCED_COURSE/PART_1/4_5_5_diagonals_exchange.py
@@ -0,0 +1,43 @@
+matrix_size = int(input())
+matrix = []
+
+for _ in range(matrix_size):
+    matrix.append(input().split())
+
+for i in range(matrix_size // 2):
+    back_idx = -(i + 1)
+    matrix[i][i] = matrix[back_idx][i]
+    matrix[back_idx][back_idx] = matrix[i][back_idx]
+
+for row in matrix:
+    print(*row)
+
+# matrix_size = 5
+# matrix = ['2 2 3 1 3'.split(),
+#           '4 6 6 7 5'.split(),
+#           '7 8 9 7 8'.split(),
+#           '4 5 6 4 5'.split(),
+#           '1 2 3 1 2'.split()]
+# main_diagonal_left = matrix[i][i]
+# secondary_diagonal_left = matrix[-(i + 1)][i]
+# main_diagonal_right = matrix[-(i + 1)][-(i + 1)]
+# secondary_diagonal_right = matrix[i][-(i + 1)]
+
+# initial bad solution
+# for i in range(matrix_size // 2):
+#     for j in range(i + 1, matrix_size):
+#         if i + j + 1 == matrix_size:
+#             matrix[i][j], matrix[-(i + 1)][j] = matrix[-(i + 1)][j], matrix[i][j]
+
+
+# 2 2 3 1 3  0, -1; 1, -2; 2, -3 -> i, -(i + 1)
+# 4 6 6 7 5
+# 7 8 9 7 8
+# 4 5 6 4 5
+# 1 2 3 1 2 -1, 0;  -2, 1; -3, 2
+
+# 1 2 3 1 2   0, 0 -> -1, 0;   1, 1 -> -2, 1   1, -2 -> -2, -2
+# 4 5 6 4 5
+# 7 8 9 7 8
+# 4 6 6 7 5
+# 2 2 3 1 3
