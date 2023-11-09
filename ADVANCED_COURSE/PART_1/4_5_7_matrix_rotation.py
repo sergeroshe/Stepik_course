abbcd09080dc67cec461277477315bb8361245c8
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-09 16:38:53 +0300

    Added 'turn and flip' type solution

--- ADVANCED_COURSE/PART_1/4_5_7_matrix_rotation.py
+++ ADVANCED_COURSE/PART_1/4_5_7_matrix_rotation.py
@@ -1,47 +1,22 @@
-# matrix_size = int(input())
-# matrix = []
-#
-# for _ in range(matrix_size):
-#     matrix.append(input().split())
-matrix_size = 4
-matrix = ['1 2 3 4'.split(),
-          '5 6 7 8'.split(),
-          '8 6 4 2'.split(),
-          '3 4 5 6'.split()]
-# result = 3 8 5 1
-        #  4 6 6 2
-        #  5 4 7 3
-        #  6 2 8 4
+matrix_size = int(input())
+matrix = []
 
-# 0 0 = 0 -1, 0 1 = 1 -1, 0 -2 = -2 -1, 0 -1 = -1 -1,
-# 1 0 = 0 -2,
-# i = 0: i = 0, j = 0: j = -1
-# i + j = 3:
-# i = matrix_size - (i + j) -> 3-3=0;
-# j = i + matrix_size -> 0 + 3 = 3
-# for i in range(matrix_size):
-#     row = matrix[i]
-#     column = []
-#     for j in range(matrix_size):
-#         column.append(matrix[j][i])
-#     matrix[i] = column
-rotated = zip(*reversed(matrix))
-for row in rotated:
+for _ in range(matrix_size):
+    matrix.append(input().split())
+
+for i in range(matrix_size):
+    for j in range(i + 1, matrix_size):
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    matrix[i].reverse()
+
+for row in matrix:
     print(*row)
 
-# for i in range(matrix_size // 2):
-#     for j in range(matrix_size):
-#         matrix[i][j], matrix[-(i + 1)][j] = matrix[-(i + 1)][j], matrix[i][j]
-#
-# for row in matrix:
-#     print(*row)
-# print()
-#
 # matrix_size = 4
 # matrix = ['1 2 3 4'.split(),
 #           '5 6 7 8'.split(),
-#           '8 6 4 2'.split(),
-#           '3 4 5 6'.split()]
+#           '9 10 11 12'.split(),
+#           '13 14 15 16'.split()]
 # result_matrix = []
 # for i in range(matrix_size):
 #     row = []
@@ -49,12 +24,4 @@ for row in rotated:
 #         row.append(matrix[j][i])
 #     result_matrix.append(row)
 
-#
-#
-# matrix = ['1 2 3'.split(),
-#           '4 5 6'.split(),
-#           '7 8 9'.split()]
 
-# result = 7 4 1 [0][0] = [0][2], [0][1] = [1][2], [0][2] = [2][2],
-#          8 5 2 [1][2] = [2][1]
-#          9 6 3
