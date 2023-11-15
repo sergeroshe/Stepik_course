22247c1a7a25a45b6f6a1f63a7e6d5ba9a424dd1
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-15 17:45:11 +0300

    Added initial task solution

--- ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
+++ ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
@@ -1,39 +1,22 @@
-MATRIX_SIZE = 8
+CHESS_BOARD_SIZE = 8
 CHESS_KNIGHT_POSITION_MARK = 'N'
+FILLING_CHAR = '.'
 POSSIBLE_MOVE_MARK = '*'
-
-matrix = []
-
-position = 'b6'
+CHESS_KNIGHT_MOVE_RANGE = 2
+# matrix = []
+position = 'a1'
 # position = input()
 knight_x = ord(position[0]) - 97
-knight_y = MATRIX_SIZE - int(position[-1])
+knight_y = CHESS_BOARD_SIZE - int(position[-1])
 
-for i in range(MATRIX_SIZE):
-    row = ''
-    for j in range(MATRIX_SIZE):
-        # if i == 2 and j == 1:
-        if abs(knight_x - j) + abs(knight_y - i) == 0:
-            row += 'K '
-        elif (knight_y - i) * (knight_x - j) in [-2, 2]: #
-            row += '* '
-        else:
-            row += '. '
-    print(row.rstrip())
+matrix = [list(FILLING_CHAR * CHESS_BOARD_SIZE) for _ in range(CHESS_BOARD_SIZE)]
+matrix[knight_y][knight_x] = CHESS_KNIGHT_POSITION_MARK
 
-# for _ in range(MATRIX_SIZE):
-#     matrix.append('. . . . . . . .'.split())
-# move_1 = [knight_x + 2, knight_y + 1]
-# move_2 = [knight_x + 2, knight_y - 1]
-# move_3 = [knight_x - 2, knight_y + 1]
-# move_4 = [knight_x - 2, knight_y - 1]
-# move_5 = [knight_x + 1, knight_y + 2]
-# move_6 = [knight_x + 1, knight_y - 2]
-# move_7 = [knight_x - 2, knight_y + 1]
-# move_8 = [knight_x - 2, knight_y - 1]
+for i in range(CHESS_BOARD_SIZE):
+    row = []
+    for j in range(CHESS_BOARD_SIZE):
+        if abs(knight_y - i) * abs(knight_x - j) == CHESS_KNIGHT_MOVE_RANGE:
+            matrix[i][j] = POSSIBLE_MOVE_MARK
 
-
-# matrix[knight_y][knight_x] = CHESS_KNIGHT_POSITION_MARK
-#
-# for row in matrix:
-#     print(*row)
+for row in matrix:
+    print(*row)
