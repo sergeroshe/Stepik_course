35dcac0cdb492e55c94b90b4301d058146e91330
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-17 17:49:22 +0300

    Added consts, checks simplified

--- ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
+++ ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
@@ -1,23 +1,26 @@
-matrix_size = int(input())
-matrix = [input().split() for i in range(matrix_size)]
-# matrix = ['6 1 8'.split(),
-#           '3 5 7'.split(),
-#           '2 9 4'.split()]
+Y_ANSWER = 'YES'
+N_ANSWER = 'NO'
+# matrix_size = int(input())
+# matrix = [input().split() for i in range(matrix_size)]
+matrix = ['6 1 8'.split(),
+          '3 5 7'.split(),
+          '2 9 4'.split()]
 
 matrix_size = len(matrix)
 
 valid_num_list = list(range(1, matrix_size ** 2 + 1))
-result = "NO"
+result = N_ANSWER
 
 int_matrix = ([[int(matrix[i][j]) for j in range(matrix_size)] for i in range(matrix_size)])
 sorted_matrix = sorted([int(matrix[i][j]) for j in range(matrix_size) for i in range(matrix_size)])
-
+# check if all numbers are present
 all_numbers_present = valid_num_list == sorted_matrix
+
 if all_numbers_present:
     row_sum = 0
-    column_sum = 0
     main_diagonal_sum = 0
     secondary_diagonal_sum = 0
+
     rows_equal = True
     columns_equal = True
     row_column_sum_equal = True
@@ -28,13 +31,12 @@ if all_numbers_present:
             row_column_sum_equal and \
             rows_equal and columns_equal \
             and i < matrix_size:
+        # row sum calculation
         row_sum = sum(int_matrix[i])
-        if i < matrix_size - 1 and sum(int_matrix[i]) != sum(int_matrix[i + 1]):
-            rows_equal = False
 
         j = 0
         cur_column = []
-        column_sum = 0
+        cur_column_sum = 0
         while row_column_sum_equal and \
                 row_column_sum_equal and \
                 rows_equal and columns_equal \
@@ -45,21 +47,23 @@ if all_numbers_present:
             if i + j + 1 == matrix_size:
                 secondary_diagonal_sum += int(matrix[i][j])
             j += 1
-
-        if 0 > i < matrix_size - 1 and column_sum != sum(cur_column):
-            columns_equal = False
-
-        column_sum = sum(cur_column)
-
-        if column_sum != row_sum:
-            row_column_sum_equal = False
+        # current column sum calculation
+        cur_column_sum = sum(cur_column)
+        # all checks after first iteration
+        if 0 < i < matrix_size:
+            if sum(int_matrix[i]) != sum(int_matrix[i - 1]):
+                rows_equal = False
+            if cur_column_sum != sum(cur_column):
+                columns_equal = False
+            if cur_column_sum != row_sum:
+                row_column_sum_equal = False
 
         i += 1
-
+    # final checks
     if row_column_sum_equal \
             and columns_equal \
             and main_diagonal_sum == secondary_diagonal_sum \
             and row_sum == secondary_diagonal_sum:
-        result = 'YES'
-print(result)
+        result = Y_ANSWER
 
+print(result)
