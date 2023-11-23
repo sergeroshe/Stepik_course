57cb7138cd0224e5a745e2f9d02943e41c1b782b
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-23 18:59:33 +0300

    Changed row and column sums equality calculation approach

--- ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
+++ ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
@@ -1,34 +1,50 @@
 Y_ANSWER = 'YES'
 N_ANSWER = 'NO'
+MATRIX_MIN_ELEMENT = 1
 matrix_size = int(input())
-matrix = [input().split() for i in range(matrix_size)]
-# matrix = ['8 1 6'.split(),
-#           '3 5 7'.split(),
-#           '4 9 2'.split()]
-#
-# matrix_size = len(matrix)
+# matrix_size = 3
+# convert string list to int list
+matrix = []
 
-valid_num_list = list(range(1, matrix_size ** 2 + 1))
+for _ in range(matrix_size):
+
+    row = input().split()
+    int_row = [int(item) for item in row]
+    matrix.append(int_row)
+#
+# matrix = [[8, 1, 6],
+#           [3, 5, 7],
+#           [4, 9, 2]]
+
+main_diagonal_sum = 0
+secondary_diagonal_sum = 0
+first_column_sum = 0
+first_row_sum = sum(matrix[0])
+min_element = matrix[0][0]
+max_element = matrix[0][0]
+
+for i in range(matrix_size):
+    for j in range(matrix_size):
+        if i == 0:
+            first_column_sum += matrix[j][i]
+        # diagonal sums calculation
+        if i == j:
+            main_diagonal_sum += int(matrix[i][j])
+        if i + j + 1 == matrix_size:
+            secondary_diagonal_sum += int(matrix[i][j])
+        if matrix[i][j] < min_element:
+            min_element = matrix[i][j]
+        if matrix[i][j] > max_element:
+            max_element = matrix[i][j]
+
 result = N_ANSWER
 
-int_matrix = ([[int(matrix[i][j]) for j in range(matrix_size)] for i in range(matrix_size)])
-checked_num_list = []
-for i in range(len(int_matrix)):
-    for j in range(len(int_matrix)):
-        if int_matrix[i][j] not in checked_num_list:
-            checked_num_list.append(int_matrix[i][j])
-# check of matrix content requirements corresponding
-all_numbers_present = len(checked_num_list) == len(valid_num_list) and \
-    min(checked_num_list) == min(valid_num_list) and \
-    max(checked_num_list) == max(valid_num_list)
+# # check of matrix content requirements corresponding
+all_numbers_present = max_element == matrix_size ** 2 and \
+                      min_element == MATRIX_MIN_ELEMENT
+diagonals_equal = main_diagonal_sum == secondary_diagonal_sum
 
-if all_numbers_present:
-    main_diagonal_sum = 0
-    secondary_diagonal_sum = 0
-    prev_row_sum = 0
-    cur_row_sum = 0
-    prev_column_sum = 0
-
+if all_numbers_present and diagonals_equal:
     rows_equal = True
     columns_equal = True
     row_sum_column_sum_equal = True
@@ -36,41 +52,33 @@ if all_numbers_present:
 
     i = 0
     while row_sum_column_sum_equal and \
-            row_sum_column_sum_equal and \
             rows_equal and columns_equal \
-            and i < matrix_size:
-        prev_row_sum = sum(int_matrix[i - 1])
-        cur_row_sum = sum(int_matrix[i])
+            and i < matrix_size - 1:
+        prev_row_sum = first_row_sum
+        cur_row_sum = sum(matrix[i + 1])
 
         j = 0
-        prev_column_sum = 0
+        prev_column_sum = first_column_sum
         cur_column_sum = 0
         while row_sum_column_sum_equal and \
-                row_sum_column_sum_equal and \
                 rows_equal and columns_equal \
                 and j < matrix_size:
-            # diagonal sums calculation
-            if i == j:
-                main_diagonal_sum += int(matrix[i][j])
-            if i + j + 1 == matrix_size:
-                secondary_diagonal_sum += int(matrix[i][j])
+
             # column sums calculation
-            prev_column_sum += int_matrix[j][i - 1]
-            cur_column_sum += int_matrix[j][i]
+            cur_column_sum += matrix[j][i]
             j += 1
 
         # all checks after each iteration
-        if prev_row_sum != cur_row_sum:
-            rows_equal = False
-        if prev_column_sum != cur_column_sum:
-            columns_equal = False
-        if cur_column_sum != cur_row_sum:
-            row_sum_column_sum_equal = False
+        rows_equal = prev_row_sum == cur_row_sum
+        columns_equal = prev_column_sum == cur_column_sum
+        row_sum_column_sum_equal = cur_column_sum == cur_row_sum
+
+        prev_row_sum = cur_row_sum
+        prev_column_sum = cur_column_sum
 
         i += 1
     # final checks
-    if rows_equal and columns_equal and main_diagonal_sum == secondary_diagonal_sum \
-            and row_sum_column_sum_equal:
+    if rows_equal and columns_equal and row_sum_column_sum_equal:
         result = Y_ANSWER
 
 print(result)
