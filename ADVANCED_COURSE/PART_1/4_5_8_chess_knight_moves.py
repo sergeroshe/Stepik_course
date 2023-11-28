ebe6f3fda1c856436f04c4e3e7400f33499acc44
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-28 13:44:32 +0300

    Changed enumerate to simple list iteration

--- ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
+++ ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
@@ -18,49 +18,11 @@ possible_move_list = [[STRAIGHT_MOVE, TURN_MOVE], [-STRAIGHT_MOVE, TURN_MOVE],
                       [STRAIGHT_MOVE, -TURN_MOVE], [-STRAIGHT_MOVE, -TURN_MOVE],
                       [TURN_MOVE, STRAIGHT_MOVE], [-TURN_MOVE, STRAIGHT_MOVE],
                       [TURN_MOVE, -STRAIGHT_MOVE], [-TURN_MOVE, -STRAIGHT_MOVE]]
-for i, _ in enumerate(possible_move_list):
-    move_y = knight_y + possible_move_list[i][0]
-    move_x = knight_x + possible_move_list[i][1]
+for el in possible_move_list:
+    move_y = knight_y + el[0]
+    move_x = knight_x + el[1]
     if 0 <= move_x <= CHESS_BOARD_SIZE - 1 and 0 <= move_y <= CHESS_BOARD_SIZE - 1:
         matrix[move_y][move_x] = POSSIBLE_MOVE_MARK
 
 for row in matrix:
     print(*row)
-
-
-
-#
-#
-#
-# # 1
-# if knight_y + CHESS_KNIGHT_MOVE_RANGE <= CHESS_BOARD_SIZE - 1 and \
-#         knight_x + 1 <= CHESS_BOARD_SIZE - 1:
-#     matrix[knight_y + CHESS_KNIGHT_MOVE_RANGE][knight_x + 1] = POSSIBLE_MOVE_MARK
-# # 2
-# if knight_y + CHESS_KNIGHT_MOVE_RANGE <= CHESS_BOARD_SIZE - 1 and \
-#         knight_x - 1 >= 0:
-#     matrix[knight_y + CHESS_KNIGHT_MOVE_RANGE][knight_x - 1] = POSSIBLE_MOVE_MARK
-# # 3
-# if knight_y - CHESS_KNIGHT_MOVE_RANGE >= 0 and \
-#         knight_x + 1 <= CHESS_BOARD_SIZE - 1:
-#     matrix[knight_y - CHESS_KNIGHT_MOVE_RANGE][knight_x + 1] = POSSIBLE_MOVE_MARK
-# # 4
-# if knight_y - CHESS_KNIGHT_MOVE_RANGE >= 0 and \
-#         knight_x - 1 >= 0:
-#     matrix[knight_y - CHESS_KNIGHT_MOVE_RANGE][knight_x - 1] = POSSIBLE_MOVE_MARK
-# # 5
-# if knight_x + CHESS_KNIGHT_MOVE_RANGE <= CHESS_BOARD_SIZE - 1 and \
-#         knight_y + 1 <= CHESS_BOARD_SIZE - 1:
-#     matrix[knight_y + 1][knight_x + CHESS_KNIGHT_MOVE_RANGE] = POSSIBLE_MOVE_MARK
-# # 6
-# if knight_x + CHESS_KNIGHT_MOVE_RANGE <= CHESS_BOARD_SIZE - 1 and \
-#         knight_y - 1 >= 0:
-#     matrix[knight_y - 1][knight_x + CHESS_KNIGHT_MOVE_RANGE] = POSSIBLE_MOVE_MARK
-# # 7
-# if knight_x - CHESS_KNIGHT_MOVE_RANGE >= 0 and \
-#         knight_y + 1 >= 0:
-#     matrix[knight_y + 1][knight_x - CHESS_KNIGHT_MOVE_RANGE] = POSSIBLE_MOVE_MARK
-# # 8
-# if knight_x - CHESS_KNIGHT_MOVE_RANGE >= 0 and \
-#         knight_y - 1 >= 0:
-#     matrix[knight_y - 1][knight_x - CHESS_KNIGHT_MOVE_RANGE] = POSSIBLE_MOVE_MARK
