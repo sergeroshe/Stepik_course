44de87a0890d2128341e643627519e5ca034e8e7
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-11-28 14:12:14 +0300

    Renamed const to X_COORD_SHIFT

--- ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
+++ ADVANCED_COURSE/PART_1/4_5_8_chess_knight_moves.py
@@ -1,15 +1,14 @@
 CHESS_BOARD_SIZE = 8
-CHESS_KNIGHT_MOVE_RANGE = 2
 STRAIGHT_MOVE = 2
 TURN_MOVE = 1
-A_ASCII_IDX = 97
+X_COORD_SHIFT = ord('a')
 CHESS_KNIGHT_POSITION_MARK = 'N'
 FILLING_CHAR = '.'
 POSSIBLE_MOVE_MARK = '*'
-# position = 'f3'
+
 position = input()
 
-knight_x = ord(position[0]) - A_ASCII_IDX
+knight_x = ord(position[0]) - X_COORD_SHIFT
 knight_y = CHESS_BOARD_SIZE - int(position[-1])
 matrix = [list(FILLING_CHAR * CHESS_BOARD_SIZE) for _ in range(CHESS_BOARD_SIZE)]
 matrix[knight_y][knight_x] = CHESS_KNIGHT_POSITION_MARK
