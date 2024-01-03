acdbe1e99cce3facf2b6fc90c094afa0cfdc6b67
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-03 15:03:16 +0300

    Added sequence validation with is_sequence_valid func implementation

--- ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
+++ ADVANCED_COURSE/PART_1/4_5_8_magic_square.py
@@ -1,20 +1,20 @@
 Y_ANSWER = 'YES'
 N_ANSWER = 'NO'
 MATRIX_MIN_ELEMENT = 1
-# matrix_size = int(input())
-matrix_size = 3
+matrix_size = int(input())
+# matrix_size = 3
 # convert string list to int list
-# matrix = []
-#
-# for _ in range(matrix_size):
-#     row = input().split()
-#     int_row = [int(item) for item in row]
-#     matrix.append(int_row)
+matrix = []
 
-matrix = [[8, 1, 6],  # i = 0, j = len_mtrx - 1
-          [3, 5, 7],  # i = 1, j = len_mtrx - 2
-          [4, 9, 1]]  # i = 2, j = len_mtrx - 3
-first_row_sum = sum(matrix[0])
+for _ in range(matrix_size):
+    row = input().split()
+    int_row = [int(item) for item in row]
+    matrix.append(int_row)
+
+
+# matrix = [[8, 1, 6],  # i = 0, j = len_mtrx - 1
+#           [3, 5, 7],  # i = 1, j = len_mtrx - 2
+#           [4, 9, 1]]  # i = 2, j = len_mtrx - 3
 
 
 # check if all numbers are non-gap valid sequence with valid min and max, make a function
@@ -46,7 +46,6 @@ def is_sequence_valid(mtrx, expected_min):
 result = N_ANSWER
 
 
-# calculate diagonal sums
 def matrix_diagonal_sums(mtrx):
     len_mtrx = len(mtrx)
     main_diagonal_sum = 0
@@ -78,20 +77,24 @@ def is_matrix_equal(sequence, target_sum):
 
         i += 1
     return is_equal
-def get_row_sum(matrix, idx):
-    row_sum = sum(matrix[idx])
+
+
+def get_row_sum(mtrx, idx):
+    row_sum = sum(mtrx[idx])
     return row_sum
 
-main_diagonal_sum, secondary_diagonal_sum = matrix_diagonal_sums(matrix)
-matrix_diagonal_sums_equal = main_diagonal_sum == secondary_diagonal_sum
-if matrix_diagonal_sums_equal:
-    matrix_all_sum_equal = is_matrix_equal(matrix, main_diagonal_sum)
 
-    if matrix_all_sum_equal:
-        result = Y_ANSWER
+sequence_valid = is_sequence_valid(matrix, MATRIX_MIN_ELEMENT)
+if sequence_valid:
+    main_diag_sum, sec_diag_sum = matrix_diagonal_sums(matrix)
+    matrix_diagonal_sums_equal = main_diag_sum == sec_diag_sum
+    if matrix_diagonal_sums_equal:
+        matrix_all_sum_equal = is_matrix_equal(matrix, main_diag_sum)
+
+        if matrix_all_sum_equal:
+            result = Y_ANSWER
 
 print(result)
-
 
 # matrix with duplicate
 # # matrix = [[5, 3, 7],
