efaf8416fa306c30c2747069ff08db8f6c4adb03
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-11 17:26:53 +0300

    Added task solution

---
+++ ADVANCED_COURSE/PART_1/4_6_2_secondary_diagonal.py
@@ -0,0 +1,19 @@
+# matrix_size = int(input())
+matrix_size = 8
+
+matrix = []
+
+for i in range(matrix_size):
+    row = []
+    for j in range(matrix_size):
+        row.append(0)
+    matrix.append(row)
+
+
+for i in range(matrix_size):
+    matrix[-(i + 1)][i] = 1
+    for j in range(i + 1, matrix_size):
+        matrix[-(i + 1)][j] = 2
+
+for row in matrix:
+    print(*row, sep=' ')
