cd2567427ae7ef0a3ece1ffe7e1a970fb44be42d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-11 13:50:43 +0300

    Renamed file

---
+++ ADVANCED_COURSE/PART_1/4_6_1_chessboard.py
@@ -0,0 +1,17 @@
+BLANK_CELL_FILLING_CHAR = '.'
+COLOURED_CELL_FILLING_CHAR = '*'
+COLOURS = ['.', '*']
+row_amount, column_amount = [int(el) for el in input().split()]
+
+
+matrix = []
+
+for i in range(row_amount):
+    row = []
+    for j in range(column_amount):
+        row.append(COLOURS[(j + i) % 2])
+
+    matrix.append(row)
+
+for row in matrix:
+    print(*row)
