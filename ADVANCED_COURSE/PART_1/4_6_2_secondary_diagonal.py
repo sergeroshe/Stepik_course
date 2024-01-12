fd0e16c4c75a97b119c108b6996c46796df59d5d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-12 15:45:53 +0300

    Reduced matrix elements iteration number

--- ADVANCED_COURSE/PART_1/4_6_2_secondary_diagonal.py
+++ ADVANCED_COURSE/PART_1/4_6_2_secondary_diagonal.py
@@ -6,14 +6,19 @@ matrix = []
 for i in range(matrix_size):
     row = []
     for j in range(matrix_size):
-        row.append(0)
+        if i + j + 1 == matrix_size:
+            row.append(1)
+        elif i + j + 1 > matrix_size:
+            row.append(2)
+        else:
+            row.append(0)
     matrix.append(row)
 
 
-for i in range(matrix_size):
-    matrix[-(i + 1)][i] = 1
-    for j in range(i + 1, matrix_size):
-        matrix[-(i + 1)][j] = 2
+# for i in range(matrix_size):
+#     matrix[-(i + 1)][i] = 1
+#     for j in range(i + 1, matrix_size):
+#         matrix[-(i + 1)][j] = 2
 
 for row in matrix:
     print(*row, sep=' ')
