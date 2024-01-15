0f61a3512c2fa1c59130a936f355f21f23e9ed87
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-15 14:36:38 +0300

    4_6_5 Matrix filling in 3 (#297)
    * Added task solution
    
    * Refactoring

---
+++ ADVANCED_COURSE/PART_1/4_6_5_matrix_filling_in_5.py
@@ -0,0 +1,17 @@
+matrix_size = int(input())
+# matrix_size = 5
+
+matrix = []
+
+for i in range(matrix_size):
+    row = []
+    for j in range(matrix_size):
+        if i + j + 1 == matrix_size or i == j:
+            row.append(1)
+        else:
+            row.append(0)
+    matrix.append(row)
+
+
+for row in matrix:
+    print(*row, sep=' ')
