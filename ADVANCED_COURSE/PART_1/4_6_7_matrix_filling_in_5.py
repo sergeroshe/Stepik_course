9fc562f5c7093769f51336f775881b914cca20b9
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-18 16:39:22 +0300

    4_6_7 matrix filling in 5 (#306)
    * Added task solution
    
    * Removed unnecessary number
    
    * Removed hardcode numbers

---
+++ ADVANCED_COURSE/PART_1/4_6_7_matrix_filling_in_5.py
@@ -0,0 +1,17 @@
+row_amount, column_amount = [int(el) for el in input().split()]
+#
+# row_amount = 5
+# column_amount = 3
+matrix = []
+val_list = [i for i in range(1, column_amount + 1)]
+
+val_list_idx = 0
+for i in range(row_amount):
+    val_list_idx = i % column_amount
+    row = val_list[val_list_idx:] + val_list[:val_list_idx]
+    matrix.append(row)
+
+for i in range(row_amount):
+    for j in range(column_amount):
+        print(str(matrix[i][j]).ljust(3), end=' ')
+    print()
