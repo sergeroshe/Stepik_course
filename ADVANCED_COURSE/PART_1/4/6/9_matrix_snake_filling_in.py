78f1bf67507f3db0b5fab161a832b3301d7ff7b6
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-22 14:28:18 +0300

    Added comment

--- ADVANCED_COURSE/PART_1/4/6/9_matrix_snake_filling_in.py
+++ ADVANCED_COURSE/PART_1/4/6/9_matrix_snake_filling_in.py
@@ -11,11 +11,16 @@ for i in range(row_amount):
         row.append(k)
         k += 1
     if i % 2 > 0:
-        row.reverse()
+        row.reverse()  # last k + column_amount and down
     matrix.append(row)
+
+# 1   2   3
+# 6   5   4
+# 7   8   9
+# 12  11  10
+# 13  14  15
 
 for i in range(row_amount):
     for j in range(column_amount):
         print(str(matrix[i][j]).ljust(3), end=' ')
     print()
-
