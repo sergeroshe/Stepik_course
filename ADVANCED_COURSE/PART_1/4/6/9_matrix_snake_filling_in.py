04f430d7be12ab4b183370d87f8cf5704f825fb3
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-01-23 11:54:51 +0300

    4_6_8 Matrix Snake Filling In (#307)
    * Added task solution
    
    * Added comment
    
    * Added new var, removed reverse() method
    
    * Extracted calc to var
    
    * Renamed var to last_col_idx
    
    * Replaced if else statement with ternary operator
    
    * Removed hardcode

---
+++ ADVANCED_COURSE/PART_1/4/6/9_matrix_snake_filling_in.py
@@ -0,0 +1,20 @@
+row_amount, column_amount = [int(el) for el in input().split()]
+
+matrix = []
+k = 1
+last_col_idx = 0
+
+for i in range(row_amount):
+    row = []
+    last_col_idx = k - 1
+    row_even = i % 2 == 0
+    for j in range(column_amount):
+        row.append(k if row_even else last_col_idx + column_amount - j)
+        k += 1
+
+    matrix.append(row)
+
+for i in range(row_amount):
+    for j in range(column_amount):
+        print(str(matrix[i][j]).ljust(3), end=' ')
+    print()
