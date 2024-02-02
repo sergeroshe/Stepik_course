f8f714be16f0a4443cce578f2a6ac48b190805ab
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-02 16:53:27 +0300

    Added one more loop for lower right corner filling out

--- ADVANCED_COURSE/PART_1/4_6_9_matrix_diagonal_filling_in.py
+++ ADVANCED_COURSE/PART_1/4_6_9_matrix_diagonal_filling_in.py
@@ -1,31 +1,67 @@
-# rows, cols = [int(el) for el in input().split()]
+rows, cols = [int(el) for el in input().split()]
+# rows = 3
+# cols = 5
 
-rows = 6
-cols = 8
-
-num = 0
+num = 1
 
 matrix = [[0] * cols for _ in range(rows)]
-diagonal_count = rows + cols - 1
 
 for i in range(cols - 1):
-    inner_loop_start_range = 0
-    inner_loop_stop_range = (i + 1)
-    for j in range(inner_loop_start_range, inner_loop_stop_range):
-        num += 1
+    x = i + 1 if i + 1 <= rows else rows
+    for j in range(x):
         matrix[j][i - j] = num
-# add one more loop for the movement within the last column
+        num += 1
+
+for i in range(rows):
+    if rows - 1 - i >= cols - 1:
+        y = cols
+
+    else:
+        y = rows - i
+    k = 0
+    for j in range(i, i + y):
+        matrix[j][cols - 1 - k] = num
+        k += 1
+        num += 1
+
 for i in range(rows):
     for j in range(cols):
         print(str(matrix[i][j]).ljust(3), end=' ')
     print()
+
+
+# 1  2  (# 1: i = 0, j = 0;)  # 2:  i = 0, j = 1
+# 3  4  # 3: i = 1, j = 0;  # 4:  i = 1, j = 1
+# 5  6  # 5: i = 2, j = 0;  # 6:  i = 2, j = 1
+# 7  8  # 7: i = 3, j = 0;  # 8:  i = 3, j = 1
+# 9  10 # 9: i = 4, j = 0;  # 10: i = 4, j = 1
+
 
 # 3 4
 # 1  2  4  7
 # 3  5  8  10
 # 6  9  11 12
 
-# 1[0, 0]
+# 3 4
+# 1   2   4
+# 3   5   7
+# 6   8   10
+# 9   11  12
+
+# add one more loop for the movement within the last column
+
+
+# 1  2  4  7  10
+# 3  5  8  11 13
+# 6  9  12 14 15
+
+# i = 0, 1
+# i = 1, 2
+# i = 2, 3
+# i = 3, 3
+# i = 4, 3
+
+# 1[0, 0] 0, 1
 
 # 2[0, 1]
 # 3[1, 0]
