fc12f54bd1f289fefecb509086efdefc537cd124
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-07 12:42:21 +0300

    Extracted business logic to funcs

--- ADVANCED_COURSE/PART_1/4_7_1_matrix_addition.py
+++ ADVANCED_COURSE/PART_1/4_7_1_matrix_addition.py
@@ -1,23 +1,31 @@
-rows, columns = [int(el) for el in input().split()]
-matrix_1 = []
-for _ in range(rows):
-    row = [int(el) for el in input().split()]
-    matrix_1.append(row)
-    # extract to func
-empty_line = input()
-matrix_2 = []
-for _ in range(rows):
-    row = [int(el) for el in input().split()]
-    matrix_2.append(row)
+def mtrx_fill(rows):
+    mtrx = []
+    for _ in range(rows):
+        row = [int(el) for el in input().split()]
+        mtrx.append(row)
+    return mtrx
 
-matrix_sum = []
-for i in range(rows):
-    # extract to func matrix_add left_matrix and right_matrix
-    # -> result_matrix
-    row = [(matrix_1[i][j] + matrix_2[i][j]) for j in range(columns)]
-    matrix_sum.append(row)
 
-for i in range(rows):
-    for j in range(columns):
-        print(str(matrix_sum[i][j]).ljust(3), end=' ')
-    print()
+def mtrx_add(left_mtrx, right_mtrx):
+    rows = len(left_mtrx)
+    cols = len(left_mtrx[0])
+    result_mtrx = []
+    for i in range(rows):
+        row = [(left_mtrx[i][j] + right_mtrx[i][j]) for j in range(cols)]
+        result_mtrx.append(row)
+    return result_mtrx
+
+
+def main():
+    rows, columns = [int(el) for el in input().split()]
+    left_mtrx = mtrx_fill(rows)
+    empty_line = input()
+    right_mtrx = mtrx_fill(rows)
+    matrix_sum = mtrx_add(left_mtrx, right_mtrx)
+    for i in range(rows):
+        for j in range(columns):
+            print(str(matrix_sum[i][j]).ljust(3), end=' ')
+        print()
+
+
+main()
