3b5a1143f3f0851d32a7343d26153e65a71691cd
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-07 13:06:25 +0300

    Extracted business logic to funcs

--- ADVANCED_COURSE/PART_1/4_7_2_matrix_multiplication.py
+++ ADVANCED_COURSE/PART_1/4_7_2_matrix_multiplication.py
@@ -1,34 +1,45 @@
-matrix_1_rows, matrix_1_cols = [int(el) for el in input().split()]
-matrix_1 = []
-for _ in range(matrix_1_rows):
-    row = [int(el) for el in input().split()]
-    matrix_1.append(row)
-empty_line = input()
-
-matrix_2_rows, matrix_2_cols = [int(el) for el in input().split()]
-matrix_2 = []
-for _ in range(matrix_2_rows):
-    row = [int(el) for el in input().split()]
-    matrix_2.append(row)
-
-matrix_mult = []
-
-for k in range(matrix_1_rows):
-    row = []
-    for i in range(matrix_2_cols):
-        mult_amount = 0
-        for j in range(matrix_1_cols):
-            matrix_1_row_el = matrix_1[k][j]
-            matrix_2_col_el = matrix_2[j][i]
-            mult_amount += matrix_1_row_el * matrix_2_col_el
-        row.append(mult_amount)
-    matrix_mult.append(row)
+def mtrx_fill(rows):
+    mtrx = []
+    for _ in range(rows):
+        row = [int(el) for el in input().split()]
+        mtrx.append(row)
+    return mtrx
 
 
-for i in range(matrix_1_rows):
-    for j in range(matrix_2_cols):
-        print(str(matrix_mult[i][j]).ljust(3), end=' ')
-    print()
+def mtrx_mult(left_mtrx, right_mtrx):
+    mtrx_mult_ = []
+    left_mtrx_rows = len(left_mtrx)
+    left_mtrx_cols = len(left_mtrx[0])
+    right_mtrx_cols = len(right_mtrx[0])
+    for k in range(left_mtrx_rows):
+        row = []
+        for i in range(right_mtrx_cols):
+            mult_amount = 0
+            for j in range(left_mtrx_cols):
+                matrix_1_row_el = left_mtrx[k][j]
+                matrix_2_col_el = right_mtrx[j][i]
+                mult_amount += matrix_1_row_el * matrix_2_col_el
+            row.append(mult_amount)
+        mtrx_mult_.append(row)
+    return mtrx_mult_
+
+
+def main():
+    left_mtrx_rows, left_mtrx_cols = [int(el) for el in input().split()]
+
+    left_mtrx = mtrx_fill(left_mtrx_rows)
+    empty_line = input()
+    right_mtrx_rows, right_mtrx_cols = [int(el) for el in input().split()]
+    right_mtrx = mtrx_fill(right_mtrx_rows)
+    matrix_mult = mtrx_mult(left_mtrx, right_mtrx)
+
+    for i in range(left_mtrx_rows):
+        for j in range(right_mtrx_cols):
+            print(str(matrix_mult[i][j]).ljust(3), end=' ')
+        print()
+
+
+main()
 
 # matrix_1_rows, matrix_1_cols = 3, 2
 # matrix_1 = [[2, 5],
@@ -38,4 +49,3 @@ for i in range(matrix_1_rows):
 # matrix_2_rows, matrix_2_cols = 2, 3
 # matrix_2 = [[1, 2, 1],
 #             [0, 1, 0]]
-
