934b20d6e0ac928ec5fb53c2a573a4a62612049d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-13 12:36:31 +0300

    Added initial code

--- Advanced/calling_stored_procedure.py
+++ Advanced/calling_stored_procedure.py
@@ -1,2 +1,16 @@
-def main():
-    pass
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Mysql@_-',
+    port='3306',
+    database='classicmodels'
+)
+mycursor = mydb.cursor()
+users = mycursor.fetchall()
+
+for user in users:
+    print(user)
+    print('Username: ' + user[1])
+    print('Password: ' + user[2])
