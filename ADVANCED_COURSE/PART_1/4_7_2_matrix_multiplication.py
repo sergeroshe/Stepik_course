8f5f4783e31298bbfcfd388d9792fa4b55dea582
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-16 12:37:17 +0300

    4_7_2 Matrix multiplication (#312)
    * Added task solution
    
    * Extracted business logic to funcs
    
    * Removed unused var
    
    * Renamed vars to _amount based
    
    * Renamed vars to _amount based, func mtrx_print used
    
    * Renamed var to result_mtrx
    
    * Renamed var to row_el

---
+++ ADVANCED_COURSE/PART_1/4_7_2_matrix_multiplication.py
@@ -0,0 +1,58 @@
+MTRX_COL_WIDTH = 5
+
+
+def mtrx_fill(rows):
+    mtrx = []
+    for _ in range(rows):
+        row = [int(el) for el in input().split()]
+        mtrx.append(row)
+    return mtrx
+
+
+def mtrx_mult(left_mtrx, right_mtrx):
+    result_mtrx = []
+    left_mtrx_row_amount = len(left_mtrx)
+    left_mtrx_col_amount = len(left_mtrx[0])
+    right_mtrx_col_amount = len(right_mtrx[0])
+    for k in range(left_mtrx_row_amount):
+        row = []
+        for i in range(right_mtrx_col_amount):
+            row_el = 0
+            for j in range(left_mtrx_col_amount):
+                left_row_el = left_mtrx[k][j]
+                right_col_el = right_mtrx[j][i]
+                row_el += left_row_el * right_col_el
+            row.append(row_el)
+        result_mtrx.append(row)
+    return result_mtrx
+
+
+def mtrx_print(mtrx, col_width):
+    for row in mtrx:
+        for el in row:
+            print(str(el).ljust(col_width), end=' ')
+        print()
+
+
+def main():
+    left_mtrx_row_amount, left_mtrx_col_amount = [int(el) for el in input().split()]
+    left_mtrx = mtrx_fill(left_mtrx_row_amount)
+    input()
+    right_mtrx_row_amount, right_mtrx_col_amount = [int(el) for el in input().split()]
+    right_mtrx = mtrx_fill(right_mtrx_row_amount)
+
+    result_mtrx = mtrx_mult(left_mtrx, right_mtrx)
+
+    mtrx_print(result_mtrx, MTRX_COL_WIDTH)
+
+
+main()
+
+# matrix_1_rows, matrix_1_cols = 3, 2
+# matrix_1 = [[2, 5],
+#             [6, 7],
+#             [1, 8]]
+#
+# matrix_2_rows, matrix_2_cols = 2, 3
+# matrix_2 = [[1, 2, 1],
+#             [0, 1, 0]]
