415fa68b3ca064f550dd63ad3a0298bf8aa86cc8
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-22 13:17:38 +0300

    Calling stored procedures (#315)
    * Added main
    
    * Added initial code
    
    * Added calling stored proc code
    
    * Used param in stored procedure
    
    * Changed SQL query to salePerOrder view execution

---
+++ Advanced/calling_stored_procedure.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Mysql@_-',
+    port='3306',
+    database='classicmodels'
+)
+cursorObject = mydb.cursor()
+
+query = "SELECT * FROM salePerOrder LIMIT 3;"
+cursorObject.execute(query)
+
+query_result = cursorObject.fetchall()
+
+for el in query_result:
+    print(el)
+
+# disconnecting from server
+mydb.close()
+# show_customers_proc = 'CALL classicmodels.show_customers()'
+#
+# # table created
+# cursorObject.execute(show_customers_proc)
+#
+# # disconnecting from server
+# mydb.close()
+
+
+
