ee2b0bfdfa007990d9255a99d07df3aa70830f47
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-28 12:59:35 +0300

    Combined two funcs into one

--- ADVANCED_COURSE/PART_1/4_6_10_matrix_spiral_filling_in.py
+++ ADVANCED_COURSE/PART_1/4_6_10_matrix_spiral_filling_in.py
@@ -1,36 +1,29 @@
 MTRX_COL_WIDTH = 5
 
 
-def left_right_filling(mtrx, num, left, right, top):
+def left_right_top_bottom_filling(mtrx, num, left, right, top, bottom):
     for i in range(left, right + 1):
         mtrx[top][i] = num
         num += 1
     top += 1
-    return num, top
-
-
-def top_bottom_filling(mtrx, num, top, bottom, right):
     for i in range(top, bottom + 1):
         mtrx[i][right] = num
         num += 1
     right -= 1
-    return num, right
+    return num, top, right
 
 
-def right_left_filling(mtrx, num, right, left, bottom):
+def right_left_bottom_top_filling(mtrx, num, left, right, top, bottom):
     for i in range(right, left - 1, -1):
         mtrx[bottom][i] = num
         num += 1
     bottom -= 1
-    return num, bottom
 
-
-def bottom_top_filling(mtrx, num, bottom, top, left):
     for i in range(bottom, top - 1, -1):
         mtrx[i][left] = num
         num += 1
     left += 1
-    return num, left
+    return num, bottom, left
 
 
 def mtrx_print(mtrx, col_width):
@@ -53,12 +46,10 @@ def main():
     left = 0
     right = cols - 1
     while top <= bottom and left <= right:
-        num, top = left_right_filling(matrix, num, left, right, top)
-        num, right = top_bottom_filling(matrix, num, top, bottom, right)
-        if top <= bottom:
-            num, bottom = right_left_filling(matrix, num, right, left, bottom)
-        if left <= right:
-            num, left = bottom_top_filling(matrix, num, bottom, top, left)
+        num, top, right = left_right_top_bottom_filling(matrix, num, left, right, top, bottom)
+
+        if top <= bottom and left <= right:
+            num, bottom, left = right_left_bottom_top_filling(matrix, num, left, right, top, bottom)
 
     mtrx_print(matrix, MTRX_COL_WIDTH)
 
