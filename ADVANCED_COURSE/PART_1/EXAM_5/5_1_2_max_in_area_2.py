60cdedd6f8f90aa19aba56d9c19e5ba6694ed593
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-02-29 11:33:15 +0300

    Renamed func to mtrx_right_lower_corner_max

--- ADVANCED_COURSE/PART_1/EXAM_5/5_1_2_max_in_area_2.py
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_2_max_in_area_2.py
@@ -1,4 +1,3 @@
-
 def mtrx_fill(rows):
     mtrx = []
     for _ in range(rows):
@@ -7,7 +6,7 @@ def mtrx_fill(rows):
     return mtrx
 
 
-def mtrx_right_lower_corner(mtrx):
+def mtrx_right_lower_corner_max(mtrx):
     max_num = mtrx[0][-1]
     rows = len(mtrx)
     cols = len(mtrx[0])
@@ -25,8 +24,9 @@ def main():
     mtrx_size = int(input())
     mtrx = mtrx_fill(mtrx_size)
 
-    max_num = mtrx_right_lower_corner(mtrx)
+    max_num = mtrx_right_lower_corner_max(mtrx)
 
     print(max_num)
+
 
 main()
