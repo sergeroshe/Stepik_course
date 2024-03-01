fa3788078f1049c500de4499c0653d9e54a9f93e
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-01 21:31:02 +0300

    Debugged: matrix length - 1 and col_amount - i - 1 calcs in loops

--- ADVANCED_COURSE/PART_1/EXAM_5/5_1_5_symmetric-matrix.py
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_5_symmetric-matrix.py
@@ -15,10 +15,9 @@ def is_mtrx_symmetric(mtrx):
     row_amount = len(mtrx)
     col_amount = len(mtrx[0])
     i = 0
-
-    while is_symmetric and i < row_amount // 2:
+    while is_symmetric and i < row_amount - 1:
         j = 0
-        while is_symmetric and j < col_amount - i:
+        while is_symmetric and j < col_amount - i - 1:
             left_upper_el = mtrx[i][j]
             right_lower_el = mtrx[-(j + 1)][-(i + 1)]
             is_symmetric = left_upper_el == right_lower_el
@@ -31,11 +30,19 @@ def is_mtrx_symmetric(mtrx):
 def main():
     answer = POSITIVE_ANSWER
     # mtrx_size = 3
-    # mtrx_size = int(input())
-    # mtrx = mtrx_fill(mtrx_size)
-    mtrx = [[0, 1, 2],
-            [1, 2, 7],
-            [2, 3, 4]]
+    mtrx_size = int(input())
+    mtrx = mtrx_fill(mtrx_size)
+    # mtrx = [[1, 3, 4, 7, 5],  # 3: 0,1 -> 3: ;-2,-1 4: 0,2 -> -3, -1
+    #         [3, 5, 8, 5, 7],  #
+    #         [7, 4, 4, 8, 4],
+    #         [6, 5, 4, 5, 3],
+    #         [5, 1, 7, 3, 1]]
+
+    # 1 3 4 7 5
+    # 3 5 8 5 7
+    # 7 4 4 8 4
+    # 6 5 4 5 3
+    # 5 1 7 3 1
 
     if not is_mtrx_symmetric(mtrx):
         answer = NEGATIVE_ANSWER
