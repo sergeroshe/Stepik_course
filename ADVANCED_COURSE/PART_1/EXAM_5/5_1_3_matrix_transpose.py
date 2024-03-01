efd46682b1ff06b79481abb98ef12311a2fb0f0d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-01 13:49:12 +0300

    5_1_3 Matrix_transpose (#321)
    * Added task solution
    
    * Replaced logic from creating to modifying in func mtrx_row_col_interchange
    
    * Renamed func to mtrx_transpose
    
    * Refactoring

---
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_3_matrix_transpose.py
@@ -0,0 +1,36 @@
+MTRX_COL_WIDTH = 2
+
+
+def mtrx_fill(rows):
+    mtrx = []
+    for _ in range(rows):
+        row = [int(el) for el in input().split()]
+        mtrx.append(row)
+    return mtrx
+
+
+def mtrx_transpose(mtrx):
+    row_amount = len(mtrx)
+    col_amount = len(mtrx[0])
+    for i in range(row_amount):
+        for j in range(i + 1, col_amount):
+            mtrx[i][j], mtrx[j][i] = mtrx[j][i], mtrx[i][j]
+
+
+def mtrx_print(mtrx, col_width):
+    for row in mtrx:
+        for el in row:
+            print(str(el).ljust(col_width), end=' ')
+        print()
+
+
+def main():
+    matrix_size = int(input())
+    matrix = mtrx_fill(matrix_size)
+
+    mtrx_transpose(matrix)
+
+    mtrx_print(matrix, MTRX_COL_WIDTH)
+
+
+main()
