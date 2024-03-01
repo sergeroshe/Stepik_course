f29aa3447f0549517f95fdc97a8c07bc2d47ab88
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-01 13:35:08 +0300

    Refactoring

--- ADVANCED_COURSE/PART_1/EXAM_5/5_1_1_every_nth_element.py
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_1_every_nth_element.py
@@ -1,16 +1,11 @@
 input_string_list = input().split()
 skipped_el_amount = int(input())
-# input_string_list = 'a b c d e f g h i j k l m n'.split()
-# skipped_el_amount = 3
 
 input_string_list_len = len(input_string_list)
 result_list = []
 for start_idx in range(skipped_el_amount):
     sublist = input_string_list[start_idx::skipped_el_amount]
-
-    # for i in range(start_idx, input_string_list_len, skipped_el_amount):
-    #     sublist.append(input_string_list[i])
-    # result_list.append(sublist)
     result_list.append(sublist)
+
 print(result_list)
 
