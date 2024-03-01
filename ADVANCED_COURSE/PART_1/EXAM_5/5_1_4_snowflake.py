eddf46fdf3bbd3ad57e88c30af1efcd5d8fc015b
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-01 14:05:05 +0300

    5_1_4 Snowflake (#323)
    * Added task solution
    
    * Combined funcs two into one
    
    * Extracted calc out of loop
    
    * Introduced func mtrx_snowflake

---
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_4_snowflake.py
@@ -0,0 +1,53 @@
+MTRX_FILLING_CHAR = '.'
+SNOWFLAKE_FILLING_CHAR = '*'
+MTRX_COL_WIDTH = 5
+
+
+def mtrx_fill(mtrx_size, filling_char):
+    mtrx = []
+    for i in range(mtrx_size):
+        row = []
+        for j in range(mtrx_size):
+            row.append(filling_char)
+        mtrx.append(row)
+    return mtrx
+
+
+def mtrx_cross(mtrx, filling_char):
+    mtrx_size = len(mtrx)
+    mtrx_mid = mtrx_size // 2
+    for i in range(mtrx_size):
+        mtrx[mtrx_mid][i] = filling_char
+        mtrx[i][mtrx_mid] = filling_char
+
+
+def mtrx_diags(mtrx, filling_char):
+    mtrx_size = len(mtrx)
+    for i in range(mtrx_size):
+        mtrx[i][i] = filling_char
+        mtrx[-(i + 1)][i] = filling_char
+
+
+def mtrx_snowflake(mtrx, filling_char):
+    mtrx_cross(mtrx, filling_char)
+    mtrx_diags(mtrx, filling_char)
+
+
+def mtrx_print(mtrx, col_width):
+    for row in mtrx:
+        for el in row:
+            print(str(el).ljust(col_width), end='')
+        print()
+
+
+def main():
+    mtrx_size = int(input())
+    # mtrx_size = 5
+    mtrx = mtrx_fill(mtrx_size, MTRX_FILLING_CHAR)
+
+    mtrx_snowflake(mtrx, SNOWFLAKE_FILLING_CHAR)
+
+    mtrx_print(mtrx, MTRX_COL_WIDTH)
+
+
+main()
