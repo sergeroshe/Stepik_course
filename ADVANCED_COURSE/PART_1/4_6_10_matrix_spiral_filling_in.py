d13617023fb7192cc6e8b32711f476255a92e620
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-05 13:26:11 +0300

    Changed iteration to list based

--- ADVANCED_COURSE/PART_1/4_6_10_matrix_spiral_filling_in.py
+++ ADVANCED_COURSE/PART_1/4_6_10_matrix_spiral_filling_in.py
@@ -1,18 +1,40 @@
 MTRX_COL_WIDTH = 5
 
 
+#
 def left_corner_bottom_filling(mtrx, num, start_hor_idx, stop_hor_idx, start_vert_idx, stop_vert_idx):
     # left to right movement
-    for i in range(start_hor_idx, stop_hor_idx + 1):
-        mtrx[start_vert_idx][i] = num
+    range_list = [[start_hor_idx, stop_hor_idx],
+                  [start_vert_idx, stop_vert_idx]]
+    for i in range(range_list[0][0], range_list[0][1] + 1):
+        mtrx[range_list[1][0]][i] = num
         num += 1
-    start_vert_idx += 1
+    # start_vert_idx += 1
+    range_list[1][0] += 1
+    # print(range_list[1][0])
     # top to bottom movement
-    for i in range(start_vert_idx, stop_vert_idx + 1):
-        mtrx[i][stop_hor_idx] = num
+    for i in range(range_list[1][0], range_list[1][1] + 1):
+        mtrx[i][range_list[0][1]] = num
         num += 1
-    stop_hor_idx -= 1
-    return num, start_vert_idx, stop_hor_idx
+    # stop_hor_idx -= 1
+    range_list[0][1] -= 1
+    # print(range_list[0][1])
+
+    return num, range_list[1][0], range_list[0][1]
+
+
+# def left_corner_bottom_filling(mtrx, num, start_hor_idx, stop_hor_idx, start_vert_idx, stop_vert_idx):
+#     # left to right movement
+#     for i in range(start_hor_idx, stop_hor_idx + 1):
+#         mtrx[start_vert_idx][i] = num
+#         num += 1
+#     start_vert_idx += 1
+#     # top to bottom movement
+#     for i in range(start_vert_idx, stop_vert_idx + 1):
+#         mtrx[i][stop_hor_idx] = num
+#         num += 1
+#     stop_hor_idx -= 1
+#     return num, start_vert_idx, stop_hor_idx
 
 
 def right_corner_top_filling(mtrx, num, start_hor_idx, stop_hor_idx, start_vert_idx, stop_vert_idx):
@@ -37,9 +59,9 @@ def mtrx_print(mtrx, col_width):
 
 
 def main():
-    rows = 5
-    cols = 7
-    # rows, cols = [int(el) for el in input().split()]
+    # rows = 5
+    # cols = 7
+    rows, cols = [int(el) for el in input().split()]
 
     matrix = [[0] * cols for _ in range(rows)]
 
