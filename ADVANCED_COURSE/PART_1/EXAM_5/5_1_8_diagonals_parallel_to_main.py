76ffa9fff939f64702fa8b716f0239b40ab5de54
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-14 11:03:37 +0300

    Added initial task solution

--- ADVANCED_COURSE/PART_1/EXAM_5/5_1_8_diagonals_parallel_to_main.py
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_8_diagonals_parallel_to_main.py
@@ -6,6 +6,21 @@ def mtrx_fill(mtrx_size, filling_char):
     return mtrx
 
 
+def mtrx_diagonals_parallel_to_main(mtrx):
+    mtrx_size = len(mtrx)
+    for i in range(mtrx_size):
+        for j in range(mtrx_size):
+            mtrx[i][i] = 0
+            num = 1
+            for k in range(i + 1, mtrx_size):
+                mtrx[i][k] = num
+                num += 1
+            num = 0
+            for l in range(i, -1, -1):
+                mtrx[i][l] = num
+                num += 1
+
+
 def mtrx_print(mtrx, col_width):
     for row in mtrx:
         for el in row:
@@ -14,9 +29,10 @@ def mtrx_print(mtrx, col_width):
 
 
 def main():
-    # matrix_size = int(input())
-    matrix_size = 6
-    matrix = mtrx_fill(matrix_size, '0')
+    matrix_size = int(input())
+    # matrix_size = 6
+    matrix = mtrx_fill(matrix_size, '*')
+    mtrx_diagonals_parallel_to_main(matrix)
     mtrx_print(matrix, MTRX_COL_WIDTH)
 
 
