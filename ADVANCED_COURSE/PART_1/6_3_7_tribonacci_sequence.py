722ff641f1a2c72f849028bfafe9eafcdf42e927
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-03-29 15:26:39 +0300

    Changed print option to program

--- ADVANCED_COURSE/PART_1/6_3_7_tribonacci_sequence.py
+++ ADVANCED_COURSE/PART_1/6_3_7_tribonacci_sequence.py
@@ -1,9 +1,7 @@
-# seq_size = 10
 seq_size = int(input())
 cur_el, next_el, after_next_el = 1, 1, 1
 seq = []
 for _ in range(seq_size):
-    seq.append(cur_el)
+    print(cur_el, end=' ')
     cur_el, next_el, after_next_el = next_el, after_next_el, cur_el + next_el + after_next_el
 
-print(*seq, sep=' ')
