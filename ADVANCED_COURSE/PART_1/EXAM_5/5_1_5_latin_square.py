284638836d093ab21b4f6c99f57aa8254e6c30fd
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-05-06 12:43:28 +0300

    Added move_x and move_y vars, changed shift elements to digits

--- ADVANCED_COURSE/PART_1/EXAM_5/5_1_5_latin_square.py
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_5_latin_square.py
@@ -18,12 +18,13 @@ def is_sequence_valid(mtrx, seq_num, seq_type, expected_min=1):
     seq_valid = True
     mtrx_size = len(mtrx)
 
-    i = 0
     # extract if condition
-    shift = [seq_num, i] if seq_type == COL_TYPE else [i, seq_num]
-    while i < mtrx_size and seq_valid:
-        # shift = [seq_num, i] if seq_type == COL_TYPE else [i, seq_num]
-        num = mtrx[shift[0]][shift[1]]
+    move_x = seq_num if seq_type == ROW_TYPE else 0
+    move_y = seq_num if seq_type == COL_TYPE else 0
+
+    shift = [0, 1] if seq_type == COL_TYPE else [1, 0]
+    while move_x < mtrx_size and move_y < mtrx_size and seq_valid:
+        num = mtrx[move_y][move_x]
         if expected_min <= num <= mtrx_size:
             if num not in checked_num_list:
                 checked_num_list.append(num)
@@ -31,7 +32,8 @@ def is_sequence_valid(mtrx, seq_num, seq_type, expected_min=1):
                 seq_valid = False
         else:
             seq_valid = False
-        i += 1
+        move_x += shift[1]
+        move_y += shift[0]
 
     return seq_valid
 
@@ -51,17 +53,17 @@ def is_latin_square(mtrx):
 
 
 def main():
-    # matrix_size = int(input())
-    matrix_size = 4
-    # mtrx = mtrx_fill(matrix_size)
-    mtrx = [[2, 3, 4, 1],
-            [3, 4, 1, 2],
-            [4, 1, 2, 3],
-            [1, 2, 3, 4]]
-    # 2 3 4 1
-    # 3 4 1 2
-    # 4 1 2 3
-    # 1 2 3 4
+    matrix_size = int(input())
+    # matrix_size = 3
+    mtrx = mtrx_fill(matrix_size)
+    # mtrx = [[2, 3, 4, 1],   # 0, 1
+    #         [3, 4, 1, 2],   # 1, 1
+    #         [4, 1, 2, 3],   # 2,
+    #         [1, 2, 3, 4]]
+    # mtrx = [[1, 2, 3],
+    #         [3, 2, 1],
+    #         [2, 3, 4]]
+
 
     answer = Y_ANSWER
 
