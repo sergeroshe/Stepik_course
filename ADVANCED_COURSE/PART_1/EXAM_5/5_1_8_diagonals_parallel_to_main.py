b4c738ec0c1de12af30e2af147b1de1bfa37922f
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-05-09 11:47:15 +0300

    5_1_8 Diagonals parallel to main (#331)
    * Initial commit
    
    * Added initial task solution
    
    * Reduced loop number
    
    * Replaced if, elif conditions with logical algorithm
    
    * Added comment
    
    * Added diagonals calculation
    
    * Refactoring
    
    * Added TODO
    
    * Changed moving method to shift based
    
    * Extracted x and y shifts to arguments
    
    * Removed unclear var start
    
    * Renamed const to SHIFT
    
    * Removed unused shift_list
    
    * Added consts for x and y shifts

---
+++ ADVANCED_COURSE/PART_1/EXAM_5/5_1_8_diagonals_parallel_to_main.py
@@ -0,0 +1,42 @@
+MTRX_COL_WIDTH = 5
+X_SHIFT = 1
+Y_SHIFT = 1
+
+
+def mtrx_fill(mtrx_size, filling_char):
+    mtrx = [list(filling_char * mtrx_size) for _ in range(mtrx_size)]
+    return mtrx
+
+
+# extract shift for x and y to arguments
+def mtrx_line_fill(x, y, x_shift, y_shift, mtrx, filling_char):
+    mtrx_size = len(mtrx)
+    x_move = x
+    y_move = y
+    while 0 <= y_move <= mtrx_size - 1 \
+            and 0 <= x_move <= mtrx_size - 1:
+        mtrx[y_move][x_move] = filling_char
+        x_move += x_shift
+        y_move += y_shift
+
+
+def mtrx_print(mtrx, col_width):
+    for row in mtrx:
+        for el in row:
+            print(str(el).ljust(col_width), end='')
+        print()
+
+
+def main():
+    # matrix_size = int(input())
+    matrix_size = 7
+    matrix = mtrx_fill(matrix_size, '*')
+
+    for i in range(matrix_size):
+        mtrx_line_fill(0, i, X_SHIFT, Y_SHIFT, matrix, i)
+        mtrx_line_fill(i, 0, X_SHIFT, Y_SHIFT, matrix, i)
+
+    mtrx_print(matrix, MTRX_COL_WIDTH)
+
+
+main()
