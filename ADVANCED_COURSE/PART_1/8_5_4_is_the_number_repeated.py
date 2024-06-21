01f18bf96dcb60a0332cb2706e7f5857a6ed6428
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-06-21 15:47:41 +0300

    Adding an element to set moved further

--- ADVANCED_COURSE/PART_1/8_5_4_is_the_number_repeated.py
+++ ADVANCED_COURSE/PART_1/8_5_4_is_the_number_repeated.py
@@ -2,7 +2,7 @@ POSITIVE_ANSWER = 'YES'
 NEGATIVE_ANSWER = 'NO'
 
 num_string_list = input().split()
-# string_list = '1 2 3 002 03 4 5 05'.split()
+# num_string_list = '10 5 48 6 38 1'.split()
 num_string_list = [el.lstrip('0') for el in num_string_list]
 
 non_repeated_num_set = set()
@@ -10,9 +10,10 @@ answer_list = []
 
 for num in num_string_list:
     if num not in non_repeated_num_set:
-        non_repeated_num_set.add(num)
         answer_list.append(NEGATIVE_ANSWER)
     else:
         answer_list.append(POSITIVE_ANSWER)
+    non_repeated_num_set.add(num)
+
 
 print(*answer_list, sep='\n')
