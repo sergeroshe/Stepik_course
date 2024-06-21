a5efe552da90766f02fadaa5ed4b62446e3f70e3
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-06-21 13:24:08 +0300

    Added task solution

---
+++ ADVANCED_COURSE/PART_1/8_5_4_is_the_number_repeated.py
@@ -0,0 +1,19 @@
+POSITIVE_ANSWER = 'YES'
+NEGATIVE_ANSWER = 'NO'
+
+num_string_list = input().split()
+# string_list = '1 2 3 002 03 4 5 05'.split()
+num_string_list = [el.lstrip('0') for el in num_string_list]
+
+non_repeated_num_set = set()
+answer_list = []
+
+for num in num_string_list:
+    if num not in non_repeated_num_set:
+        non_repeated_num_set.add(num)
+        answer_list.append(NEGATIVE_ANSWER)
+    else:
+        answer_list.append(POSITIVE_ANSWER)
+
+# print(non_repeated_num_set)
+print(*answer_list, sep='\n')
