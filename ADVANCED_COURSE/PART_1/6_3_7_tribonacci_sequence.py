c053078b432d1a268521f24e463f31f15160d6cf
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-02 13:01:10 +0300

    Refactoring

--- ADVANCED_COURSE/PART_1/6_3_7_tribonacci_sequence.py
+++ ADVANCED_COURSE/PART_1/6_3_7_tribonacci_sequence.py
@@ -1,7 +1,7 @@
 seq_size = int(input())
-cur_el, next_el, after_next_el = 1, 1, 1
+cur_el, next_el, next_next_el = 1, 1, 1
 seq = []
 for _ in range(seq_size):
     print(cur_el, end=' ')
-    cur_el, next_el, after_next_el = next_el, after_next_el, cur_el + next_el + after_next_el
+    cur_el, next_el, next_next_el = next_el, next_next_el, cur_el + next_el + next_next_el
 
