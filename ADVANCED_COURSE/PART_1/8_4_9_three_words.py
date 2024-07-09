a3c4c62ad908eb6923aae1d659e96a48d3eb9823
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-09 11:58:23 +0300

    Refactoring

--- ADVANCED_COURSE/PART_1/8_4_9_three_words.py
+++ ADVANCED_COURSE/PART_1/8_4_9_three_words.py
@@ -3,17 +3,19 @@ N_ANSWER = 'NO'
 
 answer = Y_ANSWER
 
-word_list = [set(word) for word in input().split()]
+word_set_list = [set(word) for word in input().split()]
 
 # word_list = [set(word) for word in 'автор товар отвар'.split()]
-len_word_list = len(word_list)
+word_set_list_len = len(word_set_list)
 
 char_set_equal = True
 
 i = 0
-while char_set_equal and i <= len_word_list - 1:
-    char_set_equal = word_list[i] == word_list[i - 1]
+while char_set_equal and i <= word_set_list_len - 1:
+    char_set_equal = word_set_list[i] == word_set_list[i - 1]
     i += 1
+
 if not char_set_equal:
     answer = N_ANSWER
+
 print(answer)
