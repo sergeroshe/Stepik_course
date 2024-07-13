c6044fdff0fc936ae02a51e37505fc3665958a5c
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-13 19:06:08 +0300

    Added task solution

---
+++ ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
@@ -0,0 +1,13 @@
+lesson_amount = int(input())
+
+general_attendance_list = []
+all_lessons_presented_student_list_set = set()
+
+for _ in range(lesson_amount):
+    current_lesson_attendance_list_set = {input() for _ in range(int(input()))}
+    general_attendance_list.append(current_lesson_attendance_list_set)
+    if general_attendance_list:
+        all_lessons_presented_student_list_set = general_attendance_list[0]
+        all_lessons_presented_student_list_set &= current_lesson_attendance_list_set
+
+print(*sorted(all_lessons_presented_student_list_set), sep='\n')
