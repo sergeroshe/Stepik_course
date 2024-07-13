75c4364071755503ba67542919b15318206a1284
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-13 19:16:23 +0300

    Refactoring

--- ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
+++ ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
@@ -1,13 +1,13 @@
 lesson_amount = int(input())
 
 general_attendance_list = []
-all_lessons_presented_student_list_set = set()
+all_lessons_presented_student_set = set()
 
 for _ in range(lesson_amount):
-    current_lesson_attendance_list_set = {input() for _ in range(int(input()))}
-    general_attendance_list.append(current_lesson_attendance_list_set)
-    all_lessons_presented_student_list_set = general_attendance_list[0]
+    current_lesson_attendance_set = {input() for _ in range(int(input()))}
+    general_attendance_list.append(current_lesson_attendance_set)
+    all_lessons_presented_student_set = general_attendance_list[0]
     if general_attendance_list:
-        all_lessons_presented_student_list_set &= current_lesson_attendance_list_set
+        all_lessons_presented_student_set &= current_lesson_attendance_set
 
-print(*sorted(all_lessons_presented_student_list_set), sep='\n')
+print(*sorted(all_lessons_presented_student_set), sep='\n')
