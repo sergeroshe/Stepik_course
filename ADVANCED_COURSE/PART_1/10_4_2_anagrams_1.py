dd58d586127a004ac32cb03a664ebda0a42c465f
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-27 19:43:44 +0300

    Replaced: two separate cycles with two nested

--- ADVANCED_COURSE/PART_1/10_4_2_anagrams_1.py
+++ ADVANCED_COURSE/PART_1/10_4_2_anagrams_1.py
@@ -2,29 +2,16 @@ WORD_AMOUNT = 2
 POSITIVE_ANSWER = 'YES'
 NEGATIVE_ANSWER = 'NO'
 
-input_string_dict_list = []
-
-word_1 = input()
-# word_1 = 'cat'
-
-word_1_dict = {}
-for letter in word_1:
-    word_1_dict[letter] = word_1_dict.get(letter, 0) + 1
-
-word_2 = input()
-# word_2 = 'rat'
-
-
-word_2_dict = {}
-for letter in word_2:
-    word_2_dict[letter] = word_2_dict.get(letter, 0) + 1
-
 answer = POSITIVE_ANSWER
 
-if word_1_dict != word_2_dict:
+word_dcts = [{} for _ in range(WORD_AMOUNT)]
+
+for dct in word_dcts:
+    word = input()
+    for letter in word:
+        dct[letter] = dct.get(letter, 0) + 1
+
+if word_dcts[0] != word_dcts[1]:
     answer = NEGATIVE_ANSWER
 
 print(answer)
-
-
-
