1ebf7d047c214158646979431d494455dc626b61
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-29 17:33:17 +0300

    Replaced input data list with dict

--- ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
+++ ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
@@ -1,16 +1,14 @@
 countries_info_amount = int(input())
-countries_info_list = [input().split() for _ in range(countries_info_amount)]
+
+countries_info_dict = {}
+for _ in range(countries_info_amount):
+    country, *cities = input().split()
+    countries_info_dict[country] = cities
+
 cities_amount = int(input())
 cities_list = [input() for _ in range(cities_amount)]
 
-countries_info_dict = {}
-
-
-for country, *cities in countries_info_list:
-    countries_info_dict[country] = cities
-
 countries_list = []
-
 
 for city in cities_list:
     for country, cities in countries_info_dict.items():
@@ -19,11 +17,4 @@ for city in cities_list:
             break
 
 print(*countries_list, sep='\n')
-print(countries_info_dict)
 
-# countries_info_amount = 2
-# countries_info_list = ['Германия Берлин Мюнхен Гамбург Дортмунд',
-#                        'Нидерланды Амстердам Гаага Роттердам Алкмар'
-#                        ]
-# cities_amount = 4
-# cities_list = ['Амстердам', 'Алкмар', 'Гамбург', 'Гаага']
