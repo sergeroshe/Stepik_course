185b121212525e9cfd2aea963a11d9a43ddbd09a
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-29 17:17:30 +0300

    Changed dict creation to unpacking method

--- ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
+++ ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
@@ -1,13 +1,12 @@
 countries_info_amount = int(input())
-countries_info_list = [input() for _ in range(countries_info_amount)]
+countries_info_list = [input().split() for _ in range(countries_info_amount)]
 cities_amount = int(input())
 cities_list = [input() for _ in range(cities_amount)]
 
 countries_info_dict = {}
 
-for country_info in countries_info_list:
-    country, cities = country_info[:country_info.index(' ')], \
-        country_info[country_info.index(' ') + 1:].split()
+
+for country, *cities in countries_info_list:
     countries_info_dict[country] = cities
 
 countries_list = []
@@ -20,3 +19,11 @@ for city in cities_list:
             break
 
 print(*countries_list, sep='\n')
+print(countries_info_dict)
+
+# countries_info_amount = 2
+# countries_info_list = ['Германия Берлин Мюнхен Гамбург Дортмунд',
+#                        'Нидерланды Амстердам Гаага Роттердам Алкмар'
+#                        ]
+# cities_amount = 4
+# cities_list = ['Амстердам', 'Алкмар', 'Гамбург', 'Гаага']
