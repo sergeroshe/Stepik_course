72f6720c29bf7e848aaf6b703c353c6db15dcbbb
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-29 13:01:22 +0300

    Changed for cycle to while for performance reason

--- ADVANCED_COURSE/PART_1/10_4_4_synonim_dictionary.py
+++ ADVANCED_COURSE/PART_1/10_4_4_synonim_dictionary.py
@@ -1,15 +1,25 @@
 input_string_amount = int(input())
+# input_string_amount = 1
+
+input_string_list = [(input().split()) for _ in range(input_string_amount)]
+translating_word = input()
+# translating_word = 'Pretty'
 
 synonim = ''
 input_string_dict = {}
-for _ in range(input_string_amount):
-    key, value = input().split()
-    input_string_dict[key], input_string_dict[value] = value, key
 
-translating_word = input()
+synonim_not_found = True
+i = 0
 
-synonim = input_string_dict[translating_word]
+while synonim_not_found and i < len(input_string_list):
+    key, value = input_string_list[i]
+    if key == translating_word:
+        synonim = value
+        synonim_not_found = False
+    elif value == translating_word:
+        synonim = key
+        synonim_not_found = False
+    i += 1
 
 
-print(input_string_dict)
 print(synonim)
