8a21771ab0e331b4a380c8f31e1e003cfcef3ee6
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-07-29 17:51:39 +0300

    Changed dict creation method to dict generation

--- ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
+++ ADVANCED_COURSE/PART_1/10_4_5_countries_and_cities.py
@@ -1,22 +1,16 @@
-countries_info_amount = int(input())
+country_info_amount = int(input())
+country_info_dict = {country: cities for _ in range(country_info_amount)
+                     for country, *cities in [input().split()]}
 
-countries_info_dict = {}
-for _ in range(countries_info_amount):
-    country, *cities = input().split()
-    countries_info_dict[country] = cities
-
-# d = {i : j for _ in range(int(input())) for i, *j in  [input().split()]}
-
-cities_amount = int(input())
-cities_list = [input() for _ in range(cities_amount)]
+city_amount = int(input())
+city_list = [input() for _ in range(city_amount)]
 
 countries_list = []
 
-for city in cities_list:
-    for country, cities in countries_info_dict.items():
+for city in city_list:
+    for country, cities in country_info_dict.items():
         if city in cities:
             countries_list.append(country)
             break
 
 print(*countries_list, sep='\n')
-
