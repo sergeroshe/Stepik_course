5919b41c50cc73a431ea191c93f1c8f1d557d869
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-07 22:38:04 +0300

    Removed unnecessary list

--- ADVANCED_COURSE/PART_1/11_2_6_dangerous_virus.py
+++ ADVANCED_COURSE/PART_1/11_2_6_dangerous_virus.py
@@ -4,21 +4,17 @@ ACCESS_CHAR_DICT = {'write': 'W', 'read': 'R', 'execute': 'X'}
 
 file_amount = int(input())
 file_description_list = [input() for _ in range(file_amount)]
-
 file_description_dict = {file_name: permit for file_name, *permit in [file_description.split()
                                                                       for file_description in file_description_list]}
 query_amount = int(input())
-
 query_list = [input() for _ in range(query_amount)]
 
-query_list = [(right, file) for file, right in [file_description.split()
-                                              for file_description in query_list]]
-
 respond_list = []
+for query in query_list:
+    for command, name in [query.split()]:
+        answer = ACCESS_PERMITTED_MESSAGE
+        if ACCESS_CHAR_DICT[command] not in file_description_dict[name]:
+            answer = ACCESS_DENIED_MESSAGE
+        respond_list.append(answer)
 
-for k, v in query_list:
-    answer = ACCESS_PERMITTED_MESSAGE
-    if ACCESS_CHAR_DICT[v] not in file_description_dict[k]:
-        answer = ACCESS_DENIED_MESSAGE
-    respond_list.append(answer)
 print(*respond_list, sep='\n')
