3d21a876c18cf1b5d8881aa443c2a3c07ad8e4d6
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-08 12:36:21 +0300

    Used inverted dict key iteration

--- ADVANCED_COURSE/PART_1/10_2_6_message_set.py
+++ ADVANCED_COURSE/PART_1/10_2_6_message_set.py
@@ -1,31 +1,23 @@
-PHONE_KEYS_DICT = {1: '.,?!:',
-                   2: 'ABC',
-                   3: 'DEF',
-                   4: 'GHI',
-                   5: 'JKL',
-                   6: 'MNO',
-                   7: 'PQRS',
-                   8: 'TUV',
-                   9: 'WXYZ',
-                   0: ' '
-                   }
-phone_keys_dict_list_len = len(PHONE_KEYS_DICT)
+PHONE_KEYS_DICT = {'.,?!:': 1,
+                   'ABC': 2,
+                   'DEF': 3,
+                   'GHI': 4,
+                   'JKL': 5,
+                   'MNO': 6,
+                   'PQRS': 7,
+                   'TUV': 8,
+                   'WXYZ': 9,
+                   ' ': 0}
 
-input_string = input().upper()
-# input_string = 'Hello, World!'.upper()
-
+# input_string = input().upper()
+input_string = 'Hello, World!'.upper()
+#
 output_string = ''
 
 for char in input_string:
-    char_found = False
-    i = 0
-    while not char_found and i < phone_keys_dict_list_len:
-        k = i
-        v = PHONE_KEYS_DICT[k]
-        if char in v:
-            char_idx = v.index(char) + 1
-            output_string += str(k) * char_idx
-            char_found = True
-        i += 1
+    for key in PHONE_KEYS_DICT:
+        if char in key:
+            output_string += str(PHONE_KEYS_DICT[key]) * (key.index(char) + 1)
+            break
 
 print(output_string)
