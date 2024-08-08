7ab4933517f4b55cbc0c9ba25a83d8c78cea8a42
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-08 12:54:52 +0300

    Removed unnecessary dict list

--- ADVANCED_COURSE/PART_1/10_2_5_training_courses_info.py
+++ ADVANCED_COURSE/PART_1/10_2_5_training_courses_info.py
@@ -4,19 +4,16 @@ COURSE_INFO_DICT = {'CS101': '3004, Хайнс 8:00',
                     'NT110': '1244, Берк, 11:00',
                     'CS103': '6755, Рич, 10:00'
                     }
-course_info_len = len(COURSE_INFO_DICT)
 
 course_id = input()
-
 
 course_info = ''
 
 course_not_found = True
 
-if course_id in COURSE_INFO_DICT[course_id]:
-        selected_dict = COURSE_INFO_DICT[i]
-        course_info = f'{course_id}: {selected_dict[course_id]}'
-        course_not_found = False
-    i += 1
+if course_id in COURSE_INFO_DICT:
+    selected_dict = COURSE_INFO_DICT[course_id]
+    course_info = f'{course_id}: {COURSE_INFO_DICT[course_id]}'
+    course_not_found = False
 
 print(course_info)
