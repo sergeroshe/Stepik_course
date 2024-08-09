e56d57b29b9501f9ce749219c515d431f3106406
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-09 15:38:26 +0300

    Changed cycle condition

--- ADVANCED_COURSE/PART_1/12_2_4_one_hundred_lottery_tickets.py
+++ ADVANCED_COURSE/PART_1/12_2_4_one_hundred_lottery_tickets.py
@@ -2,7 +2,7 @@ from random import randint
 
 ticket_set = set()
 
-while len(ticket_set) < 101:
+while len(ticket_set) != 100:
     cur_ticket = []
     for j in range(7):
         cur_ticket.append(str(randint(1, 9)))
