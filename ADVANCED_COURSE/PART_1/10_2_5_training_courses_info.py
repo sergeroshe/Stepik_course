5157c6a042959d823a17d68be3a158314d0760f5
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-09 12:29:02 +0300

    Removed extra calc

--- ADVANCED_COURSE/PART_1/10_2_5_training_courses_info.py
+++ ADVANCED_COURSE/PART_1/10_2_5_training_courses_info.py
@@ -1,19 +1,18 @@
-COURSE_INFO_DICT = {'CS101': '3004, Хайнс 8:00',
-                    'CM241': '1411, Ли, 13:00',
-                    'CS102': '4501, Альварадо, 9:00',
-                    'NT110': '1244, Берк, 11:00',
-                    'CS103': '6755, Рич, 10:00'
-                    }
+COURSE_DICT = {'CS101': '3004, Хайнс 8:00',
+               'CM241': '1411, Ли, 13:00',
+               'CS102': '4501, Альварадо, 9:00',
+               'NT110': '1244, Берк, 11:00',
+               'CS103': '6755, Рич, 10:00'
+               }
 
 course_id = input()
 
-course_info = ''
+course_desc = ''
 
 course_not_found = True
 
-if course_id in COURSE_INFO_DICT:
-    selected_dict = COURSE_INFO_DICT[course_id]
-    course_info = f'{course_id}: {COURSE_INFO_DICT[course_id]}'
-    course_not_found = False
+if course_id in COURSE_DICT:
+    course_info = COURSE_DICT[course_id]
+    course_desc = f'{course_id}: {course_info}'
 
-print(course_info)
+print(course_desc)
