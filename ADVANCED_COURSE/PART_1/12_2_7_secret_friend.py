2a3e1bbc78a378a428b040567b658b4b545ba869
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-12 14:43:27 +0300

    Changed for cycle to while

--- ADVANCED_COURSE/PART_1/12_2_7_secret_friend.py
+++ ADVANCED_COURSE/PART_1/12_2_7_secret_friend.py
@@ -1,14 +1,8 @@
-# student_amount = int(input())
 from random import shuffle
 
-student_amount = 5
+student_amount = int(input())
+student_list = [input() for _ in range(student_amount)]
 
-# student_list = [input() for _ in range(student_amount)]
-student_list = ['Владимир Смолов',
-                'Тагир Хан',
-                'Давид Лавров',
-                'Арина Приходько',
-                'Глеб Анисимов']
 friend_list = student_list.copy()
 shuffle(friend_list)
 secret_friend_set_list = []
@@ -16,12 +10,25 @@ added_friend_list = []
 
 for student in student_list:
     friend_list.remove(student)
-    for friend in friend_list:
+    friend_found = False
+
+    i = 0
+    while i < len(friend_list) and not friend_found:
+        friend = friend_list[i]
         if friend not in added_friend_list:
-            secret_friend_set_list.append(f'{student} - {friend}')
             added_friend_list.append(friend)
-            break
+            secret_friend_set_list.append(f'{student} - {friend}')
+            friend_found = True
+        else:
+            i += 1
     friend_list.append(student)
 
+
 print(*secret_friend_set_list, sep='\n')
-# print(*added_friend_list, sep='\n')
+
+
+# student_list = ['Владимир Смолов',
+#                 'Тагир Хан',
+#                 'Давид Лавров',
+#                 'Арина Приходько',
+#                 'Глеб Анисимов']
