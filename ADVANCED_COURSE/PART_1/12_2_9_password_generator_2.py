5f33782ac8e807e15c87ec8a0529aa3f8159419d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-13 12:26:10 +0300

    Added length checking func

--- ADVANCED_COURSE/PART_1/12_2_9_password_generator_2.py
+++ ADVANCED_COURSE/PART_1/12_2_9_password_generator_2.py
@@ -1,8 +1,9 @@
-from string import ascii_uppercase as u,\
+from string import ascii_uppercase as u, \
     ascii_lowercase as l, digits as d
 from random import shuffle, choice
 
 IGNORE_CHARS = 'lI1Oo0'
+MINIMUM_PASSWORD_LENGTH = 3
 CHAR_TYPE_AMOUNT = 3
 
 
@@ -18,11 +19,21 @@ def generate_password(pswd_length):
     return pswd_string
 
 
+def get_correct_length():
+    correct_length_entered = False
+    length = None
+    while not correct_length_entered:
+        length = int(input())
+        if length > MINIMUM_PASSWORD_LENGTH - 1:
+            correct_length_entered = True
+    return length
+
+
 def main():
     # password_amount = int(input())
     password_amount = 3
-    # password_length = int(input())
-    password_length = 15
+    password_length = get_correct_length()
+    # password_length = 15
 
     password_list = [generate_password(password_length) for _ in range(password_amount)]
     print(*password_list, sep='\n')
