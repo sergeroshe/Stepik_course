5c93fac5b7c43bd8edc37cbacc054e6fee96a023
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-19 15:17:14 +0300

    Split: logic and data output

--- ADVANCED_COURSE/PART_1/6_3_6_competetive_selection.py
+++ ADVANCED_COURSE/PART_1/6_3_6_competetive_selection.py
@@ -1,13 +1,31 @@
-pupil_number = int(input())
-pupil_list = [input().split() for _ in range(pupil_number)]
+pupil_amount = int(input())
 
-int_pupil_list = [(pupil[:-1], int(pupil[-1])) for pupil in pupil_list]
+pupil_list = []
+for i in range(pupil_amount):
+    pupil_info = input().split()
+    pupil = (pupil_info[0], int(pupil_info[-1]))
+    pupil_list.append(pupil)
 
-for pupil in pupil_list:
-    print(*pupil)
 
-print()
+result_list = []
+all_pupil_list = [pupil for pupil in pupil_list]
+result_list.append(all_pupil_list)
 
-for pupil in int_pupil_list:
-    if pupil[-1] > 3:
-        print(*pupil[0], pupil[-1])
+excellent_student_list = [ex_pupil for ex_pupil in pupil_list if ex_pupil[-1] > 3]
+result_list.append(excellent_student_list)
+
+for pupil_info_list in result_list:
+    for pupil_info in pupil_info_list:
+        print(*pupil_info)
+    print()
+
+# pupil_list = ['Круглов 4'.split(),
+#               'Кузнецов 5'.split(),
+#               'Федин 4'.split(),
+#               'Тарасов 2'.split(),
+#               'Словецкий 3'.split()]
+# pupil_list = ['Круглов 4',
+#               'Кузнецов 5',
+#               'Федин 4',
+#               'Тарасов 2',
+#               'Словецкий 3']
