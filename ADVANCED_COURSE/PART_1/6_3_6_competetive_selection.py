f949fe52902c4431467d4aa1a90ac549a834328e
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-28 12:48:24 +0300

    6_3_6 Competitive selection (#335)
    * Added task solution
    
    * Moved int casting to input
    
    * Split: logic and data output
    
    * Removed extra list

---
+++ ADVANCED_COURSE/PART_1/6_3_6_competetive_selection.py
@@ -0,0 +1,29 @@
+pupil_amount = int(input())
+
+pupil_list = []
+for i in range(pupil_amount):
+    pupil_info = input().split()
+    pupil = (pupil_info[0], int(pupil_info[-1]))
+    pupil_list.append(pupil)
+
+
+result_list = [pupil_list]   # list of two lists: all pupil and excellent pupil
+
+excellent_student_list = [ex_pupil for ex_pupil in pupil_list if ex_pupil[-1] > 3]
+result_list.append(excellent_student_list)
+
+for pupil_info_list in result_list:
+    for pupil_info in pupil_info_list:
+        print(*pupil_info)
+    print()
+
+# pupil_list = ['Круглов 4'.split(),
+#               'Кузнецов 5'.split(),
+#               'Федин 4'.split(),
+#               'Тарасов 2'.split(),
+#               'Словецкий 3'.split()]
+# pupil_list = ['Круглов 4',
+#               'Кузнецов 5',
+#               'Федин 4',
+#               'Тарасов 2',
+#               'Словецкий 3']
