757c65a4f7ba075d0f42689c67d083a90e33590d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-08-29 13:47:59 +0300

    Removed extra for loop

--- ADVANCED_COURSE/PART_1/8_5_5_correct_solutions_counter.py
+++ ADVANCED_COURSE/PART_1/8_5_5_correct_solutions_counter.py
@@ -1,5 +1,6 @@
 CORRECT_ANSWER = 'Correct'
 NO_CORRECT_SOLUTIONS_ANSWER = 'Вы можете стать первым, кто решит эту задачу'
+INACCURACY_MEASURE = 0.001
 
 attempts_total_amount = int(input())
 
@@ -8,19 +9,17 @@ result_list = [input() for _ in range(attempts_total_amount)]
 len_result_list = len(result_list)
 
 correct_answer_counter = 0
-
+unique_correct_answer_list = []
 for result in result_list:
     if result.split()[-1] == CORRECT_ANSWER:
         correct_answer_counter += 1
+        if result not in unique_correct_answer_list:
+            unique_correct_answer_list.append(result)
 
-unique_correct_answer_counter = 0
-
-for result in set(result_list):
-    if result.split()[-1] == CORRECT_ANSWER:
-        unique_correct_answer_counter += 1
+unique_correct_answer_counter = len(unique_correct_answer_list)
 
 if unique_correct_answer_counter:
-    correct_answer_percentage = round(correct_answer_counter / len_result_list * 100 + 0.001)
+    correct_answer_percentage = round(correct_answer_counter / len_result_list * 100 + INACCURACY_MEASURE)
     print(f'Верно решили {unique_correct_answer_counter} учащихся\n'
           f'Из всех попыток {correct_answer_percentage}% верных')
 else:
