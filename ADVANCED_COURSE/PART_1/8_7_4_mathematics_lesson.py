989a9673acae452cc3956632bdfc927b38e10efd
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-13 11:56:38 +0300

    Added comment

--- ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
+++ ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
@@ -7,11 +7,17 @@ all_pupils_unique_marks_set = set()
 for pupil_marks_set in pupil_marks_set_list:
     all_pupils_unique_marks_set |= pupil_marks_set
 
-two_pupils_unique_marks_set = all_pupils_unique_marks_set - \
-                              (pupil_marks_set_list[0] & pupil_marks_set_list[1]
-                               & pupil_marks_set_list[2])
+all_pupils_common_mark_set = pupil_marks_set_list[0]
+
+# common marks calculation
+for pupil_marks_set in pupil_marks_set_list[1:]:
+    all_pupils_common_mark_set &= pupil_marks_set
+
+two_pupils_unique_marks_set = all_pupils_unique_marks_set - all_pupils_common_mark_set
 
 print(*sorted(two_pupils_unique_marks_set, key=int))
+
+# make test for different pupil amount e.g. 3/2
 
 # pupil_marks_set_list = [set('1 5 4 2 5 6 6 2 3 3 5 2'.split()),
 #                         set('2 3 5 10 2 10 2 6 7 10 10 6'.split()),
