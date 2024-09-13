c3476f3c418dd783aebe876d5de5d9abaeb0c46d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-13 12:05:49 +0300

    Added comment

--- ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
+++ ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
@@ -6,8 +6,11 @@ all_lessons_presented_student_set = set()
 for _ in range(lesson_amount):
     current_lesson_attendance_set = {input() for _ in range(int(input()))}
     general_attendance_list.append(current_lesson_attendance_set)
+    # extra calc:
     all_lessons_presented_student_set = general_attendance_list[0]
+    # extra calc:
     if general_attendance_list:
         all_lessons_presented_student_set &= current_lesson_attendance_set
 
 print(*sorted(all_lessons_presented_student_set), sep='\n')
+
