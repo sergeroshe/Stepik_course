964d1bfb245f30a67e2040ac43db8c475821924f
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-17 17:46:40 +0300

    Moved break to inner loop

--- ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
+++ ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
@@ -1,10 +1,8 @@
 PUPIL_AMOUNT = 3
 MAX_COMMON_MARKS_PUPIL_AMOUNT = 2
 
-# pupil_marks_set_list = [set(input().split()) for _ in range(PUPIL_AMOUNT)]
-pupil_marks_set_list = [set('1 5 4 2 5 6 6 2 3 3 5 2'.split()),
-                        set('2 3 5 10 2 10 2 6 7 10 10 6'.split()),
-                        set('1 4 6 9 8 7 0 9 0 9 8 10'.split())]
+pupil_marks_set_list = [set(input().split()) for _ in range(PUPIL_AMOUNT)]
+
 
 all_pupils_unique_marks_set = set()
 
@@ -17,17 +15,13 @@ result_mark_set = set()
 # common marks calculation
 for pupil_marks_set in pupil_marks_set_list:
     for mark in pupil_marks_set:
-        mark_occurrence_amount = 0
+        mark_occurrience_amount = 0
 
-        for marks_set in pupil_marks_set_list[1:]:
+        for marks_set in pupil_marks_set_list:
             if mark in marks_set:
-                mark_occurrence_amount += 1
-        # remove extra calc
-        if mark_occurrence_amount <= MAX_COMMON_MARKS_PUPIL_AMOUNT:
+                mark_occurrience_amount += 1
+            if mark_occurrience_amount > MAX_COMMON_MARKS_PUPIL_AMOUNT:
+                break
+        if mark_occurrience_amount <= MAX_COMMON_MARKS_PUPIL_AMOUNT:
             result_mark_set.add(mark)
-        else:
-            break
 print(*sorted(result_mark_set, key=int))
-
-# make test for different pupil amount e.g. 3/2
-
