58e740de5fea52824b70e4cfaa7feb096a118d9d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-17 18:16:02 +0300

    Changed for loop to while

--- ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
+++ ADVANCED_COURSE/PART_1/8_7_4_mathematics_lesson.py
@@ -1,27 +1,23 @@
 PUPIL_AMOUNT = 3
 MAX_COMMON_MARKS_PUPIL_AMOUNT = 2
 
-pupil_marks_set_list = [set(input().split()) for _ in range(PUPIL_AMOUNT)]
+pupil_mark_set_list = [set(input().split()) for _ in range(PUPIL_AMOUNT)]
 
-
-all_pupils_unique_marks_set = set()
-
-for pupil_marks_set in pupil_marks_set_list:
-    all_pupils_unique_marks_set |= pupil_marks_set
-
-all_pupils_common_mark_set = set()
 result_mark_set = set()
+len_pupil_marks_set_list = len(pupil_mark_set_list)
 
-# common marks calculation
-for pupil_marks_set in pupil_marks_set_list:
+for pupil_marks_set in pupil_mark_set_list:
     for mark in pupil_marks_set:
-        mark_occurrience_amount = 0
+        mark_occurrence_amount = 0
+        max_mark_occurrence_exceeded = False
+        i = 0
+        while not max_mark_occurrence_exceeded and i < len_pupil_marks_set_list:
+            if mark in pupil_mark_set_list[i]:
+                mark_occurrence_amount += 1
+            if mark_occurrence_amount > MAX_COMMON_MARKS_PUPIL_AMOUNT:
+                max_mark_occurrence_exceeded = True
+            i += 1
 
-        for marks_set in pupil_marks_set_list:
-            if mark in marks_set:
-                mark_occurrience_amount += 1
-            if mark_occurrience_amount > MAX_COMMON_MARKS_PUPIL_AMOUNT:
-                break
-        if mark_occurrience_amount <= MAX_COMMON_MARKS_PUPIL_AMOUNT:
+        if mark_occurrence_amount <= MAX_COMMON_MARKS_PUPIL_AMOUNT:
             result_mark_set.add(mark)
 print(*sorted(result_mark_set, key=int))
