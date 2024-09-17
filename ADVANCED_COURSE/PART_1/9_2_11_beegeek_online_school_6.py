70c32d0d5e541c830f35781a66225868da5c39fa
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-17 18:26:59 +0300

    Implemented: init with first input method

--- ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
+++ ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
@@ -1,15 +1,13 @@
 lesson_amount = int(input())
 
 general_attendance_list = []
-all_lessons_presented_student_set = set()
-# init with first set, loop len - 1
-for _ in range(lesson_amount):
+
+first_lesson_attendace = {input() for _ in range(int(input()))}
+all_lessons_presented_student_set = first_lesson_attendace
+
+for _ in range(lesson_amount - 1):
     current_lesson_attendance_set = {input() for _ in range(int(input()))}
     general_attendance_list.append(current_lesson_attendance_set)
-    # extra calc:
-    all_lessons_presented_student_set = general_attendance_list[0]
-
     all_lessons_presented_student_set &= current_lesson_attendance_set
 
 print(*sorted(all_lessons_presented_student_set), sep='\n')
-
