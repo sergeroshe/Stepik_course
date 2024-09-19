ed57beb4d9a3351c4942d11fbfb77e3cacd4c5cd
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-09-19 11:45:58 +0300

    Removed extra string

--- ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
+++ ADVANCED_COURSE/PART_1/9_2_11_beegeek_online_school_6.py
@@ -2,8 +2,7 @@ lesson_amount = int(input())
 
 general_attendance_list = []
 
-first_lesson_attendace = {input() for _ in range(int(input()))}
-all_lessons_presented_student_set = first_lesson_attendace
+all_lessons_presented_student_set = {input() for _ in range(int(input()))}
 
 for _ in range(lesson_amount - 1):
     current_lesson_attendance_set = {input() for _ in range(int(input()))}
