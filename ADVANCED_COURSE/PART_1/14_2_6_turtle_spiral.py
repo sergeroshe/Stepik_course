85dc33091b52e55cf5002f6cb1c200001525620d
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-10-02 11:59:26 +0300

    Clarified: turtle final precise position

--- ADVANCED_COURSE/PART_1/14_2_6_turtle_spiral.py
+++ ADVANCED_COURSE/PART_1/14_2_6_turtle_spiral.py
@@ -1,19 +1,19 @@
 import turtle
 
 INIT_STEP = 5
-LIMIT_STEP = 100
+LIMIT_STEP = 20
 ANGLE_REDUCTION = 30
-FINAL_POSITION_ANGLE = 330
+FINAL_POSITION_ANGLE = 0
 STEP = 3
 
 
 def spiral(init_step, step_increase, limit_step, angle_reduction, final_position_angle):
-    angle = 360
+    angle = 0
     heading = turtle.heading()
 
     step = init_step
     size_reached = step > limit_step
-    turned_correct_side = heading < final_position_angle
+    turned_correct_side = final_position_angle - step <= heading <= final_position_angle
 
     spiral_complete = size_reached and turned_correct_side
     while not spiral_complete:
@@ -27,13 +27,14 @@ def spiral(init_step, step_increase, limit_step, angle_reduction, final_position
 
         heading = turtle.heading()
         print(heading)
-        turned_correct_side = heading < final_position_angle
+        turned_correct_side = final_position_angle - step <= heading <= final_position_angle
         size_reached = step > limit_step
         spiral_complete = size_reached and turned_correct_side
 
 
 def main():
     window = turtle.Screen()
+    turtle.Screen().bgcolor('green')
     turtle.showturtle()
     turtle.shape('turtle')
     spiral(INIT_STEP, STEP, LIMIT_STEP, ANGLE_REDUCTION, FINAL_POSITION_ANGLE)
