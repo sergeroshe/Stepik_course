c48400f656321c3d14dc16a8198f77d9abd8cf0f
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2024-10-04 10:27:32 +0300

    Added clarifying parameters

--- ADVANCED_COURSE/PART_1/14_2_7_multicolour_spiral.py
+++ ADVANCED_COURSE/PART_1/14_2_7_multicolour_spiral.py
@@ -1,43 +1,48 @@
 import turtle
 
-INIT_SIDE = 5
-LIMIT_SIDE = 300
-SHIFT = 5
+INIT_STEP = 5
+LIMIT_STEP = 200
+STEP_INCREASE = 5
+SIZE_INCREASE = 1
+FINAL_POSITION_ANGLE = 180
+SPIRAL_ANGLE = 45
+STEP = 3
 PENCOLOR_LIST = ['red', 'blue', 'yellow', 'green', 'violet', 'orange']
 
 
-def spiral(init_side, limit_side, shift, color_list):
-    pensize = turtle.pensize(5)
-    pencolor = turtle.pencolor()
-    side = init_side
-    size_reached = side > limit_side
+def spiral(init_step, step_increase, limit_step, size_increase, color_list, final_position_angle):
+    angle = 0
     heading = turtle.heading()
-    turned_up = heading == 90
-    spiral_complete = size_reached and turned_up
+
+    step = init_step
+    size_reached = step > limit_step
+    turned_correct_side = heading == final_position_angle + SPIRAL_ANGLE
+    color_list_len = len(color_list)
+    spiral_complete = size_reached and turned_correct_side
+    size = 1
     i = 0
     while not spiral_complete:
-        if i > len(color_list):
-            i = 0
-        turtle.forward(side)
-        increase = i * 3
+        turtle.pensize(size)
         turtle.pencolor(color_list[i])
-        pensize = turtle.pensize(increase)
-        side += shift
+        step += step_increase
+        size += size_increase
+        turtle.forward(step)
+        turtle.left(SPIRAL_ANGLE)
         heading = turtle.heading()
-        size_reached = side > limit_side
-        turned_up = heading == 90
-        spiral_complete = size_reached and turned_up
-        turtle.left(45)
+        print(i)
+        print(step)
+        turned_correct_side = heading == final_position_angle + SPIRAL_ANGLE
+        size_reached = step > limit_step
+        spiral_complete = size_reached and turned_correct_side
         i += 1
+        if i > color_list_len - 1:
+            i = 0
 
 
 def main():
     window = turtle.Screen()
-    turtle.Screen().bgcolor('green')
     turtle.showturtle()
-    turtle.shape('triangle')
-    spiral(INIT_SIDE, LIMIT_SIDE, SHIFT, PENCOLOR_LIST)
-    turtle.done()
+    spiral(INIT_STEP, STEP_INCREASE, LIMIT_STEP, SIZE_INCREASE, PENCOLOR_LIST, FINAL_POSITION_ANGLE)
     window.mainloop()
 
 
