9fbb27621115a7b6d2a5f3076b0aa8f772af5103
sergero <roshchektaev@gmail.com>
2024-10-21 11:54:55 +0300

    Added task solution

--- ADVANCED_COURSE/PART_1/14_2_11_bear_head.py
+++ ADVANCED_COURSE/PART_1/14_2_11_bear_head.py
@@ -1,5 +1,46 @@
+import turtle as t
+
+HEAD_RADIUS = 170
+FACE_RADIUS = 100
+NOSE_RADIUS = 10
+JAW_HEIGHT = 70
+MOUTH_HEIGHT = 10
+EAR_RADIUS = 50
+
+
+def bear_head():
+    t.circle(NOSE_RADIUS)
+    t.goto(0, -JAW_HEIGHT)
+    t.penup()
+    t.goto(t.pos()[0], t.pos()[1] - JAW_HEIGHT)
+    t.pendown()
+    t.circle(FACE_RADIUS)
+    t.circle(HEAD_RADIUS)
+    t.penup()
+    t.goto(120, 165)
+    t.pendown()
+    t.circle(EAR_RADIUS)
+    t.penup()
+    t.goto(t.pos()[0] - 240, t.pos()[1])
+    t.pendown()
+    t.circle(EAR_RADIUS)
+    t.penup()
+    t.goto(t.pos()[0] + 50, t.pos()[1] - FACE_RADIUS)
+    t.pendown()
+    t.dot(30)
+    t.penup()
+    t.goto(t.pos()[0] + 140, t.pos()[1])
+    t.dot(30)
+
+
 def main():
-    pass
+    window = t.Screen()
+    t.showturtle()
+    t.speed(0)
+    t.pensize(5)
+    bear_head()
+    # t.hideturtle()
+    window.mainloop()
 
 
 main()
