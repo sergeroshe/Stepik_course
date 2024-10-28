565107b8fc0ff1243e2e7d0a7fe8e9aacf736cb8
sergero <roshchektaev@gmail.com>
2024-10-28 14:29:16 +0300

    Added task solution some steps

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -1,8 +1,38 @@
 import turtle as t
 
+RADIUS = 100
 
-def snowflake():
-    pass
+
+
+def snowflake(x_pos, y_pos):
+    angle = 360 / 8
+    for _ in range(8):
+        t.forward(RADIUS)
+        t.penup()
+        t.goto(x_pos, y_pos)
+        t.left(angle)
+        t.pendown()
+    t.forward(RADIUS / 4)
+    # t.speed(1)
+    t.left(angle)
+    t.forward(RADIUS / 4)
+    for _ in range(6):
+        t.left(135)
+        t.forward(RADIUS / 4)
+        t.right(90)
+        t.forward(RADIUS / 4)
+    # t.left(135)
+    # t.forward(RADIUS / 4)
+    # t.right(90)
+    # t.forward(RADIUS / 4)
+    # t.left(135)
+    # t.forward(RADIUS / 4)
+    # t.right(90)
+    t.forward(RADIUS / 4)
+    t.left(135)
+    t.forward(RADIUS / 4)
+
+
 
 
 def main():
@@ -10,6 +40,7 @@ def main():
     t.showturtle()
     t.speed(0)
     t.pensize(5)
+    snowflake(0, 0)
     # t.hideturtle()
     window.mainloop()
 
