93f3d51875e35daef4da121fd30d9749711d16b2
sergero <roshchektaev@gmail.com>
2024-10-28 13:57:18 +0300

    Added task main

---
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+
+def snowflake():
+    pass
+
+
+def main():
+    window = t.Screen()
+    t.showturtle()
+    t.speed(0)
+    t.pensize(5)
+    # t.hideturtle()
+    window.mainloop()
+
+
+main()
