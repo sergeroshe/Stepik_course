b40cd608da02a7983f730e32937f11a40691cf9e
sergero <roshchektaev@gmail.com>
2024-11-08 19:06:12 +0300

    Added random coordinates and size calc

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -1,61 +1,59 @@
 import turtle as t
+import random as r
 
-RADIUS = 100
 
-
-def snowflake(x_pos, y_pos):
+def snowflake(x_pos, y_pos, radius):
+    feather_length = radius // 4
     t.speed(0)
     angle = 45
     center = x_pos, y_pos
+    t.penup()
+    # t.goto(center)
 
     for i in range(8):
         t.goto(center)
-        # t.setheading(0)
-        t.left(angle * i)
-        # t.forward(RADIUS)
-        # # t.penup()
-
-        for j in range(1, 4):
-            t.goto(center)
-            t.setheading(angle * i)
+        t.setheading(angle * i)
+        for j in range(3):
             t.pendown()
-            t.forward((RADIUS // 4) * j)
-            rod_pos = t.pos()[0], t.pos()[1]
-            t.left(angle)
-            t.pendown()
-            t.forward(RADIUS // 4)
-            # t.penup()
-            t.goto(rod_pos)
-            t.setheading(angle * i)
-            t.right(angle)
-            t.pendown()
-            t.forward(RADIUS // 4)
-            t.penup()
-            t.goto(rod_pos)
-            t.setheading(angle * i)
-            t.pendown()
-            t.forward(RADIUS // 4)
+            # step 1
+            t.forward(feather_length)
+            for k in range(1, 3):
+                rod_pos = t.pos()[0], t.pos()[1]
+                if k % 2:
+                    t.left(angle)
+                else:
+                    t.right(angle)
+                t.pendown()
+                # step 2
+                t.forward(feather_length)
+                t.penup()
+                t.goto(rod_pos)
+                t.setheading(angle * i)
+                t.pendown()
+            # step 3
+        t.forward(feather_length)
 
 
-        # if j == 1:
-        #     t.left(135)
-        #     t.pendown()
-        #     t.forward(RADIUS // 4)
-        #     t.penup()
-        # t.goto(rod_pos)
-        # t.setheading(angle * i)
-        # t.right(angle)
-        # t.pendown()
-        # t.forward(RADIUS // 4)
-        # t.penup()
+def snowfall():
+    while True:
+        current_coordinates_set = set()
+        snowflake_radius = r.choice(range(10, 100))
+        coordinates_range = (range(-100, 100, snowflake_radius * 3))
+        coordinates = tuple(r.sample(coordinates_range, 2))
+        print(coordinates)
+        current_coordinates_set.add(coordinates)
+        current_coordinates = r.choice(list(current_coordinates_set))
+        print(current_coordinates)
+
+        snowflake(current_coordinates[0], current_coordinates[0], snowflake_radius)
 
 
 def main():
     window = t.Screen()
     t.showturtle()
     t.speed(0)
-    t.pensize(5)
-    snowflake(0, 0)
+    t.pensize(1)
+    snowfall()
     # t.hideturtle()
     window.mainloop()
 
