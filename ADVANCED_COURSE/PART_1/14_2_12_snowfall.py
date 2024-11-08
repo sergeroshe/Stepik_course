9094bcedc1d440fbf902351638d504bd3c4dc436
sergero <roshchektaev@gmail.com>
2024-11-08 17:03:05 +0300

    Added working but poor yet task solution

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -4,71 +4,50 @@ RADIUS = 100
 
 
 def snowflake(x_pos, y_pos):
-    center = x_pos, y_pos
-    t.goto(center)
+    t.speed(0)
     angle = 45
-    for _ in range(8):
-        t.forward(RADIUS)
-        t.penup()
-        t.goto(x_pos, y_pos)
-        t.left(angle)
-        t.pendown()
-    t.forward(RADIUS // 4)
-    t.right(225)
-    for _ in range(8):
-        t.right(90)
-        t.forward(RADIUS // 4)
-        t.left(135)
-        t.forward(RADIUS // 4)
-    t.speed(1)
-    t.penup()
-    t.setheading(0)
-    x_pos = t.pos()[0]
-    y_pos = t.pos()[1]
-    shift = RADIUS // 4
-    angle = 0
-    # t.goto(x_pos + shift, y_pos)
-    for i in range(1, 8):
+    center = x_pos, y_pos
+
+    for i in range(8):
         t.goto(center)
-        t.left(angle)
+        # t.setheading(0)
+        t.left(angle * i)
+        # t.forward(RADIUS)
+        # # t.penup()
 
-        for _ in range(3):
-            # make it go to the proper position!
-            t.goto(x_pos + shift, y_pos)
-            x_pos = t.pos()[0]
-            y_pos = t.pos()[1]
-            t.penup()
-            t.left(45)
+        for j in range(1, 4):
+            t.goto(center)
+            t.setheading(angle * i)
+            t.pendown()
+            t.forward((RADIUS // 4) * j)
+            rod_pos = t.pos()[0], t.pos()[1]
+            t.left(angle)
+            t.pendown()
+            t.forward(RADIUS // 4)
+            # t.penup()
+            t.goto(rod_pos)
+            t.setheading(angle * i)
+            t.right(angle)
             t.pendown()
             t.forward(RADIUS // 4)
             t.penup()
-            t.goto(x_pos, y_pos)
-            t.setheading(angle)
-            t.right(45)
+            t.goto(rod_pos)
+            t.setheading(angle * i)
             t.pendown()
             t.forward(RADIUS // 4)
-            t.goto(x_pos, y_pos)
-            t.setheading(angle)
-        angle += 45
 
 
-
-        # x_pos = t.pos()[0]
-        # y_pos = t.pos()[1]
-        # shift += RADIUS // 4
-
-    # for _ in range(3):
-    #     t.left(45)
-    #     t.pendown()
-    #     t.forward(RADIUS / 4)
-    #     t.penup()
-    #     t.goto(x, y)
-    #     t.setheading(0)
-    #     t.right(45)
-    #     t.pendown()
-    #     t.forward(RADIUS / 4)
-    #     t.goto(x, y)
-
+        # if j == 1:
+        #     t.left(135)
+        #     t.pendown()
+        #     t.forward(RADIUS // 4)
+        #     t.penup()
+        # t.goto(rod_pos)
+        # t.setheading(angle * i)
+        # t.right(angle)
+        # t.pendown()
+        # t.forward(RADIUS // 4)
+        # t.penup()
 
 
 def main():
