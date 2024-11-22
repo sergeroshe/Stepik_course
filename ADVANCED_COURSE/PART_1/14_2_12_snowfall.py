2d10e08950ecbd0be711c9bbfa8782f9c0bbc1d5
SERGERO <roshchektaev@gmail.com>
2024-11-22 12:37:52 +0300

    Added const

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -1,10 +1,20 @@
 import turtle as t
 import random as r
 
+X_COORDINATES_RANGE = range(-200, 200)
+Y_COORDINATES_RANGE = range(-200, 200)
 
-def get_snowflake_coordinates(prev_coord, prev_snowflake_radius):
-    x_coord, y_coord = r.sample(list(range(200)), 2)
-    return x_coord, y_coord
+
+def get_coordinates(x_range, y_range, snowflake_radius):
+    x_coordinates_range_list = list(x_range)
+    y_coordinates_range_list = list(y_range)
+    x_coord = r.choice(x_coordinates_range_list)
+    y_coord = r.choice(y_coordinates_range_list)
+    taken_x_space = set(list(range(x_coord - snowflake_radius, x_coord + snowflake_radius)))
+    taken_y_space = set(list(range(y_coord - snowflake_radius, y_coord + snowflake_radius)))
+    free_x_coordinates_range = x_range - taken_x_space
+    free_y_coordinates_range = y_range - taken_y_space
+    return x_coord, y_coord, free_x_coordinates_range, free_y_coordinates_range
 
 
 def snowflake(x_pos, y_pos, radius, feather_amount, color):
@@ -45,19 +55,17 @@ def snowflake_feather(feather_length, ray_amount, angle, feather_number):
 
 
 def snowfall():
-    while True:
-        current_coordinates_set = set()
+    x_coordinates_range = (X_COORDINATES_RANGE)
+    y_coordinates_range = (Y_COORDINATES_RANGE)
+    while x_coordinates_range and y_coordinates_range:
         snowflake_radius = r.choice(range(10, 100))
-        x_coordinates_range = (range(-snowflake_radius * 10, snowflake_radius * 10, snowflake_radius))
-        y_coordinates_range = (range(-snowflake_radius * 10, snowflake_radius * 10, snowflake_radius))
-        x_coordinates = get_snowflake_coordinates(0, 0)[0]
-        y_coordinates = get_snowflake_coordinates(0, 0)[1]
+        x_coordinates = get_coordinates(x_coordinates_range, y_coordinates_range, snowflake_radius)[0]
+        y_coordinates = get_coordinates(x_coordinates_range, y_coordinates_range, snowflake_radius)[1]
+        x_coordinates_range = get_coordinates(x_coordinates, y_coordinates, snowflake_radius)[2]
+        y_coordinates_range = get_coordinates(x_coordinates, y_coordinates, snowflake_radius)[3]
         # coordinates = tuple(x_coordinates, y_coordinates)
-        current_coordinates_set.add((x_coordinates, y_coordinates))
         # make a square around used coordinates to use only wmpty space
-        current_coordinates = r.choice(list(current_coordinates_set))
-        snowflake(current_coordinates[0], current_coordinates[0], snowflake_radius, 8, 'blue')
-        print(current_coordinates_set)
+        snowflake(x_coordinates, y_coordinates, snowflake_radius, 8, 'blue')
 
 
 def main():
