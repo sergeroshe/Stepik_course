f3e3f05d65d5f2e7276db9badf8ec86a4e07cc5c
SERGERO <roshchektaev@gmail.com>
2025-01-06 13:32:19 +0300

    Removed unwanted loop condition

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -84,7 +84,7 @@ def draw_snowflake_feather(ray_length, ray_amount, angle, direction):
 def start_snowfall():
     prev_circle_list = []
     # rand color, size, ray amont
-    while len(prev_circle_list) < len(COORD_LIST_X):
+    while True:
         x_pos, y_pos, cur_radius = get_circle(prev_circle_list)
         draw_snowflake(x_pos, y_pos, cur_radius, 8, 'blue')
         value = (x_pos, y_pos, cur_radius)
