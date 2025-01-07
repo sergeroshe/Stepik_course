11feeb82ebab83919be3f24520266d60b1ddacea
SERGERO <roshchektaev@gmail.com>
2025-01-07 17:46:13 +0300

    Renamed func to get_circle_space

--- ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
+++ ADVANCED_COURSE/PART_1/14_2_12_snowfall.py
@@ -14,8 +14,8 @@ TRY_AMOUNT_LIMIT = 2
 
 def get_circle_space():
     # impelement random, compare with choice
-    x_pos = r.random() * 100
-    y_pos = r.random() * 100
+    x_pos = r.random() * 200
+    y_pos = r.random() * 200
     cur_radius = r.choice(range(MIN_SNOWFLAKE_RADIUS, MAX_SNOWFLAKE_RADIUS))
     return x_pos, y_pos, cur_radius
 
@@ -26,7 +26,7 @@ def get_snowflake_params():
 
 
 # rename vars to concistance
-def get_circle(prev_circle_list, try_amount_limit):
+def get_circle_space(prev_circle_list, try_amount_limit):
     free_space_found = False
     try_amount = 0
     prev_circle_list_len = len(prev_circle_list)
@@ -34,6 +34,7 @@ def get_circle(prev_circle_list, try_amount_limit):
     while not free_space_found and not try_limit_exceeded:
         overlay_found = False
         x_pos, y_pos, radius = get_circle_space()
+        print(f'x = {x_pos}\ny = {y_pos}')
         i = 0
         while not overlay_found and i < prev_circle_list_len:
             params = prev_circle_list[i]
@@ -41,7 +42,7 @@ def get_circle(prev_circle_list, try_amount_limit):
             prev_y_pos = params[1]
             prev_radius = params[2]
             distance = math.sqrt((x_pos - prev_x_pos) ** 2 + (y_pos - prev_y_pos) ** 2)
-
+            print(f'distance = {distance}')
             if prev_radius + radius >= distance:
                 overlay_found = True
                 print(f'overlay found!')
@@ -105,7 +106,7 @@ def start_snowfall():
     # rand color, size, ray amont
     try_limit_exceeded = False
     while not try_limit_exceeded:
-        x_pos, y_pos, radius, try_limit_exceeded = get_circle(prev_circle_list, TRY_AMOUNT_LIMIT)
+        x_pos, y_pos, radius, try_limit_exceeded = get_circle_space(prev_circle_list, TRY_AMOUNT_LIMIT)
         color = get_snowflake_params()
         if not try_limit_exceeded:
             draw_snowflake(x_pos, y_pos, radius, 2, color)
