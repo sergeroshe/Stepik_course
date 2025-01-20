cd67740f16d0c9953ed22f906fa1e5ec0c02c80d
SERGERO <roshchektaev@gmail.com>
2025-01-20 21:27:15 +0300

    Added task init solution

--- ADVANCED_COURSE/PART_1/14_3_1_little_house.py
+++ ADVANCED_COURSE/PART_1/14_3_1_little_house.py
@@ -2,7 +2,7 @@ import turtle as t
 
 
 HOUSE_BODY_PARAMS = (300, 'blue')
-HOUSE_ROOF_PARAMS = (350, 'brown')
+HOUSE_ROOF_PARAMS = (380, 'brown')
 
 
 def draw_square(size, color):
@@ -14,9 +14,8 @@ def draw_square(size, color):
     t.end_fill()
 
 
-def draw_triangle(size, color, start_pos):
-    t.goto(start_pos[0], start_pos[1])
-    # t.goto(-size // 2, start_pos[1])
+def draw_triangle(size, color):
+    t.goto(-size // 2, 0)
     t.fillcolor(color)
     t.begin_fill()
     for i in range(3):
@@ -28,24 +27,26 @@ def draw_triangle(size, color, start_pos):
 def draw_house():
     square_size = HOUSE_BODY_PARAMS[0]
     square_color = HOUSE_BODY_PARAMS[1]
-    start_pos = (-square_size // 2, -square_size)
+    body_start_pos = (-square_size // 2, -square_size)
     t.penup()
-    t.goto(start_pos)
+    t.goto(body_start_pos)
+    t.pendown()
     draw_square(square_size, square_color)
-    roof_start_pos = (start_pos[0] + start_pos[0] // 2, start_pos[1] + square_size)
+    roof_start_pos = (0, 0)
+    t.penup()
+    t.goto(roof_start_pos)
     triangle_size = HOUSE_ROOF_PARAMS[0]
     triangle_color = HOUSE_ROOF_PARAMS[1]
     t.pendown()
-    draw_triangle(triangle_size, triangle_color, roof_start_pos)
+    draw_triangle(triangle_size, triangle_color)
 
 
 def main():
     window = t.Screen()
+    t.hideturtle()
     draw_house()
     t.speed(3)
     t.pensize(1)
-    draw_house()
-    t.hideturtle()
     window.mainloop()
 
 
