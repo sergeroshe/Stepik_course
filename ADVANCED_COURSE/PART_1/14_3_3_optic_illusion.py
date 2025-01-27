6ef57235e87f755956dc5945e45dc3ff0837340b
SERGERO <roshchektaev@gmail.com>
2025-01-27 13:12:42 +0300

    Added task main funcs

--- ADVANCED_COURSE/PART_1/14_3_3_optic_illusion.py
+++ ADVANCED_COURSE/PART_1/14_3_3_optic_illusion.py
@@ -1,5 +1,22 @@
-def main():
+import turtle as t
+
+
+def draw_triangle(size, color):
     pass
+
+def draw_illusion(size, color):
+    pass
+
+def main():
+    window = t.Screen()
+    # t.hideturtle()
+    t.penup()
+    t.speed(0)
+
+    # draw_traffic_signal()
+
+    t.pensize(1)
+    window.mainloop()
 
 
 main()
