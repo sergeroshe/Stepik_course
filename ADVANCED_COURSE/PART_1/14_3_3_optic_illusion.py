da4191870e5e89fe1dff469e366806c412815390
SERGERO <roshchektaev@gmail.com>
2025-01-27 13:19:32 +0300

    Added size const

--- ADVANCED_COURSE/PART_1/14_3_3_optic_illusion.py
+++ ADVANCED_COURSE/PART_1/14_3_3_optic_illusion.py
@@ -1,11 +1,21 @@
 import turtle as t
 
+SIZE = 300
+
 
 def draw_triangle(size, color):
-    pass
+    for _ in range(3):
+        t.forward(size)
+        t.left(120)
+
 
 def draw_illusion(size, color):
+    start_position = (-size // 2, -size // 2)
+    t.goto(start_position)
+    t.pendown()
+    draw_triangle(size, color)
     pass
+
 
 def main():
     window = t.Screen()
@@ -13,7 +23,7 @@ def main():
     t.penup()
     t.speed(0)
 
-    # draw_traffic_signal()
+    draw_illusion(SIZE, 'black')
 
     t.pensize(1)
     window.mainloop()
