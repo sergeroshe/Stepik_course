e138d841df941536f6994b4ed1893f6023af3b9a
sergeroshe <roshchektaev@gmail.com>
2025-02-05 12:40:17 +0300

    Added task init solution

---
+++ 14_3_4_multicolor_circles.py
@@ -0,0 +1,44 @@
+import turtle as t
+
+CIRCLE_MIN_SIZE = 40
+CIRCLE_MAX_SIZE = 400
+
+COLOR_LIST = ['pink', 'violet', 'blue', 'white', 'yellow', 'orange', 'red', 'green', 'brown']
+SHIFT_SIZE = 50
+
+def draw_circle(size, color):
+    t.fillcolor(color)
+    t.begin_fill()
+    t.circle(size)
+    t.end_fill()
+
+
+# give a proper name
+def draw_picture():
+    t.goto(0, -CIRCLE_MAX_SIZE // 2)
+    size = CIRCLE_MAX_SIZE
+    i = len(COLOR_LIST) - 1
+    pos = t.pos()
+    while i:
+        draw_circle(size, COLOR_LIST[i])
+        pos = pos[0], pos[1] + SHIFT_SIZE
+        t.goto(pos)
+        size -= SHIFT_SIZE
+        i -= 1
+        
+
+
+
+def main():
+    window = t.Screen()
+    t.hideturtle()
+    t.penup()
+    t.speed(0)
+
+    draw_picture()
+
+    t.pensize(1)
+    window.mainloop()
+
+
+main()
