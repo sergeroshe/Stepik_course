7379171dda28e649e89910a117df8a0898987a1c
sergeroshe <roshchektaev@gmail.com>
2025-03-28 13:34:49 +0300

    Added initial task structure

---
+++ 14_3_9_chessboard_drawing.py
@@ -0,0 +1,35 @@
+import turtle as t
+import math as m
+import random as r
+
+FIELD_SIZE = 300
+FIGURE_MIN_SIDE_AMOUNT = 3
+FIGURE_MAX_SIDE_AMOUNT = 12
+POLYGON_AREA = 2500
+STEP = POLYGON_AREA // 30
+
+
+def draw_square(side_amount, side_length, color):
+  pass
+
+
+def get_square_params():
+  pass
+
+    
+def draw_squares():
+  pass
+
+
+def main():
+  window = t.Screen()
+  t.showturtle()
+  t.speed(3)
+  t.pensize(1)
+
+  draw_squares()
+
+  t.hideturtle()
+  window.mainloop()
+
+main()
