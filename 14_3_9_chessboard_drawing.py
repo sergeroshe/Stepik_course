ad768894b15be5983503f0992c7cd0cd33799596
sergeroshe <roshchektaev@gmail.com>
2025-03-31 18:27:12 +0300

    Added task solution

--- 14_3_9_chessboard_drawing.py
+++ 14_3_9_chessboard_drawing.py
@@ -2,22 +2,49 @@ import turtle as t
 import math as m
 import random as r
 
-FIELD_SIZE = 300
-FIGURE_MIN_SIDE_AMOUNT = 3
-FIGURE_MAX_SIDE_AMOUNT = 12
-POLYGON_AREA = 2500
-STEP = POLYGON_AREA // 30
+FIELD_SIZE = 100
+SQUARE_SIZE = 25
+CHESSBOARD_SIZE = FIELD_SIZE // SQUARE_SIZE
+
+COLOR_LIST = ['black', 'white']
 
 
-def draw_square(side_amount, side_length, color):
-  pass
+def draw_square(side_length):
+    for _ in range(4):
+        t.forward(side_length)
+        t.left(90)
 
 
 def get_square_params():
   pass
 
     
-def draw_squares():
+def draw_squares(chessboard_size, square_side, color_list):
+  t.penup()
+  t.goto(-chessboard_size // 2, chessboard_size // 2)
+  t.pendown()
+  setheading_list = [0, 180]
+  step_list = [square_side * 2, 0]
+  color_list_idx = 0
+
+  for i in range(chessboard_size):
+    if i:
+      t.forward(step_list[i % 2])    
+    t.setheading(setheading_list[i % 2])
+    for j in range(chessboard_size):
+      t.fillcolor(color_list[color_list_idx % 2])
+
+      t.begin_fill()
+      draw_square(square_side)
+      t.end_fill()
+
+      t.forward(square_side)
+      color_list_idx += 1
+      print(f'idx = {color_list_idx}')          
+
+    t.left(90)
+
+    
   pass
 
 
@@ -27,7 +54,7 @@ def main():
   t.speed(3)
   t.pensize(1)
 
-  draw_squares()
+  draw_squares(CHESSBOARD_SIZE, SQUARE_SIZE, COLOR_LIST)
 
   t.hideturtle()
   window.mainloop()
