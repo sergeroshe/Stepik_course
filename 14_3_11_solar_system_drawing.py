cfb041c33c41d6802e52c06debe3b0a03ad594f9
sergeroshe <roshchektaev@gmail.com>
2025-04-04 13:33:39 +0300

    Added: task main()

---
+++ 14_3_11_solar_system_drawing.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+def draw_solar_system():
+  pass
+
+
+def main():
+  window = t.Screen()
+  t.showturtle()
+  t.speed(3)
+  t.pensize(1)
+
+  draw_solar_system()
+
+  t.hideturtle()
+  window.mainloop()
+
+main()
